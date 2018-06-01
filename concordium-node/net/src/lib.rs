085a75465da0f29d0bab5026b753640213467e68
Martin Karlsen Jensen <mkj@concordium.com>
2018-06-01 14:36:16 +0200

    Added non working play with mio

--- concordium-node/net/src/lib.rs
+++ concordium-node/net/src/lib.rs
@@ -3,6 +3,9 @@
 extern crate hacl_star;
 extern crate rand;
 extern crate tokio;
+extern crate tokio_proto;
+extern crate tokio_io;
+extern crate tokio_service;
 #[macro_use]
 extern crate futures;
 extern crate bytes;
--- concordium-node/net/src/service.rs
+++ concordium-node/net/src/service.rs
@@ -1,97 +1,47 @@
-use tokio::io;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::prelude::*;
-use tokio::*;
-use futures::sync::mpsc;
-use futures::future::{self, Either};
-use futures::Map;
-use bytes::{BytesMut, Bytes, BufMut};
-use mio::Token;
-use tokio::runtime::Runtime;
+use mio::*;
+use mio::net::TcpListener;
+use mio::net::TcpStream;
 
-use std::collections::HashMap;
-use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
-
-const SERVER_TOKEN: Token = Token(0);
-
-struct P2PNode {
-    token_counter: usize,
-    peers: HashMap<Token, Arc<TcpStream>>,
-    runtime: Runtime,
-
-}
-
-impl P2PNode {
-    fn new() -> P2PNode {
-        let mut rt = Runtime::new().unwrap();
-
-        P2PNode {
-            token_counter: 0,
-            peers: HashMap::new(),
-            runtime: rt
-        }
-    }
-
-    //Connect to new peer
-    fn connect(&mut self, addr: &String) {
-        let addr = addr.parse::<SocketAddr>().unwrap();
-        let connect_future = TcpStream::connect(&addr);
-   
-        //MPSC tunnel for communicating inside closure?
-
-        let task = connect_future.and_then(move |stream| {
-            let stream = stream;
-            println!("Connected successfully!");
-            match stream.peer_addr() {
-                Err(e) => println!("Error: {}", e),
-                Ok(x) => println!("Remote Address is {}", x)
-            }
-            Ok(())
-        })
-        .map_err(|e| println!("Failed to connect; {:?}", e));
-
-        //self.peers.insert(Token(self.token_counter), stream);
-
-        self.runtime.spawn(task);
-    }
-
-    fn process(&mut self, socket: TcpStream) {
-        match socket.peer_addr() {
-            Ok(x) => println!("Address is {}", x),
-            Err(y) => println!("Error: {}",y ),
-        };
-    }
-}
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
 
 fn main() {
     let addr = "127.0.0.1:8888".parse().unwrap();
-    let listener = TcpListener::bind(&addr).unwrap();
 
-    let server = listener.incoming()
-        .map_err(|e| println!("Error= {}", e))
-        .for_each(|socket| {
-            future::lazy(move || {
-                println!("Handling socket");
-                match socket.peer_addr() {
-                    Err(e) => println!("Error: {}", e),
-                    Ok(x) => println!("Remote Address is {}", x)
-                }
-                Ok(())
-        })
-    });
-    
-    let mut node = P2PNode::new();
+    let server = TcpListener::bind(&addr).unwrap();
 
-    node.runtime.spawn(server);
+    let poll = Poll::new().unwrap();
 
+    poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
 
-    node.connect(&String::from("127.0.0.1:8888"));
-    node.connect(&String::from("127.0.0.1:8888"));
-    node.connect(&String::from("127.0.0.1:8888"));
-    node.connect(&String::from("127.0.0.1:8888"));
-    
+    let sock = TcpStream::connect(&addr).unwrap();
+    poll.register(&sock, CLIENT, Ready::readable(), PollOpt::edge()).unwrap();
 
-    node.runtime.shutdown_on_idle()
-        .wait().unwrap();
+    let mut events = Events::with_capacity(1024);
+
+    loop {
+        poll.poll(&mut events, None).unwrap();
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    let socket = server.accept();
+
+                    match socket {
+                        Some(x) => {
+                            match x.peer_addr() {
+                                Ok(y) => println!("Peer addr: {}", y),
+                                Err => println!("Couldn't retrieve peer address")
+                            }
+                        },
+                        None => println!("Broken")
+                    };
+                },
+                CLIENT => {
+                    return;
+                },
+                _ => unreachable!(),
+            };
+        }
+    }
 }
