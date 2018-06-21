bb00b0f5ae78c0d6b234aaa5adeb66a42916f8f2
Martin Karlsen Jensen <mkj@concordium.com>
2018-06-21 12:25:33 +0200

    More work

--- concordium-node/examples/p2p_sample.rs
+++ concordium-node/examples/p2p_sample.rs
@@ -2,6 +2,9 @@ extern crate p2p_client;
 #[macro_use] extern crate structopt;
 extern crate bytes;
 extern crate mio;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 use std::sync::mpsc;
 use std::thread;
 use p2p_client::p2p::*;
@@ -9,22 +12,32 @@ use p2p_client::configuration;
 use mio::Events;
 
 fn main() {
+    env_logger::init().ok().expect("Failed to initialize logger");
+
     let conf = configuration::parse_config();
-    let node_ip = match conf.connect_to {
+    let node_ip = match conf.remote_ip {
         Some(x) => { x },
         _ => { String::from("127.0.0.1") }
     };
+
+    let node_port = match conf.remote_port {
+        Some(x) => x,
+        _ => 8888,
+    };
+
+    let listen_port = match conf.listen_port {
+        Some(x) => x,
+        _ => 8888,
+    };
+
     let (out_tx, out_rx) = mpsc::channel();
     let (in_tx, in_rx) = mpsc::channel();
 
-    let mut node = P2PNode::new(out_rx, in_tx);
+    let mut node = P2PNode::new(listen_port, out_rx, in_tx);
 
     let (connect_send, mut connect_recv) = mpsc::channel();
 
     let tok1 = node.connect(P2PPeer::new(node_ip.parse().unwrap(), 8888)).unwrap();
-    println!("First token is: {:?}", tok1);
-    let tok2 = node.connect(P2PPeer::new(node_ip.parse().unwrap(), 8888)).unwrap();
-    let tok3 = node.connect(P2PPeer::new(node_ip.parse().unwrap(), 8888)).unwrap();
 
     let th = thread::spawn(move || {
         let mut events = Events::with_capacity(1024);
@@ -35,7 +48,7 @@ fn main() {
         loop {
             match in_rx.try_recv() {
                 Ok(x) => {
-                    println!("Received: {:?} from {:?}", String::from_utf8_lossy(&x.msg), x.token);
+                    println!("Received: {:?} from {:?}", String::from_utf8(x.msg).unwrap(), x.token);
                 },
                 _ => {
 
@@ -43,13 +56,6 @@ fn main() {
             };
         }
     });
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
-    out_tx.send(P2PMessage::new(tok1, String::from("Foo").into_bytes()));
 
     th.join().unwrap();
     th2.join().unwrap();
