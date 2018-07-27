887d44ff178a62273770087345bb343d342ba69f
Martin Karlsen Jensen <mkj@concordium.com>
2018-07-27 14:01:16 +0200

    Added connect and send_message RPC calls that work

--- concordium-node/src/main.rs
+++ concordium-node/src/main.rs
@@ -9,6 +9,7 @@ extern crate env_logger;
 extern crate bytes;
 extern crate mio;
 use p2p_client::configuration;
+use p2p_client::common::P2PNodeId;
 use tarpc::sync::{client, server};
 use tarpc::sync::client::ClientExt;
 use tarpc::util::{FirstSocketAddr, Never};
@@ -17,6 +18,9 @@ use std::thread;
 use p2p_client::p2p::*;
 use p2p_client::common::{NetworkRequest,NetworkPacket,NetworkMessage};
 use mio::Events;
+use std::cell::RefCell;
+use std::sync::Arc;
+use std::time::Duration;
 //use p2p_client::ffi;
 
 service! {
@@ -26,7 +30,62 @@ service! {
 }
 
 #[derive(Clone)]
-struct HelloServer;
+struct HelloServer {
+    node: RefCell<P2PNode>,
+}
+
+impl HelloServer {
+    pub fn new() -> Self {
+        let conf = configuration::parse_config();
+
+        let listen_port = match conf.listen_port {
+            Some(x) => x,
+            _ => 8888,
+        };
+
+        info!("Debuging enabled {}", conf.debug);
+
+        let (pkt_in,pkt_out) = mpsc::channel();
+
+        let _guard_pkt = thread::spawn(move|| {
+            loop {
+                if let Ok(msg) = pkt_out.recv() {
+                    match msg {
+                        NetworkMessage::NetworkPacket(NetworkPacket::DirectMessage(_,_, msg),_,_) => info!( "DirectMessage with text {} received", msg),
+                        NetworkMessage::NetworkPacket(NetworkPacket::BroadcastedMessage(_,msg),_,_) => info!("BroadcastedMessage with text {} received", msg),
+                        NetworkMessage::NetworkRequest(NetworkRequest::BanNode(_, x),_,_)  => info!("Ban node request for {:x}", x.get_id()),
+                        NetworkMessage::NetworkRequest(NetworkRequest::UnbanNode(_, x), _, _) => info!("Unban node requets for {:x}", x.get_id()), 
+                        _ => {}
+                    }
+                }
+            }
+        });
+
+        let mut node = if conf.debug {
+            let (sender, receiver) = mpsc::channel();
+            let _guard = thread::spawn(move|| {
+                loop {
+                    if let Ok(msg) = receiver.recv() {
+                        match msg {
+                            P2PEvent::ConnectEvent(ip, port) => info!("Received connection from {}:{}", ip, port),
+                            P2PEvent::DisconnectEvent(msg) => info!("Received disconnect for {}", msg),
+                            P2PEvent::ReceivedMessageEvent(node_id) => info!("Received message from {:?}", node_id),
+                            P2PEvent::SentMessageEvent(node_id) => info!("Sent message to {:?}", node_id),
+                            P2PEvent::InitiatingConnection(ip,port) => info!("Initiating connection to {}:{}", ip, port),
+                        }
+                    }
+                }
+            });
+            P2PNode::new(conf.id, listen_port, pkt_in, Some(sender))
+        } else {
+            P2PNode::new(conf.id, listen_port, pkt_in, None)
+        };
+
+        HelloServer {
+            node: RefCell::new(node),
+        }
+    }
+}
 
 impl SyncService for HelloServer {
     fn hello(&self, name: String) -> Result<String, Never> {
@@ -35,73 +94,38 @@ impl SyncService for HelloServer {
 
     fn peer_connect(&self, ip: String, port: u16) -> Result<bool, Never> {
         info!("Connecting to IP: {} and port: {}!", ip, port);
+        self.node.borrow_mut().connect(ip.parse().unwrap(), port);
         Ok(true)
     }
 
     fn send_message(&self, id: Option<String>, msg: String, broadcast: bool) -> Result<bool, Never> {
-        info!("Sending message to ID: {} with contents: {}. Broadcast? {}", id, msg, broadcast);
+        info!("Sending message to ID: {:?} with contents: {}. Broadcast? {}", id, msg, broadcast);
+        let id = match id {
+            Some(x) => Some(P2PNodeId::from_string(x)),
+            None => None,
+        };
+
+        self.node.borrow_mut().send_message(id, msg, broadcast);
         Ok(true)
     }
 }
 
 fn main() {
     let conf = configuration::parse_config();
-    //println!("I'm your new coin! Network enabled: {}, should connect to {}", conf.network, conf.remote_ip.unwrap_or(String::from("nothing")));
+    info!("I'm your new coin! Network enabled: {}, should connect to {}", conf.network, conf.remote_ip.unwrap_or(String::from("nothing")));
     //println!("calling lib in c {} version found is {}", ffi::is_present(), ffi::version());
 
     env_logger::init();
     info!("Starting up!");
 
-    let conf = configuration::parse_config();
-
-    let listen_port = match conf.listen_port {
-        Some(x) => x,
-        _ => 8888,
-    };
-
-    info!("Debuging enabled {}", conf.debug);
-
-    let (pkt_in,pkt_out) = mpsc::channel();
-
-    let _guard_pkt = thread::spawn(move|| {
-        loop {
-            if let Ok(msg) = pkt_out.recv() {
-                match msg {
-                    NetworkMessage::NetworkPacket(NetworkPacket::DirectMessage(_,_, msg),_,_) => info!( "DirectMessage with text {} received", msg),
-                    NetworkMessage::NetworkPacket(NetworkPacket::BroadcastedMessage(_,msg),_,_) => info!("BroadcastedMessage with text {} received", msg),
-                    NetworkMessage::NetworkRequest(NetworkRequest::BanNode(_, x),_,_)  => info!("Ban node request for {:x}", x.get_id()),
-                    NetworkMessage::NetworkRequest(NetworkRequest::UnbanNode(_, x), _, _) => info!("Unban node requets for {:x}", x.get_id()), 
-                    _ => {}
-                }
-            }
-        }
-    });
-
-    let mut node = if conf.debug {
-        let (sender, receiver) = mpsc::channel();
-        let _guard = thread::spawn(move|| {
-            loop {
-                if let Ok(msg) = receiver.recv() {
-                    match msg {
-                        P2PEvent::ConnectEvent(ip, port) => info!("Received connection from {}:{}", ip, port),
-                        P2PEvent::DisconnectEvent(msg) => info!("Received disconnect for {}", msg),
-                        P2PEvent::ReceivedMessageEvent(node_id) => info!("Received message from {:?}", node_id),
-                        P2PEvent::SentMessageEvent(node_id) => info!("Sent message to {:?}", node_id),
-                        P2PEvent::InitiatingConnection(ip,port) => info!("Initiating connection to {}:{}", ip, port),
-                    }
-                }
-            }
-        });
-        P2PNode::new(conf.id, listen_port, pkt_in, Some(sender))
-    } else {
-        P2PNode::new(conf.id, listen_port, pkt_in, None)
-    };
-
+    
     let mut events = Events::with_capacity(1024);
 
     let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let mut handle = match HelloServer.listen("localhost:0", server::Options::default()) {
+    let serv = HelloServer::new();
+    let th1 = thread::spawn(move || {
+        let th2 = serv.node.borrow_mut().spawn();
+        let mut handle = match serv.listen("localhost:0", server::Options::default()) {
             Ok(x) => x,
             Err(e) => panic!("Couldn't start RPC service!"),
         };
@@ -112,9 +136,16 @@ fn main() {
         };
 
         handle.run();
+        th2.join();
     });
+
+
 
     let client = SyncClient::connect(rx.recv().unwrap(), client::Options::default()).unwrap();
     client.peer_connect("127.0.0.1".to_string(), 8888).unwrap();
-    println!("{}", true);
+    thread::sleep(Duration::from_secs(5));
+
+    client.send_message(Some("c19cd000746763871fae95fcdd4508dfd8bf725f9767be68c3038df183527bb2".to_string()), "Hello World!".to_string(), false).unwrap();
+
+    th1.join().unwrap();
 }
