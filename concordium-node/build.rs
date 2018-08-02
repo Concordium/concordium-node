9afd7dccdc0d877ff7725878e6643530fc14174d
Ian Johannesen <ij@concordium.com>
2018-08-02 21:44:23 +0200

    fixes

--- concordium-node/build.rs
+++ concordium-node/build.rs
@@ -2,11 +2,11 @@ extern crate walkdir;
 extern crate protoc_grpcio;
 
 fn main() {
-    let proto_root = "src/proto";
+    let proto_root = format!("{}/src/proto", env!("CARGO_MANIFEST_DIR"));
     println!("cargo:rerun-if-changed={}", proto_root);
     protoc_grpcio::compile_grpc_protos(
         &["concordium_p2p_rpc.proto"],
-        &[proto_root],
+        &[proto_root.clone()],
         &proto_root
     ).expect("Failed to compile gRPC definitions!");
 }
