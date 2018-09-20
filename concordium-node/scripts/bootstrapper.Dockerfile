c01bdbf20e91e5edee241a7f2430083edea129a0
Martin Karlsen Jensen <mkj@concordium.com>
2018-09-20 17:11:31 +0200

    Changed all arguments to be provided by Kubernetes

--- concordium-node/scripts/bootstrapper.Dockerfile
+++ concordium-node/scripts/bootstrapper.Dockerfile
@@ -1,10 +1,5 @@
 FROM concordium/build:latest
 EXPOSE 8888
-ENV MAX_NODES="10000"
-ENV EXTERNAL_PORT="8888"
-ENV EXTRA_ARGS="--no-trust-bans"
-ENV PROMETHEUS_PUSH_GW="prometheus-pushgateway.prometheus.svc.cluster.local:9091"
-ENV PROMETHEUS_PUSH_JOBNAME="bootstrapper_push"
-ENTRYPOINT ./start-bootstrapper.sh --listen-port 8888 --max-nodes ${MAX_NODES} --external-port ${EXTERNAL_PORT} --prometheus-push-gateway ${PROMETHEUS_PUSH_GW} --prometheus-job-name ${PROMETHEUS_PUSH_JOBNAME} ${EXTRA_ARGS}
+ENTRYPOINT ./start-bootstrapper.sh 
 
 
--- concordium-node/scripts/start-bootstrapper.sh
+++ concordium-node/scripts/start-bootstrapper.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./target/release/p2p_bootstrapper-cli --external-ip $EIP --id $NODE_ID "$@" 
+
+# Create dirs
+mkdir -p $CONFIG_DIR
+mkdir -p $DATA_DIR
+
+./target/release/p2p_bootstrapper-cli --listen-port $LISTEN_PORT --external-ip $EIP --external-port $EXTERNAL_PORT --id $NODE_ID --max-nodes $MAX_NODES --prometheus-server $PROMETHEUS_METRICS_SERVER --prometheus-listen-port $PROMETHEUS_METRICS_PORT --prometheus-listen-addr $PROMETHEUS_METRICS_IP --override-config-dir $CONFIG_DIR --override-data-dir $DATA_DIR $EXTRA_ARGS
