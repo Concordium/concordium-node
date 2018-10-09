af28d4afb8f8407d9f4ee8f4307dc06b7711150b
Martin Karlsen Jensen <mkj@concordium.com>
2018-10-09 17:55:55 +0200

    Fixed argument passed to start binary

--- concordium-node/scripts/start-testrunner.sh
+++ concordium-node/scripts/start-testrunner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./target/release/testrunner --listen-port $LISTEN_PORT --listen-http-port $LISTEN_HTTP_PORT --bootstrap-server $BOOTSTRAP_FIRST_NODE --bootstrap-server $BOOTSTRAP_SECOND_NODE $EXTRA_ARGS 
+./target/release/testrunner --listen-port $LISTEN_PORT --listen-http-port $LISTEN_HTTP_PORT --bootstrap-node $BOOTSTRAP_FIRST_NODE --bootstrap-node $BOOTSTRAP_SECOND_NODE $EXTRA_ARGS 
