453b23a97816dc719ec8f50383965ad65decf39c
Martin Karlsen Jensen <mkj@concordium.com>
2018-10-22 15:44:40 +0200

    Fixed typo

--- concordium-node/scripts/start-node.sh
+++ concordium-node/scripts/start-node.sh
@@ -6,5 +6,5 @@ mkdir -p $DATA_DIR
 
 cd $DATA_DIR
 
-/build-project/target/release/p2p_client-cli --listen-port $LISTEN_PORT --desired-nodes $DESIRED_PEERS --prometheus-server $PROMETHEUS_METRICS_SERVER --prometheus-listen-port $PROMETHEUS_METRICS_PORT --prometheus-listen-addr $PROMETHEUS_METRICS_IP --override-config-dir $CONFIG_DIR --override-data-dir $DATA_DIR --bootstrap-node $BOOTSTRAP_FIRST_NODE --bootstrap-node $BOOTSTRAP_SECOND_NODE $EXTRA_ARGS
+heaptrack /build-project/target/release/p2p_client-cli --listen-port $LISTEN_PORT --desired-nodes $DESIRED_PEERS --prometheus-server $PROMETHEUS_METRICS_SERVER --prometheus-listen-port $PROMETHEUS_METRICS_PORT --prometheus-listen-addr $PROMETHEUS_METRICS_IP --override-config-dir $CONFIG_DIR --override-data-dir $DATA_DIR --bootstrap-node $BOOTSTRAP_FIRST_NODE --bootstrap-node $BOOTSTRAP_SECOND_NODE $EXTRA_ARGS
 
