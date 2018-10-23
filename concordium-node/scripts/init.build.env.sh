74b9cf607210b4e49dc4fc323627f81045783b17
Martin Karlsen Jensen <mkj@concordium.com>
2018-10-23 15:31:43 +0200

    Changed compile of heaptrack to use multiple cores and to build p2p-client with debug

--- concordium-node/scripts/init.build.env.sh
+++ concordium-node/scripts/init.build.env.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mitls/hacl-c
 rm -rf hacl-c
 
 git clone https://github.com/KDE/heaptrack.git
-(cd heaptrack && patch src/track/heaptrack.sh.cmake ../scripts/include-date-in-name.patch && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=release .. && make && make install);
+(cd heaptrack && patch src/track/heaptrack.sh.cmake ../scripts/include-date-in-name.patch && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=release .. && make -j$(nproc) && make install);
 rm -rf heaptrack
 
 ldconfig
