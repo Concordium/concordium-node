c7c991c06388c145a288f7f5a9d3fb924aa9a565
Martin Karlsen Jensen <mkj@concordium.com>
2018-10-23 13:26:36 +0200

    Added build of heaptrack where date is included in name

--- concordium-node/scripts/init.build.env.sh
+++ concordium-node/scripts/init.build.env.sh
@@ -3,6 +3,10 @@ git clone https://github.com/mitls/hacl-c
 ( cd hacl-c && make && cp libhacl.so /usr/lib );
 rm -rf hacl-c
 
+git clone https://github.com/KDE/heaptrack.git
+(cd heaptrack && patch src/track/heaptrack.sh.cmake ../scripts/include-date-in-name.patch && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=release .. && make && make install);
+rm -rf heaptrack
+
 ldconfig
 rustup default nightly
 
