335fdf302fbe6f7a3d24f908156886c0ea6d06ad
Ian Johannesen <ij@concordium.com>
2018-10-26 13:12:12 +0200

    more local testing

--- concordium-node/scripts/local-start-node.sh
+++ concordium-node/scripts/local-start-node.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/bin/heaptrack ./target/release/p2p_client-cli --desired-nodes $DESIRED_PEERS --external-port $EXTERNAL_PORT --private-node --bootstrap-node $BOOTSTRAP_NODE $EXTRA_ARGS
+# Haskell binding needs proper library path to function
+export LD_LIBRARY_PATH=/usr/local/lib:$HOME/.stack/programs/x86_64-linux/ghc-tinfo6-8.4.3/lib/ghc-8.4.3/rts
+/usr/bin/heaptrack ./target/release/p2p_client-cli --desired-nodes $DESIRED_PEERS --external-port $EXTERNAL_PORT --private-node --enable-baker --bootstrap-node $BOOTSTRAP_NODE $EXTRA_ARGS
--- concordium-node/scripts/local.testrunner.Dockerfile
+++ concordium-node/scripts/local.testrunner.Dockerfile
@@ -1,11 +1,12 @@
 FROM concordium/build:latest
 WORKDIR /build-project
-COPY ./scripts/local-start-node.sh ./start-node.sh
-RUN chmod +x ./start-node.sh
+COPY ./scripts/local-start-testrunner.sh ./start-testrunner.sh
+RUN chmod +x ./start-testrunner.sh
 EXPOSE 10000
 EXPOSE 8888
+EXPOSE 8950
 ENV EXTRA_ARGS="--debug"
 ENV DESIRED_PEERS="20"
 ENV EXTERNAL_PORT="8889"
 ENV BOOTSTRAP_NODE="127.0.0.1:8888"
-ENTRYPOINT ./start-node.sh
+ENTRYPOINT ./start-testrunner.sh
