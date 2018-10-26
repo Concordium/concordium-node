81f7df4f526e0240d5fa4b2ae36df699fcfb5283
Ian Johannesen <ij@concordium.com>
2018-10-26 13:44:51 +0200

    Cleanup of consensus-sys unused stuff and revised env vars for CI build config

--- concordium-node/consensus-sys/src/consensus.rs
+++ concordium-node/consensus-sys/src/consensus.rs
@@ -1,8 +1,6 @@
 use curryrs::types::I32;
-use std::ptr;
 use libc::c_char;
 use libc::c_int;
-use std::{thread,time};
 
 #[repr(C)] pub struct baker_runner { private: [u8; 0] }
 
@@ -21,22 +19,4 @@ extern {
     pub fn receiveBlock(baker : *mut baker_runner, block_data : *const u8, data_length : i64);
     pub fn receiveTransaction(baker : *mut baker_runner, n0 : u64, n1 : u64, n2 : u64, n3 : u64, transaction_data : *const u8, data_length : i64);
     pub fn stopBaker(baker : *mut baker_runner);
-}
-
-extern "C" fn add_one(x : I32) -> I32 {
-    println!("add_one called with {}", x);
-    x + 1
-}
-
-extern "C" fn wrap_print_cstring(string : *const u8) {
-    unsafe {printCString(string)};
-}
-
-extern "C" fn my_print_cstring(string : *const u8) {
-//    println!("{}", String::from(string));
-}
-
-extern "C" fn on_block_baked(block_data : *const u8, data_length : i64) {
-    println!("Baked a block of length {}", data_length);
-    unsafe {printBlock(block_data, data_length); }
 }
