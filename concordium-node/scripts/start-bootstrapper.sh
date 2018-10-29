e750bfd732b17cea6722afb4c400c97234a5a991
Martin Karlsen Jensen <mkj@concordium.com>
2018-10-29 10:27:52 +0100

    Changed other binaries to use debug version

--- concordium-node/scripts/start-bootstrapper.sh
+++ concordium-node/scripts/start-bootstrapper.sh
@@ -4,4 +4,6 @@
 mkdir -p $CONFIG_DIR
 mkdir -p $DATA_DIR
 
-./target/release/p2p_bootstrapper-cli --listen-port $LISTEN_PORT --external-ip $EIP --external-port $EXTERNAL_PORT --id $NODE_ID --max-nodes $MAX_NODES --prometheus-server $PROMETHEUS_METRICS_SERVER --prometheus-listen-port $PROMETHEUS_METRICS_PORT --prometheus-listen-addr $PROMETHEUS_METRICS_IP --override-config-dir $CONFIG_DIR --override-data-dir $DATA_DIR $EXTRA_ARGS
+cd $DATA_DIR
+
+heaptrack /build-project/target/debug/p2p_bootstrapper-cli --listen-port $LISTEN_PORT --external-ip $EIP --external-port $EXTERNAL_PORT --id $NODE_ID --max-nodes $MAX_NODES --prometheus-server $PROMETHEUS_METRICS_SERVER --prometheus-listen-port $PROMETHEUS_METRICS_PORT --prometheus-listen-addr $PROMETHEUS_METRICS_IP --override-config-dir $CONFIG_DIR --override-data-dir $DATA_DIR $EXTRA_ARGS
