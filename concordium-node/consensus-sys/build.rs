2745f758137608184d7997acf2270dee2c6c0705
Ian Johannesen <ij@concordium.com>
2018-11-15 18:34:14 +0100

    Link with 8.4.4 as well

--- concordium-node/consensus-sys/build.rs
+++ concordium-node/consensus-sys/build.rs
@@ -9,6 +9,7 @@ fn main() {
 fn main() {
     // Copy ouput .so file to library path
     println!("cargo:rustc-link-search=native={}/../.stack/programs/x86_64-linux/ghc-tinfo6-8.4.3/lib/ghc-8.4.3/rts", env!("CARGO_HOME"));
+    println!("cargo:rustc-link-search=native={}/../.stack/programs/x86_64-linux/ghc-tinfo6-8.4.4/lib/ghc-8.4.4/rts", env!("CARGO_HOME"));
     println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=dylib=HSConcordium-0.1.0.0");
 }
