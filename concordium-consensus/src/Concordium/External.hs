b55459c6482da15688ab272bfc5d7ac5e8adc0f7
Aleš Bizjak <ab@concordium.com>
2018-11-25 16:08:27 +0100

    Add `makeGenesisData` to exported functions and modify the `startBaker` interface.

--- concordium-consensus/src/Concordium/External.hs
+++ concordium-consensus/src/Concordium/External.hs
@@ -24,69 +24,97 @@ type I32 = Int32
 
 -- Test functions
 
-triple :: I32 -> I32
-triple x = 3 * x
+-- triple :: I32 -> I32
+-- triple x = 3 * x
 
-foreign export ccall triple :: I32 -> I32
+-- foreign export ccall triple :: I32 -> I32
 
-foreign import ccall "dynamic" mkCallback :: FunPtr (I32 -> IO I32) -> I32 -> IO I32
+-- foreign import ccall "dynamic" mkCallback :: FunPtr (I32 -> IO I32) -> I32 -> IO I32
 
-callbackTwice :: FunPtr (I32 -> IO I32) -> IO I32
-callbackTwice fun = do
-    let f = mkCallback fun
-    n <- f 0
-    f n
+-- callbackTwice :: FunPtr (I32 -> IO I32) -> IO I32
+-- callbackTwice fun = do
+--     let f = mkCallback fun
+--     n <- f 0
+--     f n
 
 
-foreign export ccall callbackTwice :: FunPtr (I32 -> IO I32) -> IO I32
+-- foreign export ccall callbackTwice :: FunPtr (I32 -> IO I32) -> IO I32
 
-printCString :: CString -> IO ()
-printCString cs = do
-    s <- peekCString cs
-    putStrLn s
+-- printCString :: CString -> IO ()
+-- printCString cs = do
+--     s <- peekCString cs
+--     putStrLn s
 
-foreign export ccall printCString :: CString -> IO ()
+-- foreign export ccall printCString :: CString -> IO ()
 
-foreign import ccall "dynamic" mkCStringCallback :: FunPtr (CString -> IO ()) -> CString -> IO ()
+-- foreign import ccall "dynamic" mkCStringCallback :: FunPtr (CString -> IO ()) -> CString -> IO ()
 
-callbackWithCString :: FunPtr (CString -> IO ()) -> IO ()
-callbackWithCString cb =
-    withCString "Hello cruel world!" (mkCStringCallback cb)
+-- callbackWithCString :: FunPtr (CString -> IO ()) -> IO ()
+-- callbackWithCString cb =
+--     withCString "Hello cruel world!" (mkCStringCallback cb)
 
-foreign export ccall callbackWithCString :: FunPtr (CString -> IO ()) -> IO ()
-
+-- foreign export ccall callbackWithCString :: FunPtr (CString -> IO ()) -> IO ()
 
 data BakerRunner = BakerRunner {
     bakerInChan :: Chan InMessage,
     bakerOutChan :: Chan OutMessage
 }
 
-makeBakers :: LotteryPower -> [(BakerInfo, BakerIdentity)]
-makeBakers lot = mbs (mkStdGen 17) 1
+makeBakerIdentity :: BakerId -> BakerIdentity
+makeBakerIdentity bid = mbs (mkStdGen 17)
     where
-        mbs gen bid = (BakerInfo epk spk lot, BakerIdentity bid ssk esk) : mbs gen'' (bid + 1)
+        mbs gen = BakerIdentity bid ssk esk
             where
                 (VRF.KeyPair esk epk, gen') = random gen
                 (Sig.KeyPair ssk spk, gen'') = random gen'
 
-makeStartupData ::
-    Timestamp   -- ^Genesis time
-    -> Word64   -- ^Number of bakers
-    -> Word64   -- ^Index of desired baker (0 <= _ < number of bakers)
-    -> (GenesisData, BakerIdentity)
-makeStartupData genTime nBakers bIndex = (genData, bid)
+makeBakerInfos :: Word64 -> [BakerInfo]
+makeBakerInfos nBakers = take (fromIntegral nBakers) $ mbs (mkStdGen 17)
     where
-        bakeShare = (1.0 / (fromIntegral nBakers))
-        bakers = take (fromIntegral nBakers) $ zip [1..] (makeBakers bakeShare)
+        mbs gen = BakerInfo epk spk lot:mbs gen''
+            where
+                (VRF.KeyPair esk epk, gen') = random gen
+                (Sig.KeyPair ssk spk, gen'') = random gen'
+        lot = 1.0 / fromIntegral nBakers
+
+type CStringCallback = CString -> Int64 -> IO ()
+foreign import ccall "dynamic" callCStringCallback :: FunPtr CStringCallback -> CStringCallback
+
+makeGenesisData :: 
+    Timestamp -- ^Genesis time
+    -> Word64 -- ^Number of bakers
+    -> FunPtr CStringCallback -- ^Function to process the generated genesis data.
+    -> IO ()
+makeGenesisData genTime nBakers cbk = do
+    BS.useAsCStringLen (encode genData) $ \(cdata, clen) -> callCStringCallback cbk cdata (fromIntegral clen)
+    where
         bps = BirkParameters (BS.pack "LeadershipElectionNonce")
-            0.5 -- Voting power
-            (Map.fromList [(i,b) | (i, (b, _)) <- bakers])
+                             0.5 -- voting power
+                             (Map.fromList $ zip [0..] (makeBakerInfos nBakers))
         fps = FinalizationParameters Map.empty
         genData = GenesisData genTime
-                10 -- Slot time in seconds
-                bps
-                fps
-        bid = snd $ snd (bakers !! (fromIntegral bIndex))
+                              10 -- slot time in seconds
+                              bps
+                              fps 
+
+-- makeStartupData ::
+--     Timestamp   -- ^Genesis time
+--     -> Word64   -- ^Number of bakers
+--     -> Word64   -- ^Index of desired baker (0 <= _ < number of bakers)
+--     -> (GenesisData, BakerIdentity)
+-- makeStartupData genTime nBakers bIndex = (genData, bid)
+--     where
+--         bakeShare = (1.0 / (fromIntegral nBakers))
+--         bakers = take (fromIntegral nBakers) $ zip [0..] (makeBakers bakeShare)
+--         bps = BirkParameters (BS.pack "LeadershipElectionNonce")
+--             0.5 -- Voting power
+--             (Map.fromList [(i,b) | (i, (b, _)) <- bakers])
+--         fps = FinalizationParameters Map.empty
+--         genData = GenesisData genTime
+--                 10 -- Slot time in seconds
+--                 bps
+--                 fps
+--         bid = snd $ snd (bakers !! (fromIntegral bIndex))
 
 type BlockCallback = CString -> Int64 -> IO ()
 
@@ -99,12 +127,18 @@ outLoop chan cbk = do
     BS.useAsCStringLen bbs $ \(cstr, l) -> cbk cstr (fromIntegral l)
     outLoop chan cbk
 
-startBaker :: Timestamp -> Word64 -> Word64 -> FunPtr BlockCallback -> IO (StablePtr BakerRunner)
-startBaker genTime nBakers bIndex bcbk = do
-    let (genData, bid) = makeStartupData genTime nBakers bIndex
-    (cin, cout) <- makeRunner bid genData
-    forkIO $ outLoop cout (callBlockCallback bcbk)
-    newStablePtr (BakerRunner cin cout)
+startBaker :: Word64 -- ^Baker id
+           -> CString -> Int64 -- ^Serialized genesis data (c string + len)
+           -> FunPtr BlockCallback -> IO (StablePtr BakerRunner)
+startBaker bIndex gdataC gdataLenC bcbk = do
+    let bid = makeBakerIdentity bIndex
+    gdata <- BS.packCStringLen (gdataC, fromIntegral gdataLenC)
+    case decode gdata of
+      Left err -> ioError (userError err)
+      Right genData -> do
+        (cin, cout) <- makeRunner bid genData
+        forkIO $ outLoop cout (callBlockCallback bcbk)
+        newStablePtr (BakerRunner cin cout)
 
 stopBaker :: StablePtr BakerRunner -> IO ()
 stopBaker bptr = do
@@ -133,7 +167,8 @@ receiveTransaction bptr n0 n1 n2 n3 tdata tlen = do
     tbs <- BS.packCStringLen (tdata, fromIntegral tlen)
     writeChan cin $ MsgTransactionReceived (Transaction (TransactionNonce n0 n1 n2 n3) tbs)
 
-foreign export ccall startBaker :: Timestamp -> Word64 -> Word64 -> FunPtr BlockCallback -> IO (StablePtr BakerRunner)
+foreign export ccall makeGenesisData :: Timestamp -> Word64 -> FunPtr CStringCallback -> IO ()
+foreign export ccall startBaker :: Word64 -> CString -> Int64 -> FunPtr BlockCallback -> IO (StablePtr BakerRunner)
 foreign export ccall stopBaker :: StablePtr BakerRunner -> IO ()
 foreign export ccall receiveBlock :: StablePtr BakerRunner -> CString -> Int64 -> IO ()
 foreign export ccall printBlock :: CString -> Int64 -> IO ()
