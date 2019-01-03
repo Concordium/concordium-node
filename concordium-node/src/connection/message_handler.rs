ba5d3e3bfa95647d2254a00ee4cb0f4feb2ece9f
Francisco Miguel Garcia <mg@concordium.com>
2019-01-03 11:50:59 +0100

    Add Fn & FnMut to ParseHandler

--- concordium-node/src/connection/message_handler.rs
+++ concordium-node/src/connection/message_handler.rs
@@ -1,5 +1,4 @@
 use network::{ NetworkMessage, NetworkRequest, NetworkResponse, NetworkPacket };
-use common::{ P2PPeer, P2PNodeId };
 
 macro_rules! run_callbacks{
     ($handlers:expr, $message:expr, $errorMsg: expr) => {
@@ -25,7 +24,7 @@ pub struct ParseHandler<T> {
 }
 
 impl<T> ParseHandler<T> {
-    pub fn new( error_msg: &String ) -> Self {
+    pub fn new( error_msg: String ) -> Self {
         ParseHandler::<T> {
             error_msg: error_msg.clone(),
             callbacks: Vec::new() 
@@ -47,6 +46,24 @@ impl<T> FnOnce<(&T,)> for ParseHandler<T> {
     }
 }
 
+impl<T> FnMut<(&T,)> for ParseHandler<T> {
+    extern "rust-call" fn call_mut(&mut self, args: (&T,)) -> ParseCallbackResult
+    {
+        let msg: &T = args.0;
+        run_callbacks!( &self.callbacks, msg, &self.error_msg)
+    }
+}
+
+impl<T> Fn<(&T,)> for ParseHandler<T> {
+    extern "rust-call" fn call(&self, args: (&T,)) -> ParseCallbackResult
+    {
+        let msg: &T = args.0;
+        run_callbacks!( &self.callbacks, msg, &self.error_msg)
+    }
+}
+
+
+
 
 #[derive(Clone)]
 pub struct MessageHandler {
@@ -67,6 +84,23 @@ impl MessageHandler {
             packet_parser: packet_parser
         }
     }
+
+    fn process_message(&self, msg: &NetworkMessage) -> ParseCallbackResult {
+        match msg {
+            NetworkMessage::NetworkRequest(ref nr, _, _) => {
+                (&self.request_parser)( nr)
+            },
+            NetworkMessage::NetworkResponse(ref nr, _, _) => {
+                (&self.response_parser)( nr)
+            },
+            NetworkMessage::NetworkPacket(ref np, _, _) => {
+                (&self.packet_parser)( np)
+            }
+            NetworkMessage::UnknownMessage | NetworkMessage::InvalidMessage => {
+                Ok(())
+            }
+        }
+    }
 }
 
 impl FnOnce<(&NetworkMessage,)> for MessageHandler {
@@ -75,24 +109,26 @@ impl FnOnce<(&NetworkMessage,)> for MessageHandler {
     extern "rust-call" fn call_once(self, args: (&NetworkMessage,)) -> ParseCallbackResult
     {
         let msg: &NetworkMessage = args.0;
-        let status :ParseCallbackResult = match msg {
-            NetworkMessage::NetworkRequest(ref nr, _, _) => {
-                (self.request_parser)( nr)
-            },
-            NetworkMessage::NetworkResponse(ref nr, _, _) => {
-                (self.response_parser)( nr)
-            },
-            NetworkMessage::NetworkPacket(ref np, _, _) => {
-                (self.packet_parser)( np)
-            }
-            NetworkMessage::UnknownMessage | NetworkMessage::InvalidMessage => {
-                Ok(())
-            }
-        };
-
-        status
+        self.process_message( msg)
     }
 }
+
+impl FnMut<(&NetworkMessage,)> for MessageHandler {
+    extern "rust-call" fn call_mut(&mut self, args: (&NetworkMessage,)) -> ParseCallbackResult
+    {
+        let msg: &NetworkMessage = args.0;
+        self.process_message( msg)
+    }
+}
+
+impl Fn<(&NetworkMessage,)> for MessageHandler {
+    extern "rust-call" fn call(&self, args: (&NetworkMessage,)) -> ParseCallbackResult
+    {
+        let msg: &NetworkMessage = args.0;
+        self.process_message( msg)
+    }
+}
+
 
 /*
 struct PacketHandlerDirect(P2PPeer, String, P2PNodeId, u16, Vec<u8>);
@@ -164,12 +200,12 @@ mod tests {
     }
 
     /// Handler function for `NetworkRequest` elements that does nothing.
-    fn network_request_handler_1( nr: &NetworkRequest) -> ParseCallbackResult {
+    fn network_request_handler_1( _nr: &NetworkRequest) -> ParseCallbackResult {
         Ok(())
     }
 
     /// Handler function for `NetworkRequest` elements. It only increases its counter.
-    fn network_request_handler_2( nr: &NetworkRequest) -> ParseCallbackResult {
+    fn network_request_handler_2( _nr: &NetworkRequest) -> ParseCallbackResult {
         NETWORK_REQUEST_COUNTER.fetch_add( 1, Ordering::SeqCst);
         Ok(())
     }
@@ -177,29 +213,25 @@ mod tests {
     /// Creates message handler for testing.
     fn make_message_handler() -> MessageHandler {
 
-        let req_handlers = vec![
-            ParseHandler<NetworkRequest>::new().add_callback( network_request_handler_1),
-            ParseHandler<NetworkRequest>::new().add_callback( network_request_handler_2),
-            ParseHandler<NetworkRequest>::new().add_callback( 
-                |x: &NetworkRequest| { 
+        let req_handler = ParseHandler::<NetworkRequest>::new( "Network Request Handler 1 error".to_string())
+                .add_callback( network_request_handler_1)
+                .add_callback( network_request_handler_2)
+                .add_callback( |_x: &NetworkRequest| { 
                     println!("Network Request {}", NETWORK_REQUEST_COUNTER.load(Ordering::Relaxed)); 
                     Ok(())
-            })
-        ];
-        let response_handlers = vec![
-            ParseHandler<NetworkResponse>::new().add_callback( |x: &NetworkResponse| { 
-                NETWORK_RESPONSE_COUNTER.fetch_add( 1, Ordering::SeqCst); 
-                Ok(())
-            })
-        ];
-        let packet_handlers = vec![
-            ParseHandler<NetworkPacket>::new().add_callback( |p: &NetworkPacketEnum| {
-                NETWORK_PACKET_COUNTER.fetch_add( 1, Ordering::SeqCst);
-                Ok(p)
-            }),
-        ];
+            });
+        let response_handler = ParseHandler::<NetworkResponse>::new( "Network Response Handler 1".to_string())
+                .add_callback( |_x: &NetworkResponse| { 
+                    NETWORK_RESPONSE_COUNTER.fetch_add( 1, Ordering::SeqCst); 
+                    Ok(())
+            });
+        let packet_handler = ParseHandler::<NetworkPacketEnum>::new( "Network Packet Handler 1".to_string())
+                .add_callback( |_p: &NetworkPacketEnum| {
+                    NETWORK_PACKET_COUNTER.fetch_add( 1, Ordering::SeqCst);
+                    Ok(())
+            });
         
-        MessageHandler::new( req_handlers, response_handlers, packet_handlers)
+        MessageHandler::new( req_handler, response_handler, packet_handler)
     }
 
 
@@ -207,14 +239,19 @@ mod tests {
     pub fn on_network_request_handler() {
         let mh: MessageHandler = make_message_handler();
 
-        for test_data in on_network_request_handler_data() {
-            let( message, exp_result) = test_data;
-            let status = mh.call( &message);
-            // assert_eq!( status, exp_result); 
+        for message in on_network_request_handler_data() {
+            let status = (&mh)( &message);
         }
 
         assert_eq!( NETWORK_REQUEST_COUNTER.load(Ordering::Relaxed), 2);
         assert_eq!( NETWORK_RESPONSE_COUNTER.load(Ordering::Relaxed), 1);
+
+        for message in on_network_request_handler_data() {
+            let status = (&mh)( &message);
+        }
+        
+        assert_eq!( NETWORK_REQUEST_COUNTER.load(Ordering::Relaxed), 4);
+        assert_eq!( NETWORK_RESPONSE_COUNTER.load(Ordering::Relaxed), 2);
     }
 }
 
