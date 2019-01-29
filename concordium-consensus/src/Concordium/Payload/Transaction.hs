07a4028eef3875c6670f592decca48731e4a23bd
Aleš Bizjak <ab@concordium.com>
2019-01-29 16:08:16 +0100

    Optimizations when calling the interpreter.

--- concordium-consensus/src/Concordium/Payload/Transaction.hs
+++ concordium-consensus/src/Concordium/Payload/Transaction.hs
@@ -101,38 +101,42 @@ encodeTransaction = encode
 messageToMsg Increment = I.Msg . fromString $ "Increment"
 messageToMsg Decrement = I.Msg . fromString $ "Decrement"
 
-tryTransaction :: MonadIO m => GlobalState -> Transaction -> m (Either String (Event, GlobalState))
-tryTransaction gs@(GlobalState {instances = instances}) (Transaction _ meta payload) =
+-- tryTransaction :: MonadIO m => GlobalState -> Transaction -> m (Either String (Event, GlobalState))
+tryTransaction ctx gs@(GlobalState {instances = instances}) (Transaction _ meta payload) =
   let Metadata {sender = sender} = meta
       Update addr msg = payload
       cstate = Map.lookup addr instances
   in case cstate of
        Nothing -> return . Left $ "Non-existent contract instance: " ++ show addr
        Just addrState -> do
-         () <- liftIO (takeMVar lock)
-         (newState, txres) <- liftIO $ I.callContract undefined I.Address (Just (messageToMsg msg)) addrState undefined I.Caller
-         liftIO (putMVar lock ())
+         (newState, txres) <- liftIO $ I.callContract ctx undefined I.Address (Just (messageToMsg msg)) addrState undefined I.Caller
          case txres of
            I.TxReject -> return . Right $ (Rejected, gs)
            I.TxAccept -> return . Right $ (Updated addr msg, GlobalState (Map.insert addr newState instances))
 
 executeBlock :: MonadIO m => GlobalState -> [Transaction] -> m (Either String ([Event], GlobalState))
 executeBlock gs msgs = do
+  () <- liftIO (takeMVar lock)
+  ctx <- liftIO $ I.makeContext 
   r <- foldM (\case (Left s) -> \_ -> return $ Left s
                     (Right (es, gs')) -> \msg -> do
-                      res <- tryTransaction gs' msg
+                      res <- tryTransaction ctx gs' msg
                       return $ res <&> (\(e, gs'') -> (e:es, gs'')))
        (Right ([], gs)) msgs
+  liftIO (putMVar lock ())
   return $ r <&> (\(es, gs) -> (reverse es, gs))
 
 makeBlock :: (MonadIO m) => GlobalState -> [Transaction] -> m ([(Transaction, Event)], [Failure String Transaction], GlobalState)
 makeBlock gs msgs = do
+  () <- liftIO (takeMVar lock)
+  ctx <- liftIO $ I.makeContext
   (sucs, fails, gs') <- foldM (\(suc, fails, gs') msg -> do
-                                  res <- tryTransaction gs' msg
+                                  res <- tryTransaction ctx gs' msg
                                   case res of
                                     Left err -> return (suc, Failure err msg : fails, gs')
                                     Right (e, gs'') -> return ((msg, e) : suc, fails, gs''))
                         ([], [], gs) msgs
+  liftIO (putMVar lock ())
   return (reverse sucs, reverse fails, gs')
 
 mkState :: Int -> I.State
