8602e309bb6208b9d53abc3fde0964944b0e68db
Ian Johannesen <ij@concordium.com>
2019-02-01 16:30:29 +0100

    Moved to consensus branch oak-integration, and adapted for new features currently available for PoC

--- concordium-node/consensus-sys/build.rs
+++ concordium-node/consensus-sys/build.rs
@@ -12,6 +12,8 @@ fn main() {
             println!("cargo:rustc-link-search=native={}/../.stack/programs/x86_64-linux/ghc-tinfo6-8.4.4/lib/ghc-8.4.4/rts", env!("CARGO_HOME"));
             println!("cargo:rustc-link-search=native=/usr/local/lib");
             println!("cargo:rustc-link-lib=dylib=HSConcordium-0.1.0.0");
+            println!("cargo:rustc-link-lib=dylib=HSlanguage-glsl-0.3.0");
+            println!("cargo:rustc-link-lib=dylib=HSoak-0.19.0");
         }
     }
 }
