59d0429b04b5e747d889bbfd43e078bb89b574c8
Martin Karlsen Jensen <mkj@concordium.com>
2019-02-04 12:28:09 +0000

    Update build-local.sh

--- concordium-node/scripts/build-local.sh
+++ concordium-node/scripts/build-local.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Foreign temporary dependency, potentially a smarter way?
+rm -rf consensus
+git clone git@gitlab.com:Concordium/consensus/prototype.git consensus
+# Locked to oak-integration for PoC due to build issues on Windows with master currently.
+( cd consensus && git checkout oak-integration && ./setup-env.sh )
+
+
 docker build -f scripts/base.Dockerfile -t concordium/base:latest .
 docker build -f scripts/build.Dockerfile -t concordium/build:latest .
 docker build -f scripts/local.testbootstrapper.Dockerfile -t concordium/test/bootstrapper:latest .
