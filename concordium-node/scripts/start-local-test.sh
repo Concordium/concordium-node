63065f856bfa3e3595f2e8c38ea6255e8a5cf59e
Martin Karlsen Jensen <mkj@concordium.com>
2019-02-04 17:02:20 +0100

    Added --baker-id to local node test

--- concordium-node/scripts/start-local-test.sh
+++ concordium-node/scripts/start-local-test.sh
@@ -6,7 +6,7 @@ sleep 5
 for i in `seq 1 $NODE_COUNT`;
 do
     PORT=$((8889+$i))
-    docker run -d --name=nodetest$i -p $PORT:8888 -e "EXTERNAL_PORT=$PORT" -e "BOOTSTRAP_NODE=$EXTERNAL_IP:8888" concordium/test/node:latest 
+    docker run -d --name=nodetest$i -p $PORT:8888 -e "EXTERNAL_PORT=$PORT" -e "BOOTSTRAP_NODE=$EXTERNAL_IP:8888" -e "BAKER_ID=$i" concordium/test/node:latest 
 done
 TESTRUNNER_PORT=$((8889+$NODE_COUNT+1))
 docker run -d --name=testrunner -p $TESTRUNNER_PORT:8888 -e "EXTERNAL_PORT=$TESTRUNNER_PORT" -e "BOOTSTRAP_NODE=$EXTERNAL_IP:8888" concordium/test/testrunner:latest
