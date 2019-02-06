50b3136be8a093a914d2b91fb424d529694d2c72
Martin Karlsen Jensen <mkj@concordium.com>
2019-02-06 13:11:45 +0100

    Added --num-bakers argument and fixed --baker-id argument

--- concordium-node/scripts/local-start-testrunner.sh
+++ concordium-node/scripts/local-start-testrunner.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
+# Haskell binding needs proper library path to function
+export LD_LIBRARY_PATH=/usr/local/lib:$HOME/.stack/programs/x86_64-linux/ghc-tinfo6-8.4.3/lib/ghc-8.4.3/rts
 ./target/debug/testrunner --private-node --desired-nodes $DESIRED_PEERS --external-port $EXTERNAL_PORT --bootstrap-node $BOOTSTRAP_NODE $EXTRA_ARGS 
