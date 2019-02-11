60f309d8754b68beb252a2ed225d4c7e7d6698ea
Aleš Bizjak <ab@concordium.com>
2019-02-11 10:38:37 +0100

    Update reference to oak-compiler.

--- concordium-consensus/setup-env.sh
+++ concordium-consensus/setup-env.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ( 
   git clone git@gitlab.com:Concordium/oak/oak-compiler.git oak-compiler &&
-  cd oak-compiler &&
-  git checkout f80840f171ffce63b85b1744f87490b9eb8eb70a )
+  cd oak-compiler )
