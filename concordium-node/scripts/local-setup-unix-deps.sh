cfd0ff66a11f505ef16b03f084b73d8543095384
Nadim Kobeissi <nadim@nadim.computer>
2019-04-04 13:28:30 +0000

    Update local-setup-unix-deps.sh to add macOS support (hopefully)

--- concordium-node/scripts/local-setup-unix-deps.sh
+++ concordium-node/scripts/local-setup-unix-deps.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
-sudo rm -f /usr/local/lib/libHSConcordium-*.so
-sudo rm -f /usr/local/lib/libHSlanguage-glsl-*.so
-sudo rm -f /usr/local/lib/libHSoak-*.so
-sudo rm -f /usr/local/lib/libHSmonadplus-*.so
-sudo rm -f /usr/local/lib/libHSacorn-*.so
-sudo rm -f /usr/local/lib/libHSconcordium-crypto-*.so
-sudo rm -f /usr/local/lib/libHSglobalstate-*.so
-sudo rm -f /usr/local/lib/libconcordium-crypto.so*
-sudo rm -f /usr/local/lib/libec_vrf_ed25519.so
-sudo rm -f /usr/local/lib/libeddsa_ed25519.so
-sudo rm -f /usr/local/lib/libsha_2.so
+#!/bin/bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	LIBEXTENSION="$LIBEXTENSION"
+else
+	LIBEXTENSION="so"
+fi
+sudo rm -f /usr/local/lib/libHSConcordium-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSlanguage-glsl-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSoak-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSmonadplus-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSacorn-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSconcordium-crypto-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libHSglobalstate-*.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libconcordium-crypto.$LIBEXTENSION*
+sudo rm -f /usr/local/lib/libec_vrf_ed25519.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libeddsa_ed25519.$LIBEXTENSION
+sudo rm -f /usr/local/lib/libsha_2.$LIBEXTENSION
 
 if [ -d ~/.stack/global-project/ ]; then
     if [ -f ~/.stack/global-project/stack.yaml ]; then
@@ -23,18 +28,18 @@ cp scripts/stack.yaml ~/.stack/global-project/stack.yaml
 
 ( cd deps/internal/crypto/rust-src &&
   LD_LIBRARY_PATH=/usr/local/lib cargo build --release &&
-  sudo cp target/release/libec_vrf_ed25519.so /usr/local/lib &&
-  sudo cp target/release/libeddsa_ed25519.so /usr/local/lib &&
-  sudo cp target/release/libsha_2.so /usr/local/lib &&
+  sudo cp target/release/libec_vrf_ed25519.$LIBEXTENSION /usr/local/lib &&
+  sudo cp target/release/libeddsa_ed25519.$LIBEXTENSION /usr/local/lib &&
+  sudo cp target/release/libsha_2.$LIBEXTENSION /usr/local/lib &&
   rm -rf target/ )
 
 ( cd deps/internal/consensus &&
   LD_LIBRARY_PATH=/usr/local/lib stack build --ghc-options '-dynamic' --force-dirty &&
-  sudo cp .stack-work/install/x86_64-linux-tinfo6/$(cat stack.yaml | grep ^resolver: | awk '{ print $NF }')/8.4.4/lib/x86_64-linux-ghc-8.4.4/libHS*.so /usr/local/lib &&
-  sudo find /usr/local/lib -name libHSConcordium\*.so -exec ln -s {} /usr/local/lib/libHSConcordium-0.1.0.0.so \; &&
-  sudo find /usr/local/lib -name libHSacorn\*.so -exec ln -s {} /usr/local/lib/libHSacorn-0.1.0.0.so \; &&
-  sudo find /usr/local/lib -name libHSconcordium-crypto\*.so -exec ln -s {} /usr/local/lib/libHSconcordium-crypto-0.1.so \; &&
-  sudo find /usr/local/lib -name libHSglobalstate-\*.so -exec ln -s {} /usr/local/lib/libHSglobalstate-0.1.so \; &&
+  sudo cp .stack-work/install/x86_64-linux-tinfo6/$(cat stack.yaml | grep ^resolver: | awk '{ print $NF }')/8.4.4/lib/x86_64-linux-ghc-8.4.4/libHS*.$LIBEXTENSION /usr/local/lib &&
+  sudo find /usr/local/lib -name libHSConcordium\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSConcordium-0.1.0.0.$LIBEXTENSION \; &&
+  sudo find /usr/local/lib -name libHSacorn\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSacorn-0.1.0.0.$LIBEXTENSION \; &&
+  sudo find /usr/local/lib -name libHSconcordium-crypto\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSconcordium-crypto-0.1.$LIBEXTENSION \; &&
+  sudo find /usr/local/lib -name libHSglobalstate-\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSglobalstate-0.1.$LIBEXTENSION \; &&
   rm -rf .stack 
   ) 
 
