5a72bdbfdaede169d79f1913b7e75f5d040a56c2
Aleš Bizjak <ab@concordium.com>
2019-04-10 10:01:11 +0200

    Move GlobalState from Scheduler, add package.yaml file.

--- concordium-consensus/src/Concordium/GlobalState.hs
+++ concordium-consensus/src/Concordium/GlobalState.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
 module Concordium.GlobalState where
 
+import Data.HashMap.Strict(HashMap)
+import qualified Data.HashMap.Strict as HM
+
+import qualified Concordium.GlobalState.Acorn.Core as Core
+import Concordium.GlobalState.Acorn.Interfaces
+import Concordium.GlobalState.Execution
 import Concordium.GlobalState.Types
 import Concordium.GlobalState.Account
 import Concordium.GlobalState.Transactions
@@ -12,3 +19,21 @@ data TreeState = TreeState {
 
 data BestBlockState
 
+
+-- | Current global state used by the scheduler.
+data GlobalState = GlobalState 
+    {instances :: !(HashMap ContractAddress Instance)  -- ^Contract instances.
+    ,accounts :: !(HashMap AccountAddress Account)      -- ^FIXME: Unclear what the distinction between accounts/instances is.
+    ,modules :: !(HashMap Core.ModuleRef (Interface, ValueInterface)) -- ^Interfaces of deployed modules.
+    }
+
+emptyGlobalState :: GlobalState
+emptyGlobalState = GlobalState {
+  instances = HM.empty
+  , accounts = HM.empty
+  , modules = HM.empty
+  }
+
+firstFreeContract :: GlobalState -> ContractAddress
+firstFreeContract (GlobalState{..}) = 
+  ContractAddress (fromIntegral (length instances)) 0 
