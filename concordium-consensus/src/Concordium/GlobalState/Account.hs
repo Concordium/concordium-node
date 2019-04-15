f7b523c751a4adae425f4d8e9f68836a59463c7e
Aleš Bizjak <ab@concordium.com>
2019-04-15 16:58:03 +0200

    Add Account lenses.

--- concordium-consensus/src/Concordium/GlobalState/Account.hs
+++ concordium-consensus/src/Concordium/GlobalState/Account.hs
@@ -21,19 +21,20 @@ emptyAccounts :: Accounts
 emptyAccounts = Accounts Map.empty Empty
 
 putAccount :: Account -> Accounts -> Accounts
-putAccount acct Accounts{..} = case Map.lookup addr accountMap of
-        Nothing -> let (i, newAccountTable) = AT.append acct accountTable in Accounts (Map.insert addr i accountMap) newAccountTable
-        Just i -> Accounts accountMap (accountTable & ix i .~ acct)
-    where
-        addr = accountAddress acct
+putAccount acct Accounts{..} =
+  case Map.lookup addr accountMap of
+    Nothing -> let (i, newAccountTable) = AT.append acct accountTable in Accounts (Map.insert addr i accountMap) newAccountTable
+    Just i -> Accounts accountMap (accountTable & ix i .~ acct)
+
+  where addr = acct ^. accountAddress
 
 exists :: AccountAddress -> Accounts -> Bool
 exists addr Accounts{..} = Map.member addr accountMap
 
 getAccount :: AccountAddress -> Accounts -> Maybe Account
 getAccount addr Accounts{..} = case Map.lookup addr accountMap of
-                             Nothing -> Nothing
-                             Just i -> accountTable ^? ix i
+                                 Nothing -> Nothing
+                                 Just i -> accountTable ^? ix i
 
 unsafeGetAccount :: AccountAddress -> Accounts -> Account
 unsafeGetAccount addr Accounts{..} = case Map.lookup addr accountMap of
