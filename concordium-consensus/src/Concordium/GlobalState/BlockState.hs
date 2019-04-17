b98b0d8a6f05a6a6452228e6cfae10b0c67bd59d
Aleš Bizjak <ab@concordium.com>
2019-04-17 09:05:36 +0200

    Lenses for blockstate to simplify the scheduler.

--- concordium-consensus/src/Concordium/GlobalState/BlockState.hs
+++ concordium-consensus/src/Concordium/GlobalState/BlockState.hs
@@ -1,19 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Concordium.GlobalState.BlockState where
 
 import Concordium.GlobalState.Account
 import Concordium.GlobalState.Instances
 import Concordium.GlobalState.Modules
 
+import Lens.Micro.Platform
+
 data BlockState = BlockState {
-    blockAccounts :: Accounts,
-    blockInstances :: Instances,
-    blockModules :: Modules
+    _blockAccounts :: Accounts,
+    _blockInstances :: Instances,
+    _blockModules :: Modules
 }
+
+makeLenses ''BlockState
 
 emptyBlockState :: BlockState
 emptyBlockState = BlockState {
-  blockAccounts = emptyAccounts
-  , blockInstances = emptyInstances
-  , blockModules = emptyModules
+  _blockAccounts = emptyAccounts
+  , _blockInstances = emptyInstances
+  , _blockModules = emptyModules
   }
 
