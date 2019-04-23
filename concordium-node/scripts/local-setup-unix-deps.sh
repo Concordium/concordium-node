76c5d6dbc8e0737e4b97a6cb96eb523f3c97221a
Martin Karlsen Jensen <mkj@concordium.com>
2019-04-23 13:34:24 +0200

    Fixed local setup script and changed crypto library to be built in debug mode

--- concordium-node/scripts/local-setup-unix-deps.sh
+++ concordium-node/scripts/local-setup-unix-deps.sh
@@ -26,13 +26,13 @@ else
     mkdir -p ~/.stack/global-project
 fi
 
-cp scripts/stack.yaml ~/.stack/global-project/stack.yaml
+echo -e "packages: []\nresolver: $(cat deps/internal/consensus/stack.yaml | grep ^resolver: | awk '{ print $NF }')" > ~/.stack/global-project/stack.yaml
 
 ( cd deps/internal/crypto/rust-src &&
-  LD_LIBRARY_PATH=/usr/local/lib cargo build --release &&
-  sudo cp target/release/libec_vrf_ed25519.$LIBEXTENSION /usr/local/lib &&
-  sudo cp target/release/libeddsa_ed25519.$LIBEXTENSION /usr/local/lib &&
-  sudo cp target/release/libsha_2.$LIBEXTENSION /usr/local/lib &&
+  LD_LIBRARY_PATH=/usr/local/lib cargo build &&
+  sudo cp target/debug/libec_vrf_ed25519.$LIBEXTENSION /usr/local/lib &&
+  sudo cp target/debug/libeddsa_ed25519.$LIBEXTENSION /usr/local/lib &&
+  sudo cp target/debug/libsha_2.$LIBEXTENSION /usr/local/lib &&
   rm -rf target/ )
 
 sudo ldconfig
@@ -42,8 +42,8 @@ sudo ldconfig
   sudo cp .stack-work/install/x86_64-$PLATFORM-tinfo6/$(cat stack.yaml | grep ^resolver: | awk '{ print $NF }')/8.4.4/lib/x86_64-$PLATFORM-ghc-8.4.4/libHS*.$LIBEXTENSION /usr/local/lib &&
   sudo find /usr/local/lib -name libHSConcordium\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSConcordium-0.1.0.0.$LIBEXTENSION \; &&
   sudo find /usr/local/lib -name libHSacorn\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSacorn-0.1.0.0.$LIBEXTENSION \; &&
-  sudo find /usr/local/lib -name libHSconcordium-crypto\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSconcordium-crypto-0.1.$LIBEXTENSION \; &&
   sudo find /usr/local/lib -name libHSglobalstate-\*.$LIBEXTENSION -exec ln -s {} /usr/local/lib/libHSglobalstate-0.1.$LIBEXTENSION \; &&
+  sudo ln -s /usr/local/lib/libHSconcordium-crypto-0.1*.$LIBEXTENSION /usr/local/lib/libHSconcordium-crypto-0.1.$LIBEXTENSION &&
   rm -rf .stack-work
   ) 
 
