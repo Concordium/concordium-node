ba6b49f567b5c547a9ec4ff28c0dd0769a13dbd2
Lukasz Jedrzejczyk <lj@concordium.com>
2019-05-06 14:43:45 +0200

    downgrade (de)serialization checks to debug

--- concordium-node/consensus-sys/src/lib.rs
+++ concordium-node/consensus-sys/src/lib.rs
@@ -24,13 +24,13 @@ macro_rules! check_serialization {
 
 macro_rules! debug_deserialization {
     ($target:expr, $bytes:expr) => {
-        info!("Deserializing an object: {} ({}B)", $target, $bytes.len());
+        debug!("Deserializing an object: {} ({}B)", $target, $bytes.len());
     };
 }
 
 macro_rules! debug_serialization {
     ($object:expr) => {
-        info!("Serializing an object: {:?}", $object);
+        debug!("Serializing an object: {:?}", $object);
     };
 }
 
