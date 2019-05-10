1a5e9aca5103240162d3462f969d28c0dd56eaf6
Lukasz Jedrzejczyk <lj@concordium.com>
2019-05-10 16:38:54 +0200

    add a block dump reader binary

---
+++ concordium-node/src/bin/block_dump_reader.rs
@@ -0,0 +1,31 @@
+use std::{
+    env,
+    fs::File,
+    io::{self, Read},
+};
+
+use concordium_consensus::{block::GenesisData, common::sha256};
+
+// for now it only reads genesis data, as only that is currently being dumped
+fn read_block_dump(bytes: &[u8]) {
+    let data = GenesisData::deserialize(bytes)
+        .expect("Can't serialize the provided data as a GenesisData object!");
+
+    println!("{:#?}\n\nshort hash: {:?}", data, sha256(bytes));
+}
+
+fn main() -> io::Result<()> {
+    let mut args = env::args();
+    let _ = args.next();
+    let filename = args
+        .next()
+        .expect("No arguments provided (expected a filename)!");
+    let mut file = File::open(filename)?;
+    let mut buffer = Vec::new();
+
+    file.read_to_end(&mut buffer)?;
+
+    read_block_dump(&buffer);
+
+    Ok(())
+}
