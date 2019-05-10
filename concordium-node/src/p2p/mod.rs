53b6f3f995096cfc8296c0ba48f912fb70d0ce7e
Javier Sagredo <js@concordium.com>
2019-05-10 09:39:35 +0000

    gRPC testing

--- concordium-node/src/p2p/mod.rs
+++ concordium-node/src/p2p/mod.rs
@@ -17,77 +17,16 @@ pub use self::p2p_node::P2PNode;
 mod tests {
     use crate::{
         common::{P2PNodeId, PeerType},
-        configuration::Config,
-        connection::P2PEvent,
-        network::NetworkMessage,
-        p2p::{banned_nodes::BannedNode, P2PNode},
+        p2p::banned_nodes::BannedNode,
+        test_utils::*,
     };
-    use concordium_common::functor::{FilterFunctor, Functorable};
     use failure::Fallible;
-    use std::{
-        str::FromStr,
-        sync::{
-            atomic::{AtomicUsize, Ordering},
-            mpsc, Arc,
-        },
-    };
-
-    static PORT_OFFSET: AtomicUsize = AtomicUsize::new(0);
-    static PORT_START_NODE: u16 = 8888;
-
-    fn next_port_offset_node(slot_size: usize) -> u16 {
-        PORT_OFFSET.fetch_add(slot_size, Ordering::SeqCst) as u16 + PORT_START_NODE
-    }
+    use std::str::FromStr;
 
     #[test]
     pub fn test_banned_functionalities() -> Fallible<()> {
-        let mut node = {
-            let node_type = PeerType::Node;
-            let id = "000000002dd2b6ed";
-            let (pkt_in, _pkt_out) = mpsc::channel::<Arc<NetworkMessage>>();
-
-            let (sender, receiver) = mpsc::channel();
-            let _guard = spawn_or_die!("Log loop", move || loop {
-                if let Ok(msg) = receiver.recv() {
-                    match msg {
-                        P2PEvent::ConnectEvent(addr) => info!("Received connection from {}", addr),
-                        P2PEvent::DisconnectEvent(msg) => info!("Received disconnect for {}", msg),
-                        P2PEvent::ReceivedMessageEvent(node_id) => {
-                            info!("Received message from {:?}", node_id)
-                        }
-                        P2PEvent::SentMessageEvent(node_id) => {
-                            info!("Sent message to {:?}", node_id)
-                        }
-                        P2PEvent::InitiatingConnection(addr) => {
-                            info!("Initiating connection to {}", addr)
-                        }
-                        P2PEvent::JoinedNetwork(peer, network_id) => {
-                            info!("Peer {} joined network {}", peer.id(), network_id);
-                        }
-                        P2PEvent::LeftNetwork(peer, network_id) => {
-                            info!("Peer {} left network {}", peer.id(), network_id);
-                        }
-                    }
-                }
-            });
-
-            let config = Config::new(
-                Some("127.0.0.1".to_owned()),
-                next_port_offset_node(1),
-                vec![],
-                100,
-            );
-
-            P2PNode::new(
-                Some(id.to_owned()),
-                &config,
-                pkt_in,
-                Some(sender),
-                node_type,
-                None,
-                Arc::new(FilterFunctor::new("Broadcasting_checks")),
-            )
-        };
+        let port = next_port_offset_node(1);
+        let (mut node, _) = make_node_and_sync(port, vec![100], false, PeerType::Node)?;
         // Empty on init
         let reply = node.get_banlist();
         assert!(reply.is_empty());
