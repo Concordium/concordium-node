4f70a298367ef5eaad227ccaed08c7588e17789f
Francisco Miguel Garcia <mg@concordium.com>
2019-05-10 09:48:56 +0200

    CR: Add Archive documentation

--- concordium-node/src/common/serialization/archive.rs
+++ concordium-node/src/common/serialization/archive.rs
@@ -5,6 +5,18 @@ use failure::{err_msg, Fallible};
 
 use std::{fmt::Display, net::IpAddr, str};
 
+/// It is the archive trait for serialization.
+///
+/// Archive is one of the key concept of serialization process.
+/// Its main target is to define 'How' types should be serialized. That means
+/// this class have to cover 'how' to serialize basic types, because composed
+/// types can be serialized by composition of simple types:
+/// This archive support the following types:
+///  - All unsigned integer types: u8, u16, u32, u64
+///  - Any type which can be converted to a `str` reference.
+///
+/// On the other hand, `Serializable` trait describes **What** have to be
+/// serialized in a composed data type like: any struct, Vec, etc.
 pub trait WriteArchive: Sized + std::io::Write {
     // Write
     fn write_u8(&mut self, data: u8) -> Fallible<()>;
@@ -20,6 +32,25 @@ pub trait WriteArchive: Sized + std::io::Write {
     }
 }
 
+/// It is the archive trait for deserialization.
+///
+/// Archive is one of the key concept of deserialization process.
+/// Its main target is to define the following:
+///  - 'How' types should be deserialized into memory object. Only native types
+///    because complex types are defined by composition of natives.
+///  - Set a deserialization 'context'. Sometimes, deserialization process
+///    requires more
+///  information than just load from data stream. Our case is a good example of
+/// that. We need some  remote peer information (P2PPeer and IpAddr), and that
+/// info is not stored in source streamed  data. That 'context' info is supplied
+/// by archive itself.
+///  - Any other optimization. For instance, `payload` function allow you to
+///    access directly to
+///  pending streamed data. It is extremely useful for messages that contains
+/// huge binary payloads  that would not need to be processed.
+///
+/// On the other hand, `Deserializable` trait describes **What** have to be
+/// deserialized in a composed data type like: any struct, Vec, etc.
 pub trait ReadArchive: Sized + std::io::Read {
     fn post_handshake_peer(&self) -> Fallible<P2PPeer> {
         self.remote_peer().clone().post_handshake_peer_or_else(|| {
@@ -38,9 +69,13 @@ pub trait ReadArchive: Sized + std::io::Read {
 
     fn read_n_bytes(&mut self, len: u32) -> Fallible<ContainerView>;
 
+    /// It gets a shadow-copy of pending streamed data.
     fn payload(&mut self, len: u64) -> Option<UCursor>;
 
     // Utilities for parsing.
+
+    /// It checks that streamed data is deserialized into `T` object and that is
+    /// equal to `tag`.
     fn tag<T>(&mut self, tag: T) -> Fallible<()>
     where
         T: Deserializable + PartialEq + Display, {
@@ -52,6 +87,7 @@ pub trait ReadArchive: Sized + std::io::Read {
         }
     }
 
+    /// It checks that streamed data is equal to `tag`.
     fn tag_slice(&mut self, tag: &[u8]) -> Fallible<()> {
         let vw = self.read_n_bytes(tag.len() as u32)?;
         if tag == vw.as_slice() {
