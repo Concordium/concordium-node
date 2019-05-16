a0559234de04c73a74ccdb6437901e5672d01533
Ian Johannesen <ij@concordium.com>
2019-05-16 10:39:19 +0200

    Refactored to using constants and added new binary

--- concordium-node/src/client/mod.rs
+++ concordium-node/src/client/mod.rs
@@ -1,3 +1,7 @@
 pub mod fails;
 mod seen_transmissions_list;
 pub mod utils;
+
+pub const FILE_NAME_GENESIS_DATA: &str = "genesis.dat";
+pub const FILE_NAME_PREFIX_BAKER_PRIVATE: &str = "baker_private_";
+pub const FILE_NAME_SUFFIX_BAKER_PRIVATE: &str = ".dat";
