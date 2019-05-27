1a66a34867908a975171316f4e14280a758edb06
Lukasz Jedrzejczyk <lj@concordium.com>
2019-05-27 15:47:05 +0200

    remove Skov statistics (Haskell-side)

--- concordium-node/concordium-global-state/src/tree.rs
+++ concordium-node/concordium-global-state/src/tree.rs
@@ -1,4 +1,4 @@
-use chrono::prelude::{DateTime, Utc};
+use chrono::prelude::Utc;
 use failure::{bail, Fallible};
 
 use concordium_common::{
@@ -76,29 +76,6 @@ pub enum BlockStatus {
     Alive,
     Dead,
     Finalized,
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Default)]
-struct ConsensusStatistics {
-    blocks_received:                 u64,
-    blocks_verified:                 u64,
-    block_last_received:             Option<DateTime<Utc>>,
-    block_receive_latency_ema:       f64,
-    block_receive_latency_ema_emvar: f64,
-    block_receive_period_ema:        Option<f64>,
-    block_receive_period_ema_emvar:  Option<f64>,
-    block_last_arrived:              Option<DateTime<Utc>>,
-    block_arrive_latency_ema:        f64,
-    block_arrive_latency_ema_emvar:  f64,
-    block_arrive_period_ema:         Option<f64>,
-    block_arrive_period_ema_emvar:   Option<f64>,
-    transactions_per_block_ema:      f64,
-    transactions_per_block_emvar:    f64,
-    finalization_count:              u64,
-    last_finalized_time:             Option<DateTime<Utc>>,
-    finalization_period_ema:         Option<f64>,
-    finalization_period_emvar:       Option<f64>,
 }
 
 #[derive(Debug, Default)]
