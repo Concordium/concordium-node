4c5831eeb6d860caf6401e0b4f757a6a8965b6c7
Ian Johannesen <ij@concordium.com>
2019-06-10 13:10:53 +0200

    nix shell support for the glorious nix users

---
+++ concordium-node/shell.nix
@@ -0,0 +1,31 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustStableChannel = (nixpkgs.rustChannelOf { date = "2019-05-22"; channel = "nightly";  }).rust.override {
+	extensions = [
+		"rust-src"
+		"rls-preview"
+		"clippy-preview"
+		"rustfmt-preview"
+	];
+  };
+in
+with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    hardeningDisable = [ "all" ];
+    buildInputs = [
+      rustStableChannel
+      openssl
+      protobuf
+      pkgconfig
+      unbound
+      numactl
+      gmp
+      cmake
+    ];
+    shellHook = ''
+        export OPENSSL_DIR="${openssl.dev}"
+        export OPENSSL_LIB_DIR="${openssl.out}/lib"
+    '';
+  }
