9190c7e0d9182fee104e5b7acefb402c215bcb55
Aleš Bizjak <ab@concordium.com>
2019-06-20 10:22:11 +0200

    Add new transactions types for adding, removing, updating bakers.

--- concordium-consensus/src/Concordium/GlobalState/Parameters.hs
+++ concordium-consensus/src/Concordium/GlobalState/Parameters.hs
@@ -6,29 +6,9 @@ module Concordium.GlobalState.Parameters where
 import GHC.Generics
 import qualified Data.Map as Map
 import Data.Word
-import Data.ByteString
 import Data.Serialize
 
-import qualified Concordium.Crypto.BlockSignature as Sig
-import qualified Concordium.Crypto.VRF as VRF
 import Concordium.Types
-
-type BakerId = Word64
-
-type LeadershipElectionNonce = ByteString
-type BakerSignVerifyKey = Sig.VerifyKey
-type BakerSignPrivateKey = Sig.KeyPair
-type BakerElectionVerifyKey = VRF.PublicKey
-type BakerElectionPrivateKey = VRF.KeyPair
-type LotteryPower = Double
-type ElectionDifficulty = Double
-
-type VoterId = Word64
-type VoterVerificationKey = Sig.VerifyKey
-type VoterVRFPublicKey = VRF.PublicKey
-type VoterSignKey = Sig.SignKey
-type VoterPower = Int
-
 
 data BakerInfo = BakerInfo {
     bakerElectionVerifyKey :: BakerElectionVerifyKey,
