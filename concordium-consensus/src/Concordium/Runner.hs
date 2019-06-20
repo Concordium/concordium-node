d3686e775ccadb1e3a96bd6987bedca96b4d4c6a
Thomas Dinsdale-Young <ty@concordium.com>
2019-06-20 17:45:02 +0200

    Remove server. Remove legacy runner.

--- concordium-consensus/src/Concordium/Runner.hs
+++ concordium-consensus/src/Concordium/Runner.hs
@@ -4,13 +4,9 @@ module Concordium.Runner where
 import Control.Concurrent.Chan
 import Control.Concurrent.MVar
 import Control.Concurrent
-import Control.Monad.State.Class
 import Control.Monad.Trans.State hiding (get)
-import Control.Monad.Writer.Class
 import Control.Monad
-import Control.Monad.IO.Class
 import Control.Exception
-import Data.IORef
 import Data.Monoid
 import Data.Time.Clock
 import Data.Either
@@ -29,85 +25,6 @@ import Concordium.Afgjort.Finalize
 import Concordium.Afgjort.Buffer
 import Concordium.Logger
 import Concordium.Getters
-
-data InMessage src =
-    MsgShutdown
-    | MsgTimer
-    | MsgBlockReceived src BakedBlock
-    | MsgTransactionReceived Transaction
-    | MsgFinalizationReceived src FinalizationMessage
-    | MsgFinalizationRecordReceived src FinalizationRecord
-
-data OutMessage src = 
-    MsgNewBlock BakedBlock
-    | MsgFinalization FinalizationMessage
-    | MsgFinalizationRecord FinalizationRecord
-    | MsgMissingBlock src BlockHash BlockHeight
-    | MsgMissingFinalization src (Either BlockHash FinalizationIndex)
-
-makeRunner :: forall m source. LogMethod IO -> BakerIdentity -> GenesisData -> BlockState (FSM m) -> IO (Chan (InMessage source), Chan (OutMessage source), IORef SkovFinalizationState)
-makeRunner logm bkr gen initBS = do
-        logm Runner LLInfo "Starting baker"
-        inChan <- newChan
-        outChan <- newChan
-        let
-            syncFinalizationInstance = FinalizationInstance (bakerSignKey bkr) (bakerElectionKey bkr)
-            sfs = initialSkovFinalizationState syncFinalizationInstance gen initBS
-        out <- newIORef sfs
-        _ <- forkIO $ runLoggerT (execFSM' (msgLoop inChan outChan out 0 MsgTimer) syncFinalizationInstance gen initBS) logm
-        return (inChan, outChan, out)
-    where
-        updateFinState :: IORef SkovFinalizationState -> FSM' LogIO ()
-        updateFinState out = get >>= liftIO . writeIORef out
-        msgLoop :: Chan (InMessage source) -> Chan (OutMessage source) -> IORef SkovFinalizationState -> Slot -> (InMessage source) -> FSM' LogIO ()
-        msgLoop _ _ _ _ MsgShutdown = return ()
-        msgLoop inChan outChan out lastBake MsgTimer = do
-            cs <- getCurrentSlot
-            handleMessagesNoSource outChan out $ when (cs > lastBake) $
-                bakeForSlot bkr cs >>= \case
-                    Nothing -> return ()
-                    Just block -> do
-                        updateFinState out
-                        liftIO $ writeChan outChan (MsgNewBlock block)
-            ns <- timeUntilNextSlot
-            _ <- liftIO $ forkIO $ do
-                threadDelay $ truncate (ns * 1e6)
-                writeChan inChan MsgTimer
-            (liftIO $ readChan inChan) >>= msgLoop inChan outChan out cs
-        msgLoop inChan outChan out lastBake (MsgBlockReceived src block) = do
-            _ <- handleMessages outChan out src $ storeBlock block
-            (liftIO $ readChan inChan) >>= msgLoop inChan outChan out lastBake
-        msgLoop inChan outChan out lastBake (MsgTransactionReceived trans) = do
-            _ <- handleMessagesNoSource outChan out $ receiveTransaction trans
-            (liftIO $ readChan inChan) >>= msgLoop inChan outChan out lastBake
-        msgLoop inChan outChan out lastBake (MsgFinalizationReceived src bs) = do
-            _ <- handleMessages outChan out src $ receiveFinalizationMessage bs
-            (liftIO $ readChan inChan) >>= msgLoop inChan outChan out lastBake
-        msgLoop inChan outChan out lastBake (MsgFinalizationRecordReceived src fr) = do
-            _ <- handleMessages outChan out src $ finalizeBlock fr
-            (liftIO $ readChan inChan) >>= msgLoop inChan outChan out lastBake    
-        handleMessages :: Chan (OutMessage source) -> IORef SkovFinalizationState -> source -> FSM' LogIO r -> FSM' LogIO r
-        handleMessages outChan out src a = censor (const (Endo id)) $ do
-            (r, Endo evs) <- listen a
-            updateFinState out
-            let
-                handleMessage (SkovFinalization (BroadcastFinalizationMessage fmsg)) = liftIO $ writeChan outChan (MsgFinalization fmsg)
-                handleMessage (SkovFinalization (BroadcastFinalizationRecord frec)) = liftIO $ writeChan outChan (MsgFinalizationRecord frec)
-                handleMessage (SkovMissingBlock bh delta) = liftIO $ writeChan outChan (MsgMissingBlock src bh delta)
-                handleMessage (SkovMissingFinalization fr) = liftIO $ writeChan outChan (MsgMissingFinalization src fr)
-            forM_ (evs []) handleMessage
-            return r
-        handleMessagesNoSource :: Chan (OutMessage source) -> IORef SkovFinalizationState -> FSM' LogIO r -> FSM' LogIO r
-        handleMessagesNoSource outChan out a = censor (const (Endo id)) $ do
-            (r, Endo evs) <- listen a
-            updateFinState out
-            let
-                handleMessage (SkovFinalization (BroadcastFinalizationMessage fmsg)) = liftIO $ writeChan outChan (MsgFinalization fmsg)
-                handleMessage (SkovFinalization (BroadcastFinalizationRecord frec)) = liftIO $ writeChan outChan (MsgFinalizationRecord frec)
-                handleMessage _ = return ()
-            forM_ (evs []) handleMessage
-            return r
-
 
 data SyncRunner = SyncRunner {
     syncFinalizationInstance :: FinalizationInstance,
@@ -204,6 +121,21 @@ syncReceiveFinalizationMessage syncRunner finMsg = runBFSMWithStateLog syncRunne
 syncReceiveFinalizationRecord :: SyncRunner -> FinalizationRecord -> IO (UpdateResult, [SkovFinalizationEvent])
 syncReceiveFinalizationRecord syncRunner finRec = runBFSMWithStateLog syncRunner (finalizeBlock finRec)
 
+
+data InMessage src =
+    MsgShutdown
+    | MsgBlockReceived src BakedBlock
+    | MsgTransactionReceived Transaction
+    | MsgFinalizationReceived src FinalizationMessage
+    | MsgFinalizationRecordReceived src FinalizationRecord
+
+data OutMessage src = 
+    MsgNewBlock BakedBlock
+    | MsgFinalization FinalizationMessage
+    | MsgFinalizationRecord FinalizationRecord
+    | MsgMissingBlock src BlockHash BlockHeight
+    | MsgMissingFinalization src (Either BlockHash FinalizationIndex)
+
 -- |This is provided as a compatibility wrapper for the test runners.
 makeAsyncRunner :: forall m source. LogMethod IO -> BakerIdentity -> GenesisData -> BlockState (BFSM m) -> IO (Chan (InMessage source), Chan (OutMessage source), MVar SkovBufferedFinalizationState)
 makeAsyncRunner logm bkr gen initBS = do
@@ -215,7 +147,6 @@ makeAsyncRunner logm bkr gen initBS = do
         let
             msgLoop = readChan inChan >>= \case
                 MsgShutdown -> stopSyncRunner sr
-                MsgTimer -> msgLoop -- TODO: Remove MsgTimer altogether
                 MsgBlockReceived src block -> do
                     (_, evts) <- syncReceiveBlock sr block
                     forM_ evts $ handleMessage src
