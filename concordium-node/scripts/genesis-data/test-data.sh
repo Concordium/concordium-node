b01b4f07d68a812123ef348327571fb2302a0fde
Ian Johannesen <ij@concordium.com>
2019-06-25 15:08:44 +0200

    err vs args test

--- concordium-node/scripts/genesis-data/test-data.sh
+++ concordium-node/scripts/genesis-data/test-data.sh
@@ -6,12 +6,12 @@ final_state=0
 
 for baker_size in "${baker_sizes[@]}" 
 do
-    echo "Testing genesis data with $baker_sizes baker(s)" &&
+    echo "Testing genesis data with $baker_size baker(s)" &&
     mkdir -p $cwd/$baker_size &&
     tar xzf $cwd/$baker_size-bakers.tar.gz -C $cwd/$baker_size &&
     cargo run --features=static --bin=read_block_dump \
-        $cwd/$baker_size/genesis_data/genesis.dat ; 
-    if [[ $@ != 0 ]]; then
+        $cwd/$baker_size/genesis_data/genesis.dat >/dev/null 
+    if [[ $? != 0 ]]; then
         echo "- failed"
         final_state=-1
     fi
