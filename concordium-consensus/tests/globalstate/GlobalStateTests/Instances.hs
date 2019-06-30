94dc62550569fab12065e0afb5d9ae1677f778c4
Aleš Bizjak <ab@concordium.com>
2019-06-30 21:23:07 +0200

    Revert "Add the ability to add annotations to acorn types, terms and patterns."
    The commit was accidentally pushed to master, and it needs review before it
    should be.
    
    This reverts commit 530c0a2e8a0b84f776ba1ccea1a2f46de5281c23.

--- concordium-consensus/tests/globalstate/GlobalStateTests/Instances.hs
+++ concordium-consensus/tests/globalstate/GlobalStateTests/Instances.hs
@@ -18,8 +18,6 @@ import Concordium.GlobalState.Instances
 
 import qualified Data.FixedByteString as FBS
 
-import Data.Void
-
 import Test.QuickCheck
 import Test.Hspec
 
@@ -75,7 +73,7 @@ makeDummyInstance (InstanceData model amount) =
         messageType = Core.TBase Core.TInt32
         owner = AccountAddress . FBS.pack . replicate 21 $ 0
 
-data InstanceData = InstanceData (Value Void) Amount
+data InstanceData = InstanceData Value Amount
     deriving (Eq, Show)
 
 instance Arbitrary InstanceData where
@@ -105,7 +103,7 @@ modelGetInstanceData (ContractAddress ci csi) m = do
         guard $ csi == csi'
         return idata
     
-modelUpdateInstanceAt :: ContractAddress -> Amount -> Value Void -> Model -> Model
+modelUpdateInstanceAt :: ContractAddress -> Amount -> Value -> Model -> Model
 modelUpdateInstanceAt (ContractAddress ci csi) amt val m = m {modelInstances = Map.adjust upd ci (modelInstances m)}
     where
         upd o@(csi', _)
