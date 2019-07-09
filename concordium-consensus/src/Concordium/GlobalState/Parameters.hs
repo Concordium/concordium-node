da656f8478ba326097388223fbdadc90f01fb5d5
Aleš Bizjak <ab@concordium.com>
2019-07-09 14:17:49 +0200

    Update globalstate to reflect changed credentials/accounts in the crypto dependency.
    This branch will only work with the id branch of the crypto library.

--- concordium-consensus/src/Concordium/GlobalState/Parameters.hs
+++ concordium-consensus/src/Concordium/GlobalState/Parameters.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE TemplateHaskell, RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 -- |This module defines types for blockchain parameters, including genesis data,
 -- baker parameters and finalization parameters.
 module Concordium.GlobalState.Parameters(
@@ -16,7 +17,28 @@ import Lens.Micro.Platform
 
 import Concordium.Types
 import Concordium.GlobalState.Bakers
+import Data.ByteString(ByteString)
 
+-- |FIXME: These should wrap pointers so that they can be passed to rust with no
+-- overhead of checking whether they are really valid elements.
+newtype ElgamalGenerator = ElgamalGenerator ByteString
+    deriving(Show, Serialize)
+newtype PedersenKey = PedersenKey ByteString
+    deriving(Show, Serialize)
+
+-- |Cryptographic parameters needed to verify on-chain proofs, e.g.,
+-- group parameters (generators), commitment keys, in the future also
+-- common reference strings, etc.
+data CryptographicParameters = CryptographicParameters {
+  -- |Generator of the group used for elgamal encryption, also serving as the
+  -- base of the discrete logarithm parameter in the dlog sigma protocol.
+  elgamalGenerator :: ElgamalGenerator,
+  -- |Commitment key used to construct pedersen commitments to individual
+  -- attributes of the attribute list.
+  attributeCommitmentKey :: PedersenKey
+} deriving (Show, Generic)
+
+instance Serialize CryptographicParameters where
 
 data BirkParameters = BirkParameters {
     _birkLeadershipElectionNonce :: LeadershipElectionNonce,
@@ -56,7 +78,8 @@ data GenesisData = GenesisData {
     genesisSlotDuration :: Duration,
     genesisBirkParameters :: BirkParameters,
     genesisBakerAccounts :: [Account],
-    genesisFinalizationParameters :: FinalizationParameters
+    genesisFinalizationParameters :: FinalizationParameters,
+    genesisCryptographicParameters :: CryptographicParameters
 } deriving (Generic, Show)
 
 instance Serialize GenesisData where
