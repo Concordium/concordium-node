46bb51c208e1709fca7fc8e64dfd8e7f57e1de38
Aleš Bizjak <ab@concordium.com>
2019-07-16 12:49:40 +0200

    Update identity providers.

--- concordium-consensus/src/Concordium/GlobalState/IdentityProviders.hs
+++ concordium-consensus/src/Concordium/GlobalState/IdentityProviders.hs
@@ -4,14 +4,37 @@ module Concordium.GlobalState.IdentityProviders where
 import Data.HashMap.Strict(HashMap)
 import qualified Data.HashMap.Strict as HM
 
-import Concordium.Types
 import Concordium.ID.Types
+import Concordium.Crypto.FFIDataTypes
 
 import qualified Data.Serialize as S
 
 import Data.Aeson
 import qualified Data.Text.Encoding as Text
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Base16 as BS16
+
+data AnonymityRevokerData = AnonymityRevokerData {
+  -- |Unique id of the anonymity revoker.
+  arIdentity :: !ARName,
+  -- |Public key of the anonymity revoker
+  arPublicKey :: !ElgamalPublicKey,
+  -- |Generator of the group the anonymity revoker uses
+  -- FIXME: This is really part of the public key but currently
+  -- this is not the abstraction in crypto.
+  arElgamalGenerator :: !ElgamalGen
+  } deriving(Show)
+
+instance S.Serialize AnonymityRevokerData where
+  put AnonymityRevokerData{..} =
+    S.put arIdentity <> S.put arPublicKey <> S.put arElgamalGenerator
+
+  get = do
+    arIdentity <- S.get
+    arPublicKey <- S.get
+    arElgamalGenerator <- S.get
+    return AnonymityRevokerData{..}
 
 -- |Data needed by the bakers to reward identity providers and keep track of any
 -- other necessary things.
@@ -24,7 +47,10 @@ data IdentityProviderData = IdentityProviderData {
   -- |Account of the identity provider. Each identity provider must designate an
   -- account to which it will receive rewards. This can change throughout the
   -- lifetime.
-  ipAccount :: !AccountAddress
+  -- FIXME: Commented out for POC.
+  -- ipAccount :: !AccountAddress,
+  -- |Associated anonymity revoker of this identity provider.
+  ipArInfo :: AnonymityRevokerData
   } deriving (Show)
 
 -- |The set of all identity providers. Identity providers are identified
@@ -44,28 +70,42 @@ emptyIdentityProviders = IdentityProviders HM.empty
 
 instance FromJSON IdentityProviderData where
   parseJSON = withObject "IdentityProviderData" $ \v ->
-    do ipIdText <- v .: "ipId"
-       ipVerifyKeyText <- v .: "ipVerifykey"
-       let ipIdbs = fst . BS16.decode . Text.encodeUtf8 $ ipIdText
-       let ipVerifyKeybs = fst . BS16.decode . Text.encodeUtf8 $ ipVerifyKeyText
-       ipAccountText <- v .: "ipAccount" -- assume base16 for now to simplify things
-       case S.decode (fst . BS16.decode . Text.encodeUtf8 $ ipAccountText) of
-         Left err -> fail $ "Cannot read account address: " ++ err
-         Right ipAccount ->
+    do ipIdbs <- Text.encodeUtf8 <$> (v .: "ipIdentity")
+       ipVerifyKeybs <- b16 <$> (v .: "ipVerifyKey")
+       arPublicKeybs <- b16 <$> (v .: "arPublicKey")
+       arName <- Text.encodeUtf8 <$> (v .: "arName")
+       arElgamalGeneratorbs <- b16 <$> (v .: "arElgamalGenerator")
+       -- we need to add length information to the deserializer for this field.
+       -- This is an unfortunate hack, but ...
+       let ipVerifyKey = S.decode (lenbs (ipVerifyKeybs))
+       let arPublicKey' = S.decode arPublicKeybs
+       let arElgamalGenerator' = S.decode arElgamalGeneratorbs
+       case (ipVerifyKey, arPublicKey', arElgamalGenerator') of
+         (Right iver, Right arPublicKey, Right arElgamalGenerator) -> 
            return IdentityProviderData{
            ipIdentity = IP_ID ipIdbs,
-           ipVerifyKey = IP_PK ipVerifyKeybs,
-           ..
+           ipVerifyKey = IP_PK iver,
+           ipArInfo = AnonymityRevokerData{
+               arIdentity = ARName arName,
+               ..
+               }
            }
+         _ -> fail "Could not decode keys."
+           
+         where b16 = fst . BS16.decode . Text.encodeUtf8
+               lenbs bs = S.runPut (S.putWord32be (fromIntegral (BS.length bs))) <> bs
 
 instance S.Serialize IdentityProviderData where
   put IdentityProviderData{..} =
     S.put ipIdentity <>
     S.put ipVerifyKey <>
-    S.put ipAccount
+    S.put ipArInfo
 
   get = do
     ipIdentity <- S.get
     ipVerifyKey <- S.get
-    ipAccount <- S.get
+    ipArInfo <- S.get
     return IdentityProviderData{..}
+
+test :: FilePath -> IO (Maybe IdentityProviderData)
+test f = decode <$> BSL.readFile f
