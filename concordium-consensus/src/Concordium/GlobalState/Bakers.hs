585780f06f737f1e87ca5c8a878e5b4c2966c796
Thomas Dinsdale-Young <ty@concordium.com>
2019-07-17 14:28:44 +0200

    Genesis parameters; bakers mapped by keys.

--- concordium-consensus/src/Concordium/GlobalState/Bakers.hs
+++ concordium-consensus/src/Concordium/GlobalState/Bakers.hs
@@ -29,6 +29,8 @@ makeLenses ''BakerInfo
 data Bakers = Bakers {
     -- |The bakers, indexed by 'BakerId'
     _bakerMap :: !(Map BakerId BakerInfo),
+    -- |The baker ids indexed by the keys
+    _bakersByKey :: !(Map (BakerSignVerifyKey, BakerElectionVerifyKey) [BakerId]),
     -- |The total stake delegated to all bakers
     _bakerTotalStake :: !Amount,
     -- |The next 'BakerId' to use for a new baker.
@@ -40,11 +42,24 @@ instance Serialize Bakers
 makeLenses ''Bakers
 
 emptyBakers :: Bakers
-emptyBakers = Bakers Map.empty 0 0
+emptyBakers = Bakers Map.empty Map.empty 0 0
+
+bakersFromList :: [BakerInfo] -> Bakers
+bakersFromList bkrs = Bakers {
+        _bakerMap = Map.fromList $ ibkrs,
+        _bakersByKey = foldr (\(k,v) -> Map.insertWith (++) k v) Map.empty [((_bakerSignatureVerifyKey, _bakerElectionVerifyKey), [id]) | (id, BakerInfo{..}) <- ibkrs],
+        _bakerTotalStake = sum $ _bakerStake <$> bkrs,
+        _nextBakerId = fromIntegral $ length bkrs
+    }
+    where
+        ibkrs = zip [0..] bkrs
 
 createBaker :: BakerCreationInfo -> Bakers -> (BakerId, Bakers)
 createBaker (BakerCreationInfo _bakerElectionVerifyKey _bakerSignatureVerifyKey _bakerAccount) bkrs =
-        (bid, bkrs & bakerMap . at bid ?~ BakerInfo{..} & nextBakerId .~ bid + 1)
+        (bid, bkrs 
+                & bakerMap . at bid ?~ BakerInfo{..}
+                & bakersByKey . at (_bakerSignatureVerifyKey, _bakerElectionVerifyKey) . non [] %~ (bid :)
+                & nextBakerId .~ bid + 1)
     where
         _bakerStake = 0
         bid = _nextBakerId bkrs
@@ -72,7 +87,10 @@ removeBaker :: BakerId -> Bakers -> (Bool, Bakers)
 removeBaker bid !bakers =
     case bakers ^. bakerMap . at bid of
         Nothing -> (False, bakers)
-        Just bkr -> (True, bakers & (bakerMap . at bid .~ Nothing) . (bakerTotalStake %~ subtract (bkr ^. bakerStake)))
+        Just bkr -> (True, bakers
+                            & (bakerMap . at bid .~ Nothing)
+                            & bakersByKey . at (bkr ^. bakerSignatureVerifyKey, bkr ^. bakerElectionVerifyKey) . non [] %~ filter (/= bid)
+                            & (bakerTotalStake %~ subtract (bkr ^. bakerStake)))
 
 modifyStake :: Maybe BakerId -> AmountDelta -> Bakers -> Bakers
 modifyStake (Just bid) delta bakers = case bakers ^. bakerMap . at bid of
