e6c8def5909516143e25fecc984b04982b6a3ef3
Ian Johannesen <ij@concordium.com>
2019-07-19 11:51:21 +0200

    Revert to curl

--- concordium-node/scripts/download-static-libs.sh
+++ concordium-node/scripts/download-static-libs.sh
@@ -13,9 +13,12 @@ BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
   mkdir $TMPDIR
   (
     cd $TMPDIR
-    wget https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$VERSION_TAG.tar.gz
+    printf "Downloading static consensus version $VERSION_TAG\n"
+    curl -s https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$VERSION_TAG.tar.gz > static-consensus-$VERSION_TAG.tar.gz
+    printf "Expanding downloaded archive\n"
     tar -xf static-consensus-$VERSION_TAG.tar.gz
     cd target/
+    printf "Replacing local version with upstream\n"
     rm -rf $BASEDIR/../deps/static-libs/linux/*
     cp -r * $BASEDIR/../deps/static-libs/linux/
   )
