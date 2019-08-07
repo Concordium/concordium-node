47e5e6b71dcf2f823705a104534d761a0d38fcd5
Martin Karlsen Jensen <mkj@concordium.com>
2019-08-07 09:07:47 +0000

    Added genesis data for more network sizes

--- concordium-node/scripts/genesis-data/generate-data.sh
+++ concordium-node/scripts/genesis-data/generate-data.sh
@@ -4,8 +4,8 @@ cwd=$(dirname $0)
 . $cwd/config.sh
 final_state=0
 
-wget https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-binaries-8.6.5.tar.gz
-tar -xf static-consensus-binaries-8.6.5.tar.gz
+wget https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-binaries-9339bff59f29e6475fdab08c717ced3b7cf59ca6.tar.gz
+tar -xf static-consensus-binaries-9339bff59f29e6475fdab08c717ced3b7cf59ca6.tar.gz
 
 for baker_size in "${baker_sizes[@]}" 
 do
@@ -17,4 +17,4 @@ do
     rm -r genesis_data
 done
 
-rm -r static-consensus-binaries-8.6.5.tar.gz binaries
+rm -r static-consensus-binaries-9339bff59f29e6475fdab08c717ced3b7cf59ca6.tar.gz binaries
