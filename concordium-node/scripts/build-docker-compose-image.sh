c79b753a7b67a60bf25af3928ef94e8daa8c82b8
Martin Karlsen Jensen <mkj@concordium.com>
2019-08-12 13:06:35 +0200

    Updated docker compose setup to be better

---
+++ concordium-node/scripts/build-docker-compose-image.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -z "$JENKINS_HOME" ]; then
+    git clone -b master --single-branch git@gitlab.com:Concordium/tools/baker_id_gen.git baker_id_gen
+
+    CONSENSUS_VERSION=`git submodule | grep consensus | head -n1 | awk '{print $1}'`
+
+    echo $CONSENSUS_VERSION > CONSENSUS_VERSION
+
+    docker build -f scripts/dev-client.Dockerfile -t concordium/dev-client:latest .
+
+    rm -f CONSENSUS_VERSION
+    rm -rf baker_id_gen
+else
+    echo "This script should only be run from Jenkins."
+fi
