c02671b1bc8bca7de4061b2c12186c4a09e47b5b
Lukasz Jedrzejczyk <lj@concordium.com>
2019-08-19 14:05:21 +0200

    add HybridBuf methods

--- concordium-node/concordium-common/src/hybrid_buf.rs
+++ concordium-node/concordium-common/src/hybrid_buf.rs
@@ -1,8 +1,14 @@
+#[cfg(feature = "s11n_serde")]
+use serde::{
+    de::{self, Deserializer, Visitor},
+    ser::{self, SerializeStruct, Serializer},
+};
 use tempfile::tempfile;
 
 use std::{
+    cmp,
     fs::File,
-    io::{Cursor, Read, Result, Seek, SeekFrom, Write},
+    io::{self, Cursor, Read, Result, Seek, SeekFrom, Write},
     mem,
 };
 
@@ -11,6 +17,7 @@ const MAX_MEM_SIZE: usize = 4 * 1024 * 1024;
 
 type MemBuf = Cursor<Vec<u8>>;
 
+#[derive(Debug)]
 pub enum HybridBuf {
     Mem(MemBuf),
     File(File),
@@ -20,6 +27,14 @@ impl HybridBuf {
     pub fn new() -> Self { Default::default() }
 
     pub fn new_on_disk() -> Result<Self> { Ok(HybridBuf::File(tempfile()?)) }
+
+    pub fn with_capacity(cap: usize) -> Result<Self> {
+        if cap <= MAX_MEM_SIZE {
+            Ok(HybridBuf::Mem(Cursor::new(Vec::with_capacity(cap))))
+        } else {
+            Self::new_on_disk()
+        }
+    }
 
     pub fn len(&self) -> Result<u64> {
         match self {
@@ -46,6 +61,31 @@ impl HybridBuf {
             file_buf.write_all(cursor.get_ref())?;
             let _ = mem::replace(self, file_buf);
         }
+        Ok(())
+    }
+
+    pub fn into_vec(mut self) -> Result<Vec<u8>> {
+        if let Self::Mem(cursor) = self {
+            Ok(cursor.into_inner())
+        } else {
+            let size = cmp::max(self.len().unwrap_or(0) as usize, MAX_MEM_SIZE);
+            let mut ret = Vec::with_capacity(size);
+            self.read_to_end(&mut ret)?;
+            Ok(ret)
+        }
+    }
+
+    pub fn remaining_len(&mut self) -> Result<u64> { Ok(self.len()? - self.position()?) }
+
+    pub fn remaining_bytes(&mut self) -> Result<Vec<u8>> {
+        let mut ret = Vec::with_capacity(self.remaining_len()? as usize);
+        io::copy(self, &mut ret)?;
+
+        Ok(ret)
+    }
+
+    pub fn rewind(&mut self) -> Result<()> {
+        self.seek(SeekFrom::Start(0))?;
         Ok(())
     }
 }
@@ -123,6 +163,52 @@ impl Write for HybridBuf {
 // the impl to TryFrom and call swap_to_disk before returning
 impl From<Vec<u8>> for HybridBuf {
     fn from(vec: Vec<u8>) -> Self { HybridBuf::Mem(Cursor::new(vec)) }
+}
+
+// TODO: decide if the check for equality should not also compare the cursor
+// position
+impl PartialEq for HybridBuf {
+    fn eq(&self, other: &Self) -> bool {
+        macro_rules! metadata_or_false {
+            ($file:expr) => {
+                if let Ok(meta) = $file.metadata() {
+                    if let Ok(modified) = meta.modified() {
+                        (meta.len(), modified)
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            };
+        }
+
+        match self {
+            Self::Mem(cursor1) => {
+                if let Self::Mem(cursor2) = other {
+                    cursor1.get_ref() == cursor2.get_ref()
+                } else {
+                    false
+                }
+            }
+            Self::File(file1) => {
+                if let Self::File(file2) = other {
+                    metadata_or_false!(file1) == metadata_or_false!(file2)
+                } else {
+                    false
+                }
+            }
+        }
+    }
+}
+
+impl Seek for HybridBuf {
+    fn seek(&mut self, tgt: SeekFrom) -> Result<u64> {
+        match self {
+            Self::Mem(ref mut cursor) => cursor.seek(tgt),
+            Self::File(ref mut file) => file.seek(tgt),
+        }
+    }
 }
 
 #[cfg(test)]
