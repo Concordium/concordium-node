ff1114204199a18bbd203e06ed46d00369cddd83
Ian Johannesen <ij@concordium.com>
2019-08-22 14:14:32 +0200

    Force a specific version of the base image to make it possible to build backwards in time without issues

--- concordium-node/scripts/build-base-docker.sh
+++ concordium-node/scripts/build-base-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -f scripts/base.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base .
+docker build -f scripts/base.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.1 .
 
-docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base
+docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.1
 
 echo "DONE BUILDING base!"
