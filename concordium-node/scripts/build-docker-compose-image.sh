4a817ee21656aefed868577c6014b80487ec3073
Ian Johannesen <ij@concordium.com>
2019-09-12 15:58:37 +0200

    More docker buildkit

--- concordium-node/scripts/build-docker-compose-image.sh
+++ concordium-node/scripts/build-docker-compose-image.sh
@@ -9,7 +9,9 @@ if [ ! -z "$JENKINS_HOME" ]; then
 
     echo $CONSENSUS_VERSION > CONSENSUS_VERSION
 
-    docker build -f scripts/dev-client.Dockerfile -t concordium/dev-client:latest --build-arg CI_JOB_TOKEN=${2} .
+    export DOCKER_BUILDKIT=1
+
+    docker build -f scripts/dev-client.Dockerfile -t concordium/dev-client:latest --ssh default .
 
     rm -f CONSENSUS_VERSION
     rm -rf baker_id_gen
