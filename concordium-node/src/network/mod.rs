9968b9b4d19b69843d23b1b09b2453041c1af145
Lukasz Jedrzejczyk <lj@concordium.com>
2019-10-01 10:47:28 +0200

    remove unused const

--- concordium-node/src/network/mod.rs
+++ concordium-node/src/network/mod.rs
@@ -22,7 +22,5 @@ pub use self::{
 
 pub const PROTOCOL_NAME: &str = "CONCORDIUMP2P";
 pub const PROTOCOL_VERSION: u16 = 1;
-
 pub const PROTOCOL_PORT_LENGTH: usize = 5;
-pub const PROTOCOL_WHOLE_PACKET_SIZE: usize = 4;
 pub const PROTOCOL_MAX_MESSAGE_SIZE: u32 = 20_971_520;
