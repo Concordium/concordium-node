9cbb39a743de9d58aad528183f49ed0d746f527d
Lukasz Jedrzejczyk <lj@concordium.com>
2019-10-01 14:25:25 +0200

    shorten the protocol name and version

--- concordium-node/src/network/mod.rs
+++ concordium-node/src/network/mod.rs
@@ -20,7 +20,7 @@ pub use self::{
     response::NetworkResponse,
 };
 
-pub const PROTOCOL_NAME: &str = "CONCORDIUMP2P";
-pub const PROTOCOL_VERSION: u16 = 1;
+pub const PROTOCOL_NAME: &str = "CP2P";
+pub const PROTOCOL_VERSION: u8 = 1;
 pub const PROTOCOL_PORT_LENGTH: usize = 5;
 pub const PROTOCOL_MAX_MESSAGE_SIZE: u32 = 20_971_520;
