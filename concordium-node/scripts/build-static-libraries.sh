e63337634e6d45f02f7ba4486544c3f76327987d
Ian Johannesen <ij@concordium.com>
2019-10-22 10:54:52 +0200

    Remove git-fetch-with-cli

--- concordium-node/scripts/build-static-libraries.sh
+++ concordium-node/scripts/build-static-libraries.sh
@@ -22,6 +22,8 @@ rustup default 1.38.0
 
 rm -rf $HOME/.cargo
 
+sed -i 's/git-fetch-with-cli = true/git-fetch-with-cli = false/' /build/Concordium/rust-src/.cargo/config
+
 (
     cd /build/crypto/rust-src &&
     cargo check
