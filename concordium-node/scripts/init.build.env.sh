f8d2b36a34f2d590ac622109c6f8062af300cd56
Ian Johannesen <ij@concordium.com>
2019-10-26 11:33:32 +0200

    Adding verbosity

--- concordium-node/scripts/init.build.env.sh
+++ concordium-node/scripts/init.build.env.sh
@@ -7,8 +7,10 @@ CONSENSUS_VERSION=$(cat scripts/CONSENSUS_VERSION)
 ln -s /usr/lib/libtinfo.so.6 /usr/lib/libtinfo.so.5
 
 if [[ ! -z "$CONSENSUS_TYPE" && "$CONSENSUS_TYPE" != "default" ]]; then
+    echo "Using consensus type $CONSENSUS_TYPE"
     curl -o static-consensus-$CONSENSUS_VERSION.tar.gz https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$CONSENSUS_VERSION-$CONSENSUS_TYPE.tar.gz
 else
+    echo "Using default consensus type"
     curl -o static-consensus-$CONSENSUS_VERSION.tar.gz https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$CONSENSUS_VERSION.tar.gz
 fi
 
