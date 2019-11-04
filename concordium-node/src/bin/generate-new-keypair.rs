42913da17c02076d969bcd7ebcd8d3ca5906eeb0
Aleš Bizjak <ab@concordium.com>
2019-11-04 16:40:49 +0100

    Do not use FFI exports to generate keys.
    Instead use the normal pure rust functions.

--- concordium-node/src/bin/generate-new-keypair.rs
+++ concordium-node/src/bin/generate-new-keypair.rs
@@ -1,12 +1,20 @@
-use p2p_client::crypto::KeyPair;
+use concordium_common::blockchain_types::{AccountAddress, SchemeId};
+use ed25519_dalek::Keypair;
+use rand::thread_rng;
 use std::result::Result;
 
+// FIXME: Once the crypto id library exports relevant functions use them instead
+// of using the dalek library directly.
 fn main() -> Result<(), &'static str> {
-    let keys = KeyPair::new();
+    let mut csprng = thread_rng();
+    let keys = Keypair::generate(&mut csprng);
 
-    println!("Private key: {}", keys.private_key_as_base64());
-    println!("Public key: {}", keys.public_key_as_base64());
-    println!("Address: {}", keys.address());
+    println!("Private key: {}", base64::encode(keys.secret.as_bytes()));
+    println!("Public key: {}", base64::encode(keys.public.as_bytes()));
+    println!(
+        "Address: {}",
+        AccountAddress::from((&keys.public.as_bytes()[..], SchemeId::Ed25519))
+    );
 
     Ok(())
 }
