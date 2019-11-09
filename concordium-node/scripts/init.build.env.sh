5ccc1aede3e8f0c9a251757e47d2f9bd63103dbb
Ian Johannesen <ij@concordium.com>
2019-11-09 23:06:41 +0100

    Fixes

--- concordium-node/scripts/init.build.env.sh
+++ concordium-node/scripts/init.build.env.sh
@@ -2,11 +2,10 @@
 
 set -e 
 
+ln -s /usr/lib/libtinfo.so.6 /usr/lib/libtinfo.so.5
+CONSENSUS_VERSION=$( cat CONSENSUS_VERSION ) 
 (
     cd deps/internal/consensus/consensus-rust
-    CONSENSUS_VERSION=$( git rev-parse HEAD ) 
-    ln -s /usr/lib/libtinfo.so.6 /usr/lib/libtinfo.so.5
-
     if [[ ! -z "$CONSENSUS_TYPE" && "$CONSENSUS_TYPE" != "default" ]]; then
         echo "Using consensus type $CONSENSUS_TYPE"
         curl -o static-consensus-$CONSENSUS_VERSION.tar.gz https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$CONSENSUS_VERSION-$CONSENSUS_TYPE.tar.gz
@@ -14,13 +13,9 @@ set -e
         echo "Using default consensus type"
         curl -o static-consensus-$CONSENSUS_VERSION.tar.gz https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/static-consensus-$CONSENSUS_VERSION.tar.gz
     fi
-
     tar -xf static-consensus-$CONSENSUS_VERSION.tar.gz
     rm -rf deps/static-libs/linux/*
-
     mv target/* deps/static-libs/linux/
-
     rm -r target static-consensus-$CONSENSUS_VERSION.tar.gz
 )
-
 ldconfig
