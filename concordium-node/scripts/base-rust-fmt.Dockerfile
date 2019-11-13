de9110296b953d8c37be2c3b0167a8d63df862e9
Ian Johannesen <ij@concordium.com>
2019-11-13 09:40:13 +0100

    Add rust lint:fmt image

--- concordium-node/scripts/base-rust-fmt.Dockerfile
+++ concordium-node/scripts/base-rust-fmt.Dockerfile
@@ -1,6 +1,6 @@
 FROM rustlang/rust:nightly
 RUN apt-get update && apt-get install -y moreutils curl
 RUN apt-get clean
-RUN rustup default nightly-2019-07-25
+RUN rustup default nightly-2019-11-13
 RUN rustup component add rustfmt
 RUN rustc --version && cargo --version
--- concordium-node/scripts/build-base-rust-fmt-docker.sh
+++ concordium-node/scripts/build-base-rust-fmt-docker.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -f scripts/base-rust-fmt.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base-rust-fmt:0.1 .
+docker build -f scripts/base-rust-fmt.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base-rust-fmt:2019-11-13 .
 
-docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base-rust-fmt:0.1
+docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base-rust-fmt:2019-11-13
 
 echo "DONE BUILDING base!"
