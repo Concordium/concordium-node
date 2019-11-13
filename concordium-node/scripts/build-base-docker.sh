6f7b08f9b8e9e113bb1d59afb54d9a22d5a8d9b5
Ian Johannesen <ij@concordium.com>
2019-11-13 10:57:25 +0100

    Bump base

--- concordium-node/scripts/build-base-docker.sh
+++ concordium-node/scripts/build-base-docker.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -f scripts/base.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.4 .
+docker build -f scripts/base.Dockerfile -t 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.5 .
 
-docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.4
+docker push 192549843005.dkr.ecr.eu-west-1.amazonaws.com/concordium/base:0.5
 
 echo "DONE BUILDING base!"
