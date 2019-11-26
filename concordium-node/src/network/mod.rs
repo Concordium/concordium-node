76ba12c93929e8a70e8e97da5c64861643c84547
Lukasz Jedrzejczyk <lj@concordium.com>
2019-11-26 10:36:47 +0100

    move NetworkId

--- concordium-node/src/network/mod.rs
+++ concordium-node/src/network/mod.rs
@@ -1,6 +1,5 @@
 pub mod buckets;
 pub mod message;
-pub mod network_id;
 pub mod packet;
 pub mod protocol_message_type;
 pub mod request;
@@ -10,7 +9,6 @@ pub mod serialization;
 pub use self::{
     buckets::Buckets,
     message::{NetworkMessage, NetworkMessagePayload},
-    network_id::NetworkId,
     packet::{NetworkPacket, NetworkPacketBuilder, NetworkPacketType},
     protocol_message_type::{
         AsProtocolMessageType, AsProtocolPacketType, AsProtocolRequestType, AsProtocolResponseType,
@@ -20,8 +18,23 @@ pub use self::{
     response::NetworkResponse,
 };
 
+use std::fmt;
+
 pub const PROTOCOL_NAME: &str = "CP2P";
 pub const PROTOCOL_VERSION: u8 = 1;
 pub const PROTOCOL_HEADER_LEN: usize = 4 + 1 + 8; // name + version + timestamp; FIXME: shouldn't include the stamp
-
 pub const PROTOCOL_MAX_MESSAGE_SIZE: u32 = 20_971_520;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "s11n_serde", derive(Serialize, Deserialize))]
+pub struct NetworkId {
+    pub id: u16,
+}
+
+impl From<u16> for NetworkId {
+    fn from(id: u16) -> Self { NetworkId { id } }
+}
+
+impl fmt::Display for NetworkId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{:05}", self.id) }
+}
