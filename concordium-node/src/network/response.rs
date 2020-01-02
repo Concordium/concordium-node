e0e2b0c538eeb96973c16e37aa8d54fac967dcfe
Lukasz Jedrzejczyk <lj@concordium.com>
2020-01-02 14:52:56 +0100

    shrink the network module

--- concordium-node/src/network/response.rs
+++
@@ -1,8 +0,0 @@
-use crate::common::P2PPeer;
-
-#[derive(Debug, PartialEq)]
-#[cfg_attr(feature = "s11n_serde", derive(Serialize, Deserialize))]
-pub enum NetworkResponse {
-    Pong,
-    PeerList(Vec<P2PPeer>),
-}
