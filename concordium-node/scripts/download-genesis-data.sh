d4ef51ebb7c99513b60260ae92d98f23808a80ca
Martin Karlsen Jensen <mkj@concordium.com>
2020-02-06 10:02:57 +0100

    Changed the S3 URL for genesis data downloads to correct one

--- concordium-node/scripts/download-genesis-data.sh
+++ concordium-node/scripts/download-genesis-data.sh
@@ -13,6 +13,6 @@ mkdir -p genesis-data
 (
     cd genesis-data &&
     curl -s https://s3-eu-west-1.amazonaws.com/genesis-data.concordium.com/CONTENTS-${GENESIS_VERSION} | while read BAKER_SIZE; do
-        curl -s https://s3-eu-west-1.amazonaws.com/static-libraries.concordium.com/${BAKER_SIZE}-bakers-${GENESIS_VERSION}.tar.gz > ${BAKER_SIZE}-bakers.tar.gz
+        curl -s https://s3-eu-west-1.amazonaws.com/genesis-data.concordium.com/${BAKER_SIZE}-bakers-${GENESIS_VERSION}.tar.gz > ${BAKER_SIZE}-bakers.tar.gz
     done
 )
