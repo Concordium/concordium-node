cb1ca9657b0d09511c8478ee8a3efcf7e81fe227
Aleš Bizjak <ab@concordium.com>
2020-02-23 22:35:03 +0100

    Make tests compile again.

--- concordium-consensus/src/Concordium/GlobalState/TransactionLogs.hs
+++ concordium-consensus/src/Concordium/GlobalState/TransactionLogs.hs
@@ -4,6 +4,7 @@ module Concordium.GlobalState.TransactionLogs where
 import Control.Monad.Except
 import Control.Monad.Trans.Maybe
 import Control.Monad.Identity
+import Control.Monad.RWS.Strict
 
 import Concordium.Types
 import Concordium.GlobalState.Classes
@@ -13,6 +14,11 @@ class Monad m => TransactionLogger m where
 
 newtype NoTransactionLogger m a = NoTransactionLogger (m a)
     deriving(Functor, Applicative, Monad)
+
+-- FIXME: Dummy instance to get things going.
+instance TransactionLogger IO where
+  {-# INLINE tlNotifyAccountEffect #-}
+  tlNotifyAccountEffect = \_ _ -> return ()
 
 -- An instance that does no transaction logging.
 instance Monad m => TransactionLogger (NoTransactionLogger m) where
@@ -27,3 +33,4 @@ deriving via NoTransactionLogger Identity instance TransactionLogger Identity
 
 deriving via (MGSTrans MaybeT m) instance TransactionLogger m => TransactionLogger (MaybeT m)
 deriving via (MGSTrans (ExceptT e) m) instance TransactionLogger m => TransactionLogger (ExceptT e m)
+deriving via (MGSTrans (RWST r w s) m) instance (Monoid w, TransactionLogger m) => TransactionLogger (RWST r w s m)
