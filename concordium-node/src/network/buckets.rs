efa4d939dcb434c4c9c9ee0ab07c8b5ae5ee8dec
Lukasz Jedrzejczyk <lj@concordium.com>
2020-03-04 10:17:54 +0100

    document the buckets

--- concordium-node/src/network/buckets.rs
+++ concordium-node/src/network/buckets.rs
@@ -11,10 +11,12 @@ use crate::{
 
 const BUCKET_COUNT: usize = 1;
 
+/// A representation of a node in a bucket.
 #[derive(Eq, Clone)]
 pub struct Node {
-    pub peer:      P2PPeer,
-    pub networks:  HashSet<NetworkId>,
+    pub peer: P2PPeer,
+    pub networks: HashSet<NetworkId>,
+    /// The timestamp pointing to when the node was seen last.
     pub last_seen: u64,
 }
 
@@ -26,7 +28,10 @@ impl Hash for Node {
     fn hash<H: Hasher>(&self, state: &mut H) { self.peer.hash(state) }
 }
 
+/// A bucket of nodes.
 pub type Bucket = HashSet<Node>;
+
+/// The set of buckets.
 pub struct Buckets {
     pub buckets: Vec<Bucket>,
 }
@@ -40,6 +45,7 @@ impl Default for Buckets {
 }
 
 impl Buckets {
+    /// Adds a peer to a bucket.
     pub fn insert_into_bucket(&mut self, peer: &P2PPeer, networks: HashSet<NetworkId>) {
         let bucket = &mut self.buckets[0];
 
@@ -50,6 +56,7 @@ impl Buckets {
         });
     }
 
+    /// Update the networks of a node in the bucket.
     pub fn update_network_ids(&mut self, peer: &P2PPeer, networks: HashSet<NetworkId>) {
         let bucket = &mut self.buckets[0];
 
@@ -60,6 +67,7 @@ impl Buckets {
         });
     }
 
+    /// Returns all the nodes in buckets.
     pub fn get_all_nodes(
         &self,
         sender: Option<&P2PPeer>,
@@ -83,16 +91,19 @@ impl Buckets {
         nodes
     }
 
+    /// Returns the number of networks in the buckets.
     pub fn len(&self) -> usize {
         self.buckets.iter().flat_map(HashSet::iter).map(|node| node.networks.len()).sum()
     }
 
+    /// Checks whether the buckets are empty.
     pub fn is_empty(&self) -> bool { self.len() == 0 }
 
+    /// Returns the desired number of nodes from the buckets.
     pub fn get_random_nodes(
         &self,
         sender: &P2PPeer,
-        amount: usize,
+        number: usize,
         networks: &HashSet<NetworkId>,
         partition: bool,
     ) -> Vec<P2PPeer> {
@@ -107,12 +118,13 @@ impl Buckets {
                         peer.id.0 % 2 != 0
                     }
                 })
-                .choose_multiple(&mut rng, amount)
+                .choose_multiple(&mut rng, number)
         } else {
-            self.get_all_nodes(Some(sender), networks).into_iter().choose_multiple(&mut rng, amount)
+            self.get_all_nodes(Some(sender), networks).into_iter().choose_multiple(&mut rng, number)
         }
     }
 
+    /// Removes the bucket nodes older than then specified amount of time.
     pub fn clean_buckets(&mut self, timeout_bucket_entry_period: u64) {
         let clean_before = get_current_stamp() - timeout_bucket_entry_period;
         self.buckets[0].retain(|entry| entry.last_seen >= clean_before);
