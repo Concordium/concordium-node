38cde85e35eeedbf8e754fef278743a2e246b2cc
Javier Sagredo <js@concordium.com>
2020-03-16 18:00:00 +0100

    Increase strictness in some places

--- concordium-consensus/src/Concordium/GlobalState/TransactionTable.hs
+++ concordium-consensus/src/Concordium/GlobalState/TransactionTable.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, BangPatterns #-}
 module Concordium.GlobalState.TransactionTable where
 
 import qualified Data.HashMap.Strict as HM
@@ -9,8 +9,8 @@ import Concordium.Types.Transactions
 
 data TransactionTable = TransactionTable {
     -- |Map from transaction hashes to transactions, together with their current status.
-    _ttHashMap :: HM.HashMap TransactionHash (BlockItem, TransactionStatus),
-    _ttNonFinalizedTransactions :: HM.HashMap AccountAddress AccountNonFinalizedTransactions
+    _ttHashMap :: !(HM.HashMap TransactionHash (BlockItem, TransactionStatus)),
+    _ttNonFinalizedTransactions :: !(HM.HashMap AccountAddress AccountNonFinalizedTransactions)
 }
 makeLenses ''TransactionTable
 
