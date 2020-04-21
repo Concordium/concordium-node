e95484c9cba495e9020085b03d8d1621ad486ed6
Marianna Rapoport <mra@concordium.com>
2020-04-21 19:09:11 +0200

    Store hash of block nonces rather than full block-nonce list in BirkParameters

--- concordium-consensus/src/Concordium/GlobalState/SeedState.hs
+++ concordium-consensus/src/Concordium/GlobalState/SeedState.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module Concordium.GlobalState.SeedState where
 
@@ -7,6 +6,7 @@ import GHC.Generics
 import Data.Word
 import Data.Serialize
 
+import Concordium.Crypto.SHA256 (Hash)
 import Concordium.Types
 
 -- |State for computing the leadership election nonce.
@@ -17,12 +17,16 @@ data SeedState = SeedState {
   epochLength :: EpochLength,
   -- |Current epoch
   epoch :: Word64,
-  -- |List of blocknonces from current epoch in reverse order
-  revBlockNonces :: [BlockNonce]
+  -- |Hash of the block nonces in the current epoch up to block B,
+  --  where B is either the last of the first 2/3 of the blocks in this epoch,
+  --  or the current block, whichever comes first.
+  --  blockNonceHash should be Nothing only for the genesis block.
+  blockNonceHash :: Maybe Hash
+
 } deriving (Eq, Generic, Show)
 instance Serialize SeedState
 
 -- |Instantiate a seed state: leadership election nonce should be random, epoch length should be long, but not too long...
 genesisSeedState :: LeadershipElectionNonce -> EpochLength -> SeedState
 genesisSeedState nonce epochLength =
-  SeedState nonce epochLength 0 []
+  SeedState nonce epochLength 0 Nothing
