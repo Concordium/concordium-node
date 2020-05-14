1c7238bf13083c0287213c9f7cacda7614543fd5
Felix Wiemuth <fw@concordium.com>
2020-05-14 14:12:43 +0200

    Add cost for storing and loading contract instances
    CB-163

--- concordium-consensus/src/Concordium/Scheduler/Cost.hs
+++ concordium-consensus/src/Concordium/Scheduler/Cost.hs
@@ -5,6 +5,8 @@ Definition of cost functions for the different transactions.
 -}
 module Concordium.Scheduler.Cost where
 
+import Data.Word
+
 import Concordium.Scheduler.Types
 
 import Control.Exception(assert)
@@ -23,6 +25,50 @@ toInterpreterEnergy = (* interpreterEnergy)
 -- | Convert interpreter energy to general energy (rounding down).
 fromInterpreterEnergy :: Energy -> Energy
 fromInterpreterEnergy = (`div` interpreterEnergy)
+
+-- ** Cost for storage
+
+-- $storage-cost
+-- Storage cost is mainly determined by the limits on storage size per block we want to ensure.
+-- It must be ensured that the actual storage cost is not higher.
+-- TODO The current values are estimates, set them more precisely.
+
+-- | Cost for performing a storage operation.
+storageBase :: Energy
+storageBase = 5
+
+-- | Cost for storing 100 bytes.
+storagePer100Byte :: Energy
+storagePer100Byte = 5
+
+-- | Cost for storing the given number of bytes. It is charged for every started 100 bytes.
+storage :: Word64 -> Energy
+storage n = assert (n >= 0) $ storageBase + ((fromIntegral n + 99) `div` 100) * storagePer100Byte
+
+-- | For a given amount of energy, determine the number of bytes that can be stored using that energy.
+maxStorage :: Energy -> Word64
+maxStorage e = fromIntegral $ ((max 0 (e-storageBase)) * 100) `div` storagePer100Byte
+
+-- | Cost for performing a lookup operation.
+lookupBase :: Energy
+lookupBase = 3
+
+-- | Cost for looking up 100 bytes.
+lookupPer100Byte :: Energy
+lookupPer100Byte = 3
+
+-- | Cost for looking up the given number of bytes. It is charged for every started 100 bytes.
+lookup :: Word64 -> Energy
+lookup n = assert (n >= 0) $ lookupBase + ((fromIntegral n + 99) `div` 100) * lookupPer100Byte
+
+-- | For a given amount of energy, determine the number of bytes that can be stored using that energy.
+maxLookup :: Energy -> Word64
+maxLookup e = fromIntegral $ ((max 0 (e-lookupBase)) * 100) `div` lookupPer100Byte
+
+-- | A bound on the maximum size of a contract state ('Value') in bytes.
+-- This is used to calculate the maximum possible energy an instance lookup can cost.
+maxInstanceSize :: Word64
+maxInstanceSize = 1000000 -- TODO set based on calculated restrictions
 
 -- |The cost to process the header.
 -- Processing includes hashing the transaction and checking all signatures against this hash,
