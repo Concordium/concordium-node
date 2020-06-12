3b2e9893db10288a3c71904ce459560c2cf098c5
Aleš Bizjak <ab@concordium.com>
2020-06-12 16:34:02 +0200

    Documentation.

--- concordium-consensus/src/Concordium/Scheduler/Cost.hs
+++ concordium-consensus/src/Concordium/Scheduler/Cost.hs
@@ -204,6 +204,7 @@ updateElectionDifficulty = 0
 updateBakerAggregationVerifyKey :: Energy
 updateBakerAggregationVerifyKey = 2700
 
--- |Cost to update baker election key
+-- |Cost to update baker election key.
+-- The underlying computation is the same as for updating the baker or account key.
 updateBakerElectionKey :: Energy
 updateBakerElectionKey = 90
