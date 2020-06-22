13784d67fcc5e9e813bf6f0eff0fdf251001965e
Ian Johannesen <ij@concordium.com>
2020-06-22 16:20:01 +0200

    Support via cli arg, and for both types

--- concordium-node/src/configuration.rs
+++ concordium-node/src/configuration.rs
@@ -1,5 +1,6 @@
 //! The client's parameters and constants used by other modules.
 
+use crate::connection::DeduplicationHashAlgorithm;
 use app_dirs2::*;
 use failure::Fallible;
 use preferences::{Preferences, PreferencesMap};
@@ -347,6 +348,12 @@ pub struct ConnectionConfig {
         default_value = "10"
     )]
     pub events_queue_size: usize,
+    #[structopt(
+        long = "deduplication-hashing-algorithm",
+        help = "Hash algorithm used for deduplication [xxhash64|sha256]",
+        default_value = "xxhash64"
+    )]
+    pub deduplication_hashing_algorithm: DeduplicationHashAlgorithm,
 }
 
 #[derive(StructOpt, Debug)]
