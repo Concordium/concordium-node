ba5dc9a5d7b6a1a0480fd7bfcf4d14161b54dda6
Aleš Bizjak <ab@concordium.com>
2020-08-03 11:41:44 +0200

    Add test to make sure the init context is passed correctly to the contract.

--- concordium-consensus/tests/scheduler/Spec.hs
+++ concordium-consensus/tests/scheduler/Spec.hs
@@ -3,6 +3,7 @@ module Main where
 -- import qualified SchedulerTests.ContractCommSpec(tests)
 import qualified SchedulerTests.SimpleTransfersTest(tests)
 import qualified SchedulerTests.ChainMetatest(tests)
+import qualified SchedulerTests.InitContextTest(tests)
 -- import qualified SchedulerTests.ReceiveContextTest(tests)
 -- import qualified SchedulerTests.FibonacciTest(tests)
 import qualified SchedulerTests.AccountTransactionSpecs(tests)
@@ -24,6 +25,7 @@ main = hspec $ do
          -- SchedulerTests.ContractCommSpec.tests
          SchedulerTests.SimpleTransfersTest.tests
          SchedulerTests.ChainMetatest.tests
+         SchedulerTests.InitContextTest.tests
          -- SchedulerTests.ReceiveContextTest.tests
          -- SchedulerTests.FibonacciTest.tests
          SchedulerTests.AccountTransactionSpecs.tests
