57d9bc43da34fd8f5f1159324b840dfdf2932f65
Aleš Bizjak <ab@concordium.com>
2020-08-20 08:55:09 +0200

    Expose account encryption key.

--- concordium-consensus/src/Concordium/Getters.hs
+++ concordium-consensus/src/Concordium/Getters.hs
@@ -206,6 +206,8 @@ getInstances hash sfsRef = runStateQuery sfsRef $
 -- - accountCredentials -- a list of versioned credential values.
 -- - accountDelegation -- null or a baker id
 -- - accountInstances -- a list of contract instance addresses owned by this account.
+-- - accountEncryptedAmount -- an object detailed the current encrypted balance on the account (which consists of a number of encrypted amounts)
+-- - accountEncryptionKey -- the encryption key others can use to send to this account.
 getAccountInfo :: (SkovStateQueryable z m) => BlockHash -> z -> AccountAddress -> IO Value
 getAccountInfo hash sfsRef addr = runStateQuery sfsRef $
   withBlockStateJSON hash $ \st ->
@@ -218,6 +220,7 @@ getAccountInfo hash sfsRef addr = runStateQuery sfsRef $
               delegate <- BS.getAccountStakeDelegate acc
               instances <- BS.getAccountInstances acc
               encrypted <- BS.getAccountEncryptedAmount acc
+              encryptionKey <- BS.getAccountEncryptionKey acc
               return $ object ["accountNonce" .= nonce
                               ,"accountAmount" .= amount
                                 -- credentials, most recent first
@@ -225,6 +228,7 @@ getAccountInfo hash sfsRef addr = runStateQuery sfsRef $
                               ,"accountDelegation" .= delegate
                               ,"accountInstances" .= S.toList instances
                               ,"accountEncryptedAmount" .= encrypted
+                              ,"accountEncryptionKey" .= encryptionKey
                               ]
 
 getContractInfo :: (SkovStateQueryable z m) => BlockHash -> z -> AT.ContractAddress -> IO Value
