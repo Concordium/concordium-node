b41ee841aa85bd60dd1db2b7821f02ab78f00c88
Javier Sagredo <js@concordium.com>
2020-09-04 15:39:42 +0200

    Moved function to Globalstate.TreeState

--- concordium-consensus/tools/database-exporter/DatabaseExporter.hs
+++ concordium-consensus/tools/database-exporter/DatabaseExporter.hs
@@ -13,7 +13,6 @@ import           Concordium.GlobalState.Persistent.LMDB
 import           Concordium.GlobalState.Persistent.BlockState (PersistentBlockState)
 import           Concordium.GlobalState.Types
 import           Concordium.Types
-import           Concordium.Types.Transactions
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Data.ByteString as BS hiding (putStrLn, tail, last, sort)
@@ -24,7 +23,7 @@ import           Lens.Micro.Platform
 import           System.Directory
 import           System.FilePath.Posix
 import           System.IO
-import           System.Exit
+import           Concordium.GlobalState.TreeState (ImportingResult(..), readBlocks)
 
 data ReadEnv = ReadEnv
     { _path       :: FilePath
@@ -62,7 +61,7 @@ exportBlockV0 finalizedHeight = do
   case mblock of
     Nothing -> return ()
     Just storedBlock -> do
-      let serializedBlock = S.runPut (putBlockV0 (sbBlock storedBlock))
+      let serializedBlock = S.runPut (putBlockV1 (sbBlock storedBlock))
           len = BS.length serializedBlock
       export <- view exportFile
       liftIO $ do
@@ -88,14 +87,6 @@ exportDatabaseV0 = do
 initialReadingHandle :: FilePath -> IO Handle
 initialReadingHandle p = openBinaryFile p ReadMode
 
--- |Read the header of the export file.
--- The header consists of
---
--- - version number that determines the format of all the subsequent blocks in the file.
---
--- The function returns, if successfull, the version, and the unconsumed input.
-readHeader :: LBS.ByteString -> Either String (Version, LBS.ByteString)
-readHeader = S.runGetLazyState S.get
 
 readExportedDatabase :: DatabaseExporterMonad ()
 readExportedDatabase = do
@@ -104,23 +95,7 @@ readExportedDatabase = do
     tm <- liftIO getCurrentTime
     lbs <- LBS.hGetContents export -- file is automatically closed after EOF is reached
     putStrLn "Starting database processing"
-    case readHeader lbs of
-      Left err -> die $ "Cannot read export file header: " ++ err
-      Right (version, rest)
-          | version == 0 -> readBlocksFromFileV0 tm rest
-          | otherwise -> die $ "Unsupported export file version " ++ show version
-  where readBlocksFromFileV0 tm rest
-            | LBS.null rest = putStrLn "Finished reading export file."
-            | otherwise =
-              case S.runGetLazyState blockGetter rest of
-                Left err -> die $ "Error reading block bytes: " ++ err
-                Right (blockBS, remainingBS) -> do
-                  case S.runGet (getBlockV0 (utcTimeToTransactionTime tm)) blockBS of
-                    Left e -> die $ "Error deserializing block: " ++ e
-                    Right block -> do
-                      putStrLn $ "Properly read block at blockSlot " ++ show (blockSlot block)
-                      readBlocksFromFileV0 tm remainingBS
-
-        blockGetter = do
-          len <- S.getWord64be
-          S.getByteString (fromIntegral len)
+    result <- readBlocks lbs tm (\_ _ -> putStrLn) undefined (const $ return Success) :: IO (ImportingResult ())
+    case result of
+      SerializationFail -> putStrLn "Error."
+      _ -> putStrLn "Done."
