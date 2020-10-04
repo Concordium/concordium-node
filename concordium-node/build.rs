13984147b5278c108bef9f510ed00d6720acf350
Aleš Bizjak <ab@concordium.com>
2020-10-04 10:20:35 +0200

    Automatically add path to haskell libraries.
    This allows to run the node as simply `cargo run p2p_client-cli` (after
    consensus libraries are build with stack build in deps/internal/consensus).

--- concordium-node/build.rs
+++ concordium-node/build.rs
@@ -1,8 +1,10 @@
-use std::path::Path;
-#[cfg(windows)]
-use std::{process::Command, str};
+use std::{path::Path, process::Command, str};
 
-fn main() {
+fn command_output(cmd: &mut Command) -> String {
+    str::from_utf8(&cmd.output().unwrap().stdout).unwrap().trim_end().to_string()
+}
+
+fn main() -> std::io::Result<()> {
     // Compile the Cap'n'Proto schema
     #[cfg(feature = "s11n_capnp")]
     capnpc::CompilerCommand::new()
@@ -41,9 +43,37 @@ fn main() {
         println!(r"cargo:rustc-link-search=native=.\deps\internal\consensus\Concordium");
     }
 
+    #[cfg(unix)]
+    {
+        // Add Haskell packages to the library search search path so that cargo run
+        // works without messing with paths.
+        let local_package = Path::new(&command_output(Command::new("stack").args(&[
+            "--stack-yaml",
+            "./deps/internal/consensus/stack.yaml",
+            "path",
+            "--local-install-root",
+        ])))
+        .join("lib")
+        .join("x86_64-linux-ghc-8.8.3");
+        let ghc_lib_dir = Path::new(&command_output(Command::new("stack").args(&[
+            "--stack-yaml",
+            "./deps/internal/consensus/stack.yaml",
+            "ghc",
+            "--",
+            "--print-libdir",
+        ])))
+        .join("rts");
+
+        println!(
+            "cargo:rustc-env=LD_LIBRARY_PATH={}:{}",
+            ghc_lib_dir.as_path().to_string_lossy(),
+            local_package.as_path().to_string_lossy()
+        );
+    }
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .compile(&[&proto], &[&proto_root_input])
         .expect("Failed to compile gRPC definitions!");
+    Ok(())
 }
