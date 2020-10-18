de0cea2d390b38d1acc19837c881dfe61efd3e48
Aleš Bizjak <ab@concordium.com>
2020-10-18 15:21:59 +0200

    Update build script to be consistent with the one from consensus.

--- concordium-node/build.rs
+++ concordium-node/build.rs
@@ -1,4 +1,7 @@
-use std::{path::Path, process::Command, str};
+use std::{env, path::Path, process::Command, str};
+
+#[cfg(all(unix, not(feature = "static")))]
+const GHC_VARIANT: &str = "x86_64-linux-ghc-8.8.3";
 
 fn command_output(cmd: &mut Command) -> String {
     str::from_utf8(&cmd.output().unwrap().stdout).unwrap().trim_end().to_string()
@@ -43,25 +46,29 @@ fn main() -> std::io::Result<()> {
         println!(r"cargo:rustc-link-search=native=.\deps\internal\consensus\Concordium");
     }
 
-    #[cfg(unix)]
+    #[cfg(all(unix, not(feature = "static")))]
     {
         // Add Haskell packages to the library search search path so that cargo run
         // works without messing with paths.
-        let local_package = Path::new(&command_output(Command::new("stack").args(&[
-            "--stack-yaml",
-            "./deps/internal/consensus/stack.yaml",
-            "path",
-            "--local-install-root",
-        ])))
+        let local_package = Path::new(&env::var("CONCORDIUM_HASKELL_ROOT").unwrap_or_else(|_| {
+            command_output(Command::new("stack").args(&[
+                "--stack-yaml",
+                "./deps/internal/consensus/stack.yaml",
+                "path",
+                "--local-install-root",
+            ]))
+        }))
         .join("lib")
-        .join("x86_64-linux-ghc-8.8.3");
-        let ghc_lib_dir = Path::new(&command_output(Command::new("stack").args(&[
-            "--stack-yaml",
-            "./deps/internal/consensus/stack.yaml",
-            "ghc",
-            "--",
-            "--print-libdir",
-        ])))
+        .join(GHC_VARIANT);
+        let ghc_lib_dir = Path::new(&env::var("HASKELL_GHC_LIBDIR").unwrap_or_else(|_| {
+            command_output(Command::new("stack").args(&[
+                "--stack-yaml",
+                "./deps/internal/consensus/stack.yaml",
+                "ghc",
+                "--",
+                "--print-libdir",
+            ]))
+        }))
         .join("rts");
 
         println!(
@@ -70,6 +77,7 @@ fn main() -> std::io::Result<()> {
             local_package.as_path().to_string_lossy()
         );
     }
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
