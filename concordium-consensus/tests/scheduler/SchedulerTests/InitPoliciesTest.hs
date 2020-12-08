7c03114e7e64fe51543eaa0afe2042f0d3deee17
Aleš Bizjak <ab@concordium.com>
2020-12-08 08:55:42 +0100

    More testing of non-trivial policies in smart-contracts.

--- concordium-consensus/tests/scheduler/SchedulerTests/InitPoliciesTest.hs
+++ concordium-consensus/tests/scheduler/SchedulerTests/InitPoliciesTest.hs
@@ -9,7 +9,9 @@ import Test.Hspec
 import Test.HUnit(assertFailure, assertEqual, assertBool, Assertion)
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Short as BSS
 import Data.Maybe
+import qualified Data.Map.Strict as Map
 
 import qualified Concordium.Scheduler.Types as Types
 
@@ -19,13 +21,17 @@ import Concordium.Scheduler.WasmIntegration
 
 import Concordium.Types.DummyData
 
-testMany :: Assertion
-testMany = do
+setup :: String -> IO ModuleInterface
+setup errString = do
   source <- BS.readFile "./testdata/contracts/context_test.wasm"
   let wasmMod = WasmModule 0 source
   let miface = processModule wasmMod
-  assertBool "Module not valid" (isJust miface)
-  let iface = fromJust miface
+  assertBool ("Module not valid " ++ errString) (isJust miface)
+  return (fromJust miface)
+
+testNoAttributes :: Assertion
+testNoAttributes = do
+  iface <- setup "testNoAttributes"
   let initCtx1 = InitContext{
         initOrigin = alesAccount,
         icSenderPolicies = []
@@ -57,6 +63,33 @@ testMany = do
       assertEqual "No logs should be produced." [] logs
       assertEqual "State should be the singleton 0" (ContractState (BS.singleton 0)) newState
 
+testSingleAttribute :: Assertion
+testSingleAttribute = do
+  iface <- setup "testSingleAttribute"
+  let initCtx3 = InitContext{
+        initOrigin = alesAccount,
+        icSenderPolicies = [
+            SenderPolicy{
+                spIdentityProvider = IP_ID 17,
+                spCreatedAt = 0,
+                spValidTo = 0,
+                spItems = [
+                    (mapping Map.! "countryOfResidence",
+                     AttributeValue (BSS.toShort (BS.pack [1..31]))
+                    )
+                    ]
+                }
+            ]
+        }
+  let res3 = applyInitFun iface Types.dummyChainMeta initCtx3 (InitName "init_context_test_2") (Parameter mempty) 0 100000
+  case res3 of
+    Nothing -> assertFailure "Initialization failed due to out of energy."
+    Just (Left execFailure, _) -> assertFailure $ "Initalizatio failed due to " ++ show execFailure
+    Just (Right SuccessfulResultData{..}, _) -> do
+      assertEqual "No logs should be produced." [] logs
+      assertEqual "State should be the singleton 0" (ContractState (BS.singleton 0)) newState
+
 tests :: Spec
-tests = describe "Init policies test." $
-  specify "Test simple" testMany
+tests = describe "Init policies test." $ do
+  specify "Test with no attributes" testNoAttributes
+  specify "Test with single attribute" testSingleAttribute
