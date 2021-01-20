7ec29a025f4d67e9c1679526f5a7abbe214f3b38
Thomas Dinsdale-Young <ty@concordium.com>
2021-01-20 17:07:24 +0100

    Tweaks to fix build on Windows.

--- concordium-consensus/Setup.hs
+++ concordium-consensus/Setup.hs
@@ -24,9 +24,9 @@ makeRust args flags = do
         (("CARGO_NET_GIT_FETCH_WITH_CLI", "true") : env)
     case buildOS of
        Windows -> do
-            -- Copy just the dynamic library, since it doesn't link with the static one.
-            notice verbosity "Copying wasmer_interp.dll"
+            notice verbosity "Copying wasm_chain_integration library"
             rawSystemExit verbosity "cp" ["-u", "./smart-contracts/wasm-chain-integration/target/release/wasm_chain_integration.dll", "./smart-contracts/lib/"]
+            rawSystemExit verbosity "cp" ["-u", "./smart-contracts/wasm-chain-integration/target/release/libwasm_chain_integration.a", "./smart-contracts/lib/"]
        _ -> do
             rawSystemExit verbosity "ln" ["-s", "-f", "../wasm-chain-integration/target/release/libwasm_chain_integration.a", "./smart-contracts/lib/"]
             case buildOS of
@@ -36,20 +36,8 @@ makeRust args flags = do
                     rawSystemExit verbosity "ln" ["-s", "-f", "../wasm-chain-integration/target/release/libwasm_chain_integration.so", "./smart-contracts/lib/libwasm_chain_integration.so"]
     return emptyHookedBuildInfo
 
--- This is a quick and dirty hook to copy the wasm_chain_integration DLL on Windows.
-copyExtLib :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-copyExtLib _ flags _ lbi = case hostPlatform lbi of
-    Platform _ Windows -> do
-        let verbosity = fromFlag $ copyVerbosity flags
-        let dest = fromPathTemplate $ bindir $ installDirTemplates lbi
-        notice verbosity $ "Copying wasm_chain_integration.dll to: " ++ dest
-        rawSystemExit verbosity "cp" ["./smart-contracts/wasm-chain-integration/target/release/wasm_chain_integration.dll", dest]
-    _ -> return ()
-
-
 main = defaultMainWithHooks simpleUserHooks
   {
     preConf = makeRust
-  , postCopy = copyExtLib
   }
 
