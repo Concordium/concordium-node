4c8316155d44902250ac628a93b95a5e351921ef
Thomas Dinsdale-Young <ty@concordium.com>
2021-01-27 12:08:36 +0100

    Clean up

--- concordium-consensus/src/Concordium/GlobalState/Parameters.hs
+++ concordium-consensus/src/Concordium/GlobalState/Parameters.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -20,17 +19,16 @@ import Control.Monad hiding (fail)
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Aeson as AE
 import Data.Aeson.Types (FromJSON(..), (.:), withObject)
-import Concordium.Types.Updates
 
+import Concordium.Types.Updates
 import Concordium.Common.Version
 import Concordium.Types
 import Concordium.Types.Parameters
-
-import Concordium.GlobalState.BakerInfo
 import Concordium.Types.IdentityProviders
 import Concordium.Types.AnonymityRevokers
-
 import Concordium.Genesis.Data
+
+import Concordium.GlobalState.BakerInfo
 
 readIdentityProviders :: BSL.ByteString -> Maybe IdentityProviders
 readIdentityProviders bs = do
@@ -71,163 +69,6 @@ getExactVersionedCryptographicParameters bs = do
    guard (vVersion v == 0)
    return (vValue v)
 
-{-
--- 'GenesisBaker' is an abstraction of a baker at genesis.
--- It includes the minimal information for generating a baker.
-data GenesisBaker = GenesisBaker {
-    -- |The baker's public VRF key
-    gbElectionVerifyKey :: BakerElectionVerifyKey,
-    -- |The baker's public signature key
-    gbSignatureVerifyKey :: BakerSignVerifyKey,
-    -- |The baker's public key for aggregate signatures
-    gbAggregationVerifyKey :: BakerAggregationVerifyKey,
-    -- |The baker's initial stake
-    gbStake :: Amount,
-    -- |Whether to restake the baker's earnings from rewards
-    gbRestakeEarnings :: Bool,
-    -- |The baker ID is defined by the account, however we use it here
-    -- so we can report an error if inconsistent data is used.
-    gbBakerId :: BakerId
-}
-
-instance FromJSON GenesisBaker where
-    parseJSON = withObject "GenesisBaker" $ \v -> do
-            gbElectionVerifyKey <- v .: "electionVerifyKey"
-            gbSignatureVerifyKey <- v .: "signatureVerifyKey"
-            gbAggregationVerifyKey <- v .: "aggregationVerifyKey"
-            gbStake <- v .: "stake"
-            gbRestakeEarnings <- v .: "restakeEarnings"
-            gbBakerId <- v .: "bakerId"
-            return GenesisBaker{..}
-
--- |'GenesisAccount' are special account existing in the genesis block, in
--- addition to baker accounts which are defined by the 'GenesisBaker' structure.
-data GenesisAccount = GenesisAccount {
-  gaAddress :: !AccountAddress,
-  gaVerifyKeys :: !ID.AccountKeys,
-  gaBalance :: !Amount,
-  -- |We only need the credential values. However when parsing we parse a full
-  -- credential, due to some legacy format issues, and then extract the values.
-  -- The legacy issues are that the commitments are part of the "proofs" object
-  -- in the credential, which, in JSON, is represented just as a hex-string.
-  -- This should be reworked at some point, so that it is more principled than
-  -- the current, slighly hacky, solution.
-  gaCredential :: !ID.AccountCredential,
-  gaBaker :: !(Maybe GenesisBaker)
-}
-
-instance FromJSON GenesisAccount where
-  parseJSON = withObject "GenesisAccount" $ \obj -> do
-    gaAddress <- obj .: "address"
-    gaVerifyKeys <- obj .: "accountKeys"
-    gaBalance <- obj .: "balance"
-    Versioned{..} <- obj .: "credential"
-    unless (vVersion == 0) $ fail "Only V0 credentials supported in genesis."
-    gaCredentialFull <- parseJSON vValue
-    gaCredential <- case ID.values gaCredentialFull of
-      Nothing -> fail "Account credential is malformed."
-      Just gaCredential -> return gaCredential
-    gaBaker <- obj .:? "baker"
-    -- Check that bakers do not stake more than their balance.
-    case gaBaker of
-      Just gb | gbStake gb > gaBalance -> fail "Stake exceeds balance"
-      _ -> return ()
-    return GenesisAccount{..}
-
-data GenesisChainParameters = GenesisChainParameters {
-    -- |Election difficulty parameter.
-    gcpElectionDifficulty :: !ElectionDifficulty,
-    -- |Euro:Energy rate.
-    gcpEuroPerEnergy :: !ExchangeRate,
-    -- |uGTU:Euro rate.
-    gcpMicroGTUPerEuro :: !ExchangeRate,
-    -- |Number of additional epochs that bakers must cool down when
-    -- removing stake. The cool-down will effectively be 2 epochs
-    -- longer than this value, since at any given time, the bakers
-    -- (and stakes) for the current and next epochs have already
-    -- been determined.
-    gcpBakerExtraCooldownEpochs :: !Epoch,
-    -- |LimitAccountCreation: the maximum number of accounts
-    -- that may be created in one block.
-    gcpAccountCreationLimit :: !CredentialsPerBlockLimit,
-    -- |Reward parameters.
-    gcpRewardParameters :: !RewardParameters,
-    -- |Foundation account address.
-    gcpFoundationAccount :: !AccountAddress
-}
-
-instance FromJSON GenesisChainParameters where
-  parseJSON = withObject "GenesisChainParameters" $ \v ->
-    GenesisChainParameters
-      <$> v .: "electionDifficulty"
-      <*> v .: "euroPerEnergy"
-      <*> v .: "microGTUPerEuro"
-      <*> v .: "bakerCooldownEpochs"
-      <*> v .: "accountCreationLimit"
-      <*> v .: "rewardParameters"
-      <*> v .: "foundationAccount"
-
-instance ToJSON GenesisChainParameters where
-  toJSON GenesisChainParameters{..} = object [
-      "electionDifficulty" AE..= gcpElectionDifficulty,
-      "euroPerEnergy" AE..= gcpEuroPerEnergy,
-      "microGTUPerEuro" AE..= gcpMicroGTUPerEuro,
-      "bakerCooldownEpochs" AE..= gcpBakerExtraCooldownEpochs,
-      "accountCreationLimit" AE..= gcpAccountCreationLimit,
-      "rewardParameters" AE..= gcpRewardParameters,
-      "foundationAccount" AE..= gcpFoundationAccount
-    ]
-
--- 'GenesisParameters' provides a convenient abstraction for
--- constructing 'GenesisData'.
-data GenesisParametersV2 = GenesisParametersV2 {
-    gpGenesisTime :: Timestamp,
-    gpSlotDuration :: Duration,
-    gpLeadershipElectionNonce :: LeadershipElectionNonce,
-    gpEpochLength :: EpochLength,
-    gpFinalizationParameters :: FinalizationParameters,
-    gpCryptographicParameters :: CryptographicParameters,
-    gpIdentityProviders :: IdentityProviders,
-    gpAnonymityRevokers :: AnonymityRevokers,
-    -- |Initial accounts. Since an account can be a baker, it is important that the
-    -- order of the accounts matches the assigned baker ids.
-    gpInitialAccounts :: [GenesisAccount],
-    -- |Maximum total energy that can be consumed by the transactions in a block
-    gpMaxBlockEnergy :: Energy,
-    -- |The initial update authorizations
-    gpAuthorizations :: Authorizations,
-    -- |The initial (updatable) chain parameters
-    gpChainParameters :: GenesisChainParameters
-}
-
-instance FromJSON GenesisParametersV2 where
-    parseJSON = withObject "GenesisParameters" $ \v -> do
-        gpGenesisTime <- v .: "genesisTime"
-        gpSlotDuration <- v .: "slotDuration"
-        gpLeadershipElectionNonce <- v .: "leadershipElectionNonce"
-        gpEpochLength <- Slot <$> v .: "epochLength"
-        when(gpEpochLength == 0) $ fail "Epoch length should be non-zero"
-        gpFinalizationParameters <- v .: "finalizationParameters"
-        gpCryptographicParameters <- v .: "cryptographicParameters"
-        gpIdentityProviders <- v .:? "identityProviders" .!= emptyIdentityProviders
-        gpAnonymityRevokers <- v .:? "anonymityRevokers" .!= emptyAnonymityRevokers
-        gpInitialAccounts <- v .:? "initialAccounts" .!= []
-        let hasBaker GenesisAccount{gaBaker=Nothing} = False
-            hasBaker _ = True
-        unless (any hasBaker gpInitialAccounts) $ fail "Must have at least one baker at genesis"
-        gpMaxBlockEnergy <- v .: "maxBlockEnergy"
-        gpAuthorizations <- v .: "updateAuthorizations"
-        gpChainParameters <- v .: "chainParameters"
-        let facct = gcpFoundationAccount gpChainParameters
-        unless (any ((facct ==) . gaAddress) gpInitialAccounts) $
-          fail $ "Foundation account (" ++ show facct ++ ") is not in initialAccounts"
-        return GenesisParametersV2{..}
-
-type GenesisParameters = GenesisParametersV2
-
-genesisParametersVersion :: Version
-genesisParametersVersion = 2
--}
 
 -- |Implementation-defined parameters, such as block size. They are not
 -- protocol-level parameters hence do not fit into 'GenesisParameters'.
