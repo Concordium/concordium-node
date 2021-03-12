eed3942e08c645d9cb1cd7cb793f1bbef1fdfb65
Javier Sagredo <js@concordium.com>
2021-03-12 16:28:46 +0100

    Fix other tests

--- concordium-consensus/tests/scheduler/SchedulerTests/RandomBakerTransactions.hs
+++ concordium-consensus/tests/scheduler/SchedulerTests/RandomBakerTransactions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, NumericUnderscores #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
 module SchedulerTests.RandomBakerTransactions where
@@ -7,6 +7,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Control.Monad
 
 import qualified Concordium.Scheduler.EnvironmentImplementation as EI
@@ -31,6 +32,8 @@ import Concordium.GlobalState.DummyData
 import Concordium.Crypto.DummyData
 
 import SchedulerTests.Helpers
+
+import qualified Data.List as List
 
 -- | The amount of energy to deposit in every test transaction.
 energy :: Energy
@@ -84,83 +87,128 @@ updateBaker :: Model -> Gen (TransactionJSON, Model)
 updateBaker m0 = do
         (bkrAcct, (kp, nonce, currentlyBaker)) <- elements $ Map.toList $ m0 ^. mAccounts
         let toTJSON p = TJSON {
-            payload = p,
-            metadata = makeDummyHeader bkrAcct nonce energy,
-            keys = [(0,[(0, kp)])]
-        }
-        let changeStakeFail = case currentlyBaker of
-                Baker _ -> id
-                _ -> mRejects %~ ((bkrAcct, nonce) :)
-        let changeBakerFail = case currentlyBaker of
-                NoBaker -> mRejects %~ ((bkrAcct, nonce) :)
-                _ -> id
-        let removeBaker = do
-                let changeBakerState (Baker _) = ReducedRemoved
-                    changeBakerState s = s
+              payload = p,
+              metadata = makeDummyHeader bkrAcct nonce energy,
+              keys = [(0,[(0, kp)])]
+              }
+        -- operations on the model
+        let reject :: Model -> Model
+            reject = mRejects %~ ((bkrAcct, nonce) :)
+            -- if stake is under required threshold, this transaction will be rejected
+            notEnoughStakeFail :: Amount -> Model -> Model
+            notEnoughStakeFail s =
+             if s < 300_000_000_000
+              then reject
+              else id
+            -- if we try to change the stake of a non-existing baker, this transaction should be rejected
+            mustBeBaker :: Model -> Model
+            mustBeBaker = case currentlyBaker of
+              Baker _ -> id
+              _ -> reject
+            mustBeNotBaker :: Model -> Model
+            mustBeNotBaker = case currentlyBaker of
+              NoBaker -> id
+              _ -> reject
+            rejectIfBaker :: Model -> Model
+            rejectIfBaker = case currentlyBaker of
+              Baker _ -> reject
+              _ -> id
+            rejectIfNotBaker :: Model -> Model
+            rejectIfNotBaker = case currentlyBaker of
+              NoBaker -> reject
+              _ -> id
+            -- increment the nonce on this account
+            incrementNonce :: Model -> Model
+            incrementNonce = mAccounts . ix bkrAcct %~ _2 +~ 1
+            -- change the state of the baker with a provided function
+            changeBakerState :: (BakerStatus -> BakerStatus) -> Model -> Model
+            changeBakerState f = mAccounts . ix bkrAcct %~ _3 %~ f
+
+
+        let removeBaker, updateBakerStake, updateBakerRestakeEarnings, updateBakerKeys, addBaker :: Gen (TransactionJSON, Model)
+            removeBaker =
+              let update (Baker _) = ReducedRemoved
+                  update s = s
+              in
                 return (
-                    toTJSON RemoveBaker,
-                    m0
-                    & mAccounts . ix bkrAcct %~ ((_2 +~ 1) . (_3 %~ changeBakerState))
-                    & changeStakeFail)
+                toTJSON RemoveBaker,
+                m0
+                  & incrementNonce
+                  & changeBakerState update
+                  & mustBeBaker)
+
             updateBakerStake = do
-                newStake <- elements [0,1,10,100,1000,10000,100000,1000000]
-                let changeBakerState (Baker v)
-                        | v > newStake = ReducedRemoved
-                        | otherwise = Baker newStake
-                    changeBakerState s = s
-                return (
-                    toTJSON (UpdateBakerStake newStake),
-                    m0
-                    & mAccounts . ix bkrAcct %~ ((_2 +~ 1) . (_3 %~ changeBakerState))
-                    & changeStakeFail
-                    )
+              newStake <- elements [0,100000,100_000_000_000,300_000_000_000,300_000_000_100]
+              let update (Baker v)
+                    -- if stake is decreased and under threshold, no change is done
+                    | v > newStake, newStake < 300_000_000_000 = Baker v
+                    -- if stake is decreased, change to reduceremoved
+                    | v > newStake = ReducedRemoved
+                    -- otherwise, just store the new stake
+                    | otherwise = Baker newStake
+                  update s = s
+              return (
+                  toTJSON (UpdateBakerStake newStake),
+                  m0
+                    & incrementNonce
+                    & changeBakerState update
+                    & mustBeBaker
+                    & notEnoughStakeFail newStake)
+
             updateBakerRestakeEarnings = do
                 restake <- arbitrary
                 return (
-                    toTJSON (UpdateBakerRestakeEarnings restake),
-                    m0
-                    & mAccounts . ix bkrAcct . _2 +~ 1
-                    & changeBakerFail
-                    )
-            updateBakerKeys = do
+                  toTJSON (UpdateBakerRestakeEarnings restake),
+                  m0
+                    & incrementNonce
+                    & rejectIfNotBaker)
+
+            updateBakerKeys =
                 let (fbkr, electionSecretKey, signKey, aggregationKey) = mkFullBaker (m0 ^. mNextSeed) 0
                     bkr = fbkr ^. bakerInfo
-                return (toTJSON UpdateBakerKeys{
-                        bElectionVerifyKey = bkr ^. bakerElectionVerifyKey,
-                        bElectionSecretKey = electionSecretKey,
-                        bSignVerifyKey = bkr ^. bakerSignatureVerifyKey,
-                        bSignSecretKey = signKey,
-                        bAggregateVerifyKey = bkr ^. bakerAggregationVerifyKey,
-                        bAggregateSecretKey = aggregationKey
-                        },
-                        m0
-                        & mAccounts . ix bkrAcct . _2 +~ 1
-                        & mNextSeed +~ 1
-                        & changeBakerFail)
+                in do
+                  return (toTJSON UpdateBakerKeys{
+                             bElectionVerifyKey = bkr ^. bakerElectionVerifyKey,
+                             bElectionSecretKey = electionSecretKey,
+                             bSignVerifyKey = bkr ^. bakerSignatureVerifyKey,
+                             bSignSecretKey = signKey,
+                             bAggregateVerifyKey = bkr ^. bakerAggregationVerifyKey,
+                             bAggregateSecretKey = aggregationKey
+                             },
+                          m0
+                           & incrementNonce
+                           & mNextSeed +~ 1
+                           & rejectIfNotBaker)
+
             addBaker = do
                 let (fbkr, electionSecretKey, signKey, aggregationKey) = mkFullBaker (m0 ^. mNextSeed) 0
                     bkr = fbkr ^. bakerInfo
-                initStake <- elements [0,1,10,100,1000,10000,100000,1000000]
+
+                initStake <- elements [0,100000,100_000_000_000,300_000_000_000,300_000_000_100]
+                let update NoBaker =
+                      if initStake < 300_000_000_000
+                      then NoBaker
+                      else Baker initStake
+                    update s = s
                 restake <- arbitrary
-                let updateBakerState NoBaker = Baker initStake
-                    updateBakerState s = s
+
                 return (toTJSON AddBaker{
-                        bElectionVerifyKey = bkr ^. bakerElectionVerifyKey,
-                        bElectionSecretKey = electionSecretKey,
-                        bSignVerifyKey = bkr ^. bakerSignatureVerifyKey,
-                        bSignSecretKey = signKey,
-                        bAggregateVerifyKey = bkr ^. bakerAggregationVerifyKey,
-                        bAggregateSecretKey = aggregationKey,
-                        bInitialStake = initStake,
-                        bRestakeEarnings = restake
-                        },
-                        m0
-                        & mAccounts . ix bkrAcct %~ ((_2 +~ 1) . (_3 %~ updateBakerState))
-                        & mNextSeed +~ 1
-                        & case currentlyBaker of
-                            NoBaker -> id
-                            _ -> mRejects %~ ((bkrAcct, nonce) :)
-                        )
+                             bElectionVerifyKey = bkr ^. bakerElectionVerifyKey,
+                             bElectionSecretKey = electionSecretKey,
+                             bSignVerifyKey = bkr ^. bakerSignatureVerifyKey,
+                             bSignSecretKey = signKey,
+                             bAggregateVerifyKey = bkr ^. bakerAggregationVerifyKey,
+                             bAggregateSecretKey = aggregationKey,
+                             bInitialStake = initStake,
+                             bRestakeEarnings = restake
+                             },
+                          m0
+                           & incrementNonce
+                           & changeBakerState update
+                           & mNextSeed +~ 1
+                           & notEnoughStakeFail initStake
+                           & mustBeNotBaker)
+
         oneof [removeBaker,updateBakerStake,updateBakerRestakeEarnings,updateBakerKeys,addBaker]
 
 simpleTransfer :: Model -> Gen (TransactionJSON, Model)
@@ -208,7 +256,7 @@ testTransactions = forAll makeTransactions (ioProperty . tt)
             let checkResults = do
                     invariantBlockState gs (finState ^. EI.schedulerExecutionCosts)
                     unless (null ftFailed) $ Left $ "some transactions failed: " ++ show ftFailed
-                    checkRejects rejs predRejects
+                    checkRejects rejs $ map head $ List.group predRejects
             case checkResults of
                 Left f -> return $ counterexample f False
                 Right _ -> return $ property True
