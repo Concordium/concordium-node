10d9bac4d48c0342a31aee6ef0866021a71dccae
Daniel Tschudi <tschudid@gmail.com>
2021-04-13 18:16:07 +0200

    code simplification

--- concordium-node/src/connection/mod.rs
+++ concordium-node/src/connection/mod.rs
@@ -639,19 +639,15 @@ impl Connection {
         let peer_list_resp = match self.handler.peer_type() {
             PeerType::Bootstrapper => {
                 // select random nodes that are post-handshake
-                let get_random_nodes = || -> Fallible<Vec<P2PPeer>> {
-                    Ok(read_or_die!(self.handler.buckets())
-                        .get_random_nodes(
-                            requestor,
-                            self.handler.config.bootstrapper_peer_list_size,
-                            &nets,
-                        )
-                        .iter()
-                        .filter_map(RemotePeer::peer)
-                        .collect())
-                };
-
-                let random_nodes = get_random_nodes()?;
+                let random_nodes = read_or_die!(self.handler.buckets())
+                    .get_random_nodes(
+                        requestor,
+                        self.handler.config.bootstrapper_peer_list_size,
+                        &nets,
+                    )
+                    .iter()
+                    .filter_map(RemotePeer::peer)
+                    .collect::<Vec<_>>();
 
                 if !random_nodes.is_empty()
                     && random_nodes.len()
