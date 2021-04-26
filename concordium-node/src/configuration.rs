4a855771c9eb8cb4af686ac5519669615ea95101
Aleš Bizjak <ab@concordium.com>
2021-04-26 12:43:53 +0200

    Bump version.
    This node is incompatible with the last one.

--- concordium-node/src/configuration.rs
+++ concordium-node/src/configuration.rs
@@ -28,7 +28,7 @@ pub const APP_INFO: AppInfo = AppInfo {
 /// When we reach version 1 we should stick to major versions being for breaking
 /// changes.
 pub(crate) fn is_compatible_version(other: &semver::Version) -> bool {
-    other.major == 0 && other.minor == 6
+    other.major == 0 && other.minor == 7
 }
 
 /// Check that the other wire version is compatible with ours. See
