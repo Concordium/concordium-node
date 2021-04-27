a70b169cf8f84332350691321e06c05c32e97e5c
Aleš Bizjak <ab@concordium.com>
2021-04-27 12:31:39 +0200

    Bugfixes.
    Actually do not redistribute transactions if consensus says so.
    
    Fix configuration parsing.

--- concordium-node/src/configuration.rs
+++ concordium-node/src/configuration.rs
@@ -228,6 +228,7 @@ pub struct BakerConfig {
         long = "import-blocks-from",
         help = "Path to a file exported by the database exporter"
     )]
+    pub import_path: Option<String>,
     #[structopt(
         long = "max-expiry-duration",
         help = "Maximum allowed time difference between now and a transaction's expiry time in \
@@ -235,7 +236,6 @@ pub struct BakerConfig {
         default_value = "7200"
     )]
     pub max_time_to_expiry: u64,
-    pub import_path: Option<String>,
     #[structopt(long = "baker-credentials-file", help = "Path to the baker credentials file")]
     pub baker_credentials_file: Option<PathBuf>,
     #[structopt(
