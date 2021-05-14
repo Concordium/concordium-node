f911f1385c04a53a69dd08b05b879cba4a377af2
Aleš Bizjak <ab@concordium.com>
2021-05-14 15:20:18 +0200

    Add scripts for building static binaries on ubuntu versions.

---
+++ scripts/static-binaries/build-on-ubuntu.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Build concordium-node and related binaries in a format suitable for distribution.
+# The binaries are linked mostly statically, apart from a few common libraries that
+# seem difficult to impossible to link statically. These are libc, libpq.
+
+set -euxo pipefail
+
+# The script must be invoked with the concordium-node source directory as working directory.
+
+# Expected parameters:
+# - EXTRA_FEATURES, features to use in addition to `static` when building the node, e.g., collector. Comma separated list.
+
+extra_features="$EXTRA_FEATURES"
+
+flatbuffers_version=v1.12.1
+rust_toolchain_version=1.45.2
+
+# Install dependencies.
+
+apt-get update && \
+DEBIAN_FRONTEND=noninteractive apt-get -y install \
+	git \
+	curl \
+	libunbound-dev \
+	libprotobuf-dev \
+	libssl-dev \
+	protobuf-compiler \
+	cmake \
+	pkg-config \
+	libnuma-dev \
+	libgmp-dev \
+	liblmdb0 \
+	postgresql-server-dev-all \
+	locales \
+	liblmdb-dev \
+    build-essential
+
+# Install Rust.
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+rustup set profile minimal
+rustup default "$rust_toolchain_version"
+
+# Install flatbuffers.
+
+# Must be kept in sync with 'base-images/base.Dockerfile'.
+# See 'https://gitlab.com/Concordium/devops/-/commit/f41ac413c3583ec53d06a2c0fe5c8795e35f1a46'.
+git clone https://github.com/google/flatbuffers.git
+( cd flatbuffers && git checkout "$flatbuffers_version" && cmake -G "Unix Makefiles" && make -j$(nproc) && make install )
+
+# Build all the binaries and copy them to ./bin/
+cargo generate-lockfile --manifest-path concordium-node/Cargo.toml
+cargo install --path $(pwd)/concordium-node/ --locked --features=static,$extra_features --root $(pwd)
+# Strip all the generated binaries to remove debugging and unused symbols
+strip $(pwd)/bin/*
---
+++ scripts/static-binaries/build-static-binaries.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+ubuntu_version="${UBUNTU_VERSION}"
+static_libraries_image_tag="${STATIC_LIBRARIES_IMAGE_TAG}"
+ghc_version="${GHC_VERSION}"
+extra_features=${EXTRA_FEATURES:-""}
+
+# Setting '--no-cache' because we clone repos from the build script.
+docker build \
+	--build-arg ubuntu_version="$ubuntu_version" \
+	--build-arg static_libraries_image_tag="$static_libraries_image_tag" \
+	--build-arg ghc_version="$ghc_version" \
+    --build-arg extra_features="$extra_features" \
+	--label ubuntu_version="$ubuntu_version" \
+	--label static_libraries_image_tag="$static_libraries_image_tag" \
+	--label ghc_version="$ghc_version" \
+    --label extra_features="$extra_features" \
+	-f "scripts/static-binaries/static-binaries.Dockerfile" \
+	-t static-node-binaries \
+	"."
+
+echo "The binaries are ready inside the 'static-node-binaries' image in the '/build/bin/' directory."
+echo "They can be obtained by mounting the directory using 'docker run -v $OUT_DIR:/out/ static-node-binaries' cp ..."
+echo "and copying the files."
