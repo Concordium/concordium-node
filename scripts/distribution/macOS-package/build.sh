d0082ae74c2fe77c394da7c5209a8693eb50c6cc
Kasper Dissing Bargsteen <kb@concordium.com>
2021-07-21 15:03:17 +0200

    Reorganize

--- scripts/distribution/macOS-package/build.sh
+++ scripts/distribution/macOS-package/build.sh
@@ -10,7 +10,7 @@ readonly developerIdInstaller="Developer ID Installer: Concordium Software Aps (
 readonly GREEN='\033[0;32m'
 readonly NC='\033[0m' # No Color
 
-step () {
+logInfo () {
     printf "\n${GREEN}$@${NC}\n"
 }
 
@@ -23,70 +23,70 @@ readonly consensusDir="$macPackageDir/../../../concordium-consensus"
 readonly distDir="$macPackageDir/dist"
 readonly tmpDir="$macPackageDir/tmp"
 readonly macdylibbundlerDir="$tmpDir/macdylibbundler-1.0.0"
+readonly installDir="/Library/concordium-node/$version"
 
 
-# Cleanup
-[ -d "$distDir" ] && step "Cleaning '$distDir' folder" && rm -r "$distDir"
+function cleanOldDist() {
+    [ -d "$distDir" ] && logInfo "Cleaning '$distDir' folder" && rm -r "$distDir"
+}
 
 
-# Compile consensus
-cd "$consensusDir"
-step "Building Consensus..."
-stack build
-step "Done"
+function compileConsensus() {
+    cd "$consensusDir"
+    logInfo "Building Consensus..."
+    stack build
+    logInfo "Done"
+}
+
+function compileNodeAndCollector() {
+    cd "$nodeDir"
+    logInfo "Building Node and Collector..."
+    cargo build --bin concordium-node --bin node-collector --features collector --release
+    logInfo "Done"
+}
+
+function compile() {
+    compileConsensus
+    compileNodeAndCollector
+}
+
+function copyBinaries() {
+    logInfo "Copy concordium-node and node-collector binaries to '$distDir'.."
+    mkdir "$distDir"
+    cp "$nodeDir/target/release/concordium-node" "$distDir"
+    cp "$nodeDir/target/release/node-collector" "$distDir"
+    logInfo "Done"
+}
+
+function downloadGenesis() {
+    logInfo "Downloading genesis.dat"
+    curl -sSL "https://distribution.mainnet.concordium.software/data/genesis.dat" > "$distDir/genesis.dat"
+    logInfo "Done"
+}
 
 
-# Compile node
-cd "$nodeDir"
-step "Building Node and Collector..."
-cargo build --bin concordium-node --bin node-collector --features collector --release
-step "Done"
+function getDylibbundler() {
+    logInfo "Getting dylibbundler..."
 
+    if test -f "$macdylibbundlerDir/dylibbundler"
+    then
+        logInfo "Skipped: already exists"
+    else
+        logInfo " -- Downloading..."
+        mkdir "$tmpDir"
+        cd "$macPackageDir"
+        curl -sSL "https://github.com/auriamg/macdylibbundler/archive/refs/tags/1.0.0.zip" > "$tmpDir/dylibbundler.zip" \
+                    && logInfo " -- Unzipping..." \
+                    && cd "$tmpDir" \
+                    && unzip "dylibbundler.zip" \
+                    && logInfo " -- Building..." \
+                    && cd "$macdylibbundlerDir" \
+                    && make
+        logInfo "Done"
+    fi
+}
 
-# Get concordium-node binary
-step "Copy concordium-node and node-collector binaries to '$distDir'.."
-mkdir "$distDir"
-cp "$nodeDir/target/release/concordium-node" "$distDir"
-cp "$nodeDir/target/release/node-collector" "$distDir"
-step "Done"
-
-step "Downloading genesis.dat"
-curl -sSL "https://distribution.mainnet.concordium.software/data/genesis.dat" > "$distDir/genesis.dat"
-step "Done"
-
-
-# Fetch dylibbundler and build it
-step "Getting dylibbundler..."
-
-if test -f "$macdylibbundlerDir/dylibbundler"
-then
-    step "Skipped: already exists"
-else
-    step " -- Downloading..."
-    mkdir "$tmpDir"
-    cd "$macPackageDir"
-    curl -sSL "https://github.com/auriamg/macdylibbundler/archive/refs/tags/1.0.0.zip" > "$tmpDir/dylibbundler.zip" \
-                && step " -- Unzipping..." \
-                && cd "$tmpDir" \
-                && unzip "dylibbundler.zip" \
-                && step " -- Building..." \
-                && cd "$macdylibbundlerDir" \
-                && make
-    step "Done"
-fi
-
-
-# Collect dylibs
-step "Collecting dylibs with dylibbundler"
-
-concordiumDylibDir=$(stack --stack-yaml "$consensusDir/stack.yaml" path --local-install-root)"/lib/$ghcVariant"
-stackSnapshotDir=$(stack --stack-yaml "$consensusDir/stack.yaml" path --snapshot-install-root)"/lib/$ghcVariant"
-stackLibDirs=$(find "$(stack --stack-yaml "$consensusDir/stack.yaml" ghc -- --print-libdir)" -maxdepth 1 -type d | awk '{print "-s "$0}')
-readonly concordiumDylibDir
-readonly stackSnapshotDir
-readonly stackLibDirs
-
-function collectDylibs() {
+function collectDylibsFor() {
     local fileToFix=${1:?"Missing file to fix with dylibbundler"};
     cd "$distDir"
     "$macdylibbundlerDir/dylibbundler" --fix-file "$fileToFix" --bundle-deps --dest-dir "./libs" --install-path "@executable_path/libs/" --overwrite-dir \
@@ -95,12 +95,29 @@ function collectDylibs() {
         $stackLibDirs # Unquoted on purpose to use as arguments correctly
 }
 
-step " -- Processing concordium-node"
-collectDylibs "$distDir/concordium-node"
-step " -- Processing node-collector"
-collectDylibs "$distDir/node-collector"
+function collectDylibs() {
+    logInfo "Collecting dylibs with dylibbundler"
 
-step "Done"
+    concordiumDylibDir=$(stack --stack-yaml "$consensusDir/stack.yaml" path --local-install-root)"/lib/$ghcVariant"
+    stackSnapshotDir=$(stack --stack-yaml "$consensusDir/stack.yaml" path --snapshot-install-root)"/lib/$ghcVariant"
+    stackLibDirs=$(find "$(stack --stack-yaml "$consensusDir/stack.yaml" ghc -- --print-libdir)" -maxdepth 1 -type d | awk '{print "-s "$0}')
+    readonly concordiumDylibDir
+    readonly stackSnapshotDir
+    readonly stackLibDirs
+
+    logInfo " -- Processing concordium-node"
+    collectDylibsFor "$distDir/concordium-node"
+    logInfo " -- Processing node-collector"
+    collectDylibsFor "$distDir/node-collector"
+
+    logInfo "Done"
+}
+
+# TODO
+# function sign() {
+#
+# }
+
 function buildPackage {
     logInfo "Building package"
     cd "$macPackageDir"
@@ -123,3 +140,16 @@ function buildProduct {
         concordium-node-signed.pkg
 }
 
+function main() {
+
+    cleanOldDist
+    compile
+    copyBinaries
+    downloadGenesis
+    getDylibbundler
+    collectDylibs
+    buildPackage
+    buildProduct
+}
+
+main
