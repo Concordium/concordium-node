962329c3cbbdf24bef93f1c641ce491195e51e4d
Michael Bisgaard Olesen <mo@concordium.com>
2021-07-29 23:43:31 +0200

    Fixes - working now!

--- docker-compose/concordium-node-entrypoint.sh
+++ docker-compose/concordium-node-entrypoint.sh
@@ -5,9 +5,12 @@ set -euxo pipefail
 # Ensure that expected dirs exist.
 mkdir -p "$CONCORDIUM_NODE_CONFIG_DIR" "$CONCORDIUM_NODE_DATA_DIR"
 
-if [ -n "$COPY_GENESIS_DAT_FILE" ]; then
-	# Better solution: support flag in node to set location.
-	cp "$COPY_GENESIS_DAT_FILE" "$CONCORDIUM_NODE_DATA_DIR"
+if [ -n "$GENESIS_DATA_PATH" ]; then
+	# Copy 'genesis.dat' - better solution: support flag in concordium-node to set location.
+	cp "$GENESIS_DATA_PATH/genesis.dat" "$CONCORDIUM_NODE_DATA_DIR"
+	# Select unique baker credentials file.
+	id="$(curl "$BAKER_ID_GEN_NEXT_ID_URL")"
+	export CONCORDIUM_NODE_BAKER_CREDENTIALS_FILE="$GENESIS_DATA_PATH/bakers/baker-$id-credentials.json"
 fi
 
 # Run binary - inherits env vars and args.
