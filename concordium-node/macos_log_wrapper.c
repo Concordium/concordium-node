285041363930b58f16f046a6e3f27cf6f8d2951a
Kasper Dissing Bargsteen <kb@concordium.com>
2021-09-06 15:29:05 +0200

    Document wrapping of macro

--- concordium-node/macos_log_wrapper.c
+++ concordium-node/macos_log_wrapper.c
@@ -1,5 +1,6 @@
 #include <os/log.h>
 
+// Wrap os_log_with_type as it is a macro which cannot be used directly through FFI.
 void wrapped_os_log_with_type(os_log_t log, os_log_type_t type, const char* message) {
     os_log_with_type(log, type, "%{public}s", message);
 }
