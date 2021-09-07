fb26b405081795e8a0ab628e8b06570f447e55f7
Thomas Dinsdale-Young <ty@concordium.com>
2021-09-07 13:25:35 +0200

    Set default rust version in windows build script.

--- scripts/distribution/windows/build-all.ps1
+++ scripts/distribution/windows/build-all.ps1
@@ -5,6 +5,8 @@ Write-Output "cargo version: $(cargo --version)"
 Write-Output "flatc version: $(flatc --version)"
 Write-Output "protoc version: $(protoc --version)"
 
+# Set the default rust toolchain so that consensus rust dependencies use it.
+rustup default $rustVersion-x86_64-pc-windows-gnu
 
 Write-Output "Building consensus..."
 stack build
