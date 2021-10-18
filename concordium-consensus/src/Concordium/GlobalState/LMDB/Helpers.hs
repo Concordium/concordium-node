e9321f814b81ce72e23e5369e5187f1ad1a44579
Aleš Bizjak <ab@concordium.com>
2021-10-18 16:10:44 +0200

    Some documentation of the lock states.

--- concordium-consensus/src/Concordium/GlobalState/LMDB/Helpers.hs
+++ concordium-consensus/src/Concordium/GlobalState/LMDB/Helpers.hs
@@ -33,40 +33,85 @@ import Foreign.Storable
 import Lens.Micro.Platform
 
 
--- Implementation of reader-writer locks that strongly prefer writers.
-
+-- |State of a reader-writer lock.
 data RWState =
    -- |Nobody has acquired the lock.
-  Free { waitingWriters :: !Word64 }
-  -- |The given number of readers have acquired the lock.
+  Free {
+      -- |The lock is not acquired, but there might be pending writers that want to acquire it.
+      waitingWriters :: !Word64
+      }
+  -- |There is at least one active reader.
   | ReadLocked {
+      -- |The number of readers that are currently active.
       readers :: !Word64,
+      -- |The number of pending writers.
       waitingWriters :: !Word64
       }
   -- |The lock is acquired by a single writer.
   | WriteLocked {
+      -- |The number of writers that are pending (that is, currently blocked on this lock).
       waitingWriters :: !Word64
       }
     deriving(Show)
 
--- |A reader writer lock with a bias for writers. If there are waiting writers
--- readers will block.
+-- |A reader-writer lock that strongly prefer writers. More precisely this means the following
+-- - readers and writers are mutually exclusive
+-- - multiple readers may hold the lock at the same time if there is no writer
+-- - at most one writer may hold the lock
+--
+-- If a writer tries to acquire a lock it will either
+-- - succeed if there are no current readers or writers
+-- - block after recording the intent to lock. While there are pending writers no new readers can acquire the lock.
+--
+-- If multiple writers are blocking on the lock they will be served in an
+-- unspecified order and in principle it is possible that with heavy write
+-- contention some writers would be starved. This is not the case for the
+-- use-case we have.
+--
+-- Let ⊤ mean that the MVar is full and ⊥ that it is empty. The fields of the lock satisfy the following
+-- properties.
+-- - there are exactly waitingWriters threads blocking on acquireWrite
+-- - rwlState == Free if and only if rwlReadLock == ⊤ and rwlWriteLock == ⊤
+-- - rwlReadLock == ⊥ if and only if there is an active reader.
+-- - rwlWriteLock == ⊥ if and only if there is an active writer.
+--
+-- Transitions between states are governed by the following transition system
+-- where AW/RW and AR/RR mean acquire write, release write and acquire read,
+-- release read, respectively. The WR and WW mean that the thread that
+-- executed the transition is blocked waiting for rwlReadLock and rwlWriteLock MVar to be full.
+-- (Free 0, ⊤, ⊤) -AR-> (ReadLocked 1 0, ⊥, ⊤)
+-- (Free (n+1), ⊤, ⊤) -AR-> (Free (n+1), ⊤, ⊤)
+-- (Free 0, ⊤, ⊤) -AW-> (WriteLocked 0, ⊤, ⊥)
+-- (Free (n+1), ⊤, ⊤) -AW-> (WriteLocked n, ⊤, ⊥)
+--
+-- (ReadLocked n 0, ⊥, ⊤) -AR-> (ReadLocked (n+1) 0, ⊥, ⊤)
+-- (ReadLocked n (m+1), ⊥, ⊤) -AR-> (ReadLocked n (m+1), ⊥, ⊤), WR
+-- (ReadLocked n m, ⊥, ⊤) -AW-> (ReadLocked n (m+1), ⊥, ⊤), WW
+-- (ReadLocked 1 m, ⊥, ⊤) -RR-> (Free m, ⊤, ⊤)
+-- (ReadLocked (n+1) m, ⊥, ⊤) -RR-> (ReadLocked n m, ⊥, ⊤)
+--
+-- (WriteLocked n, ⊤, ⊥) -AR-> (WriteLocked n, ⊤, ⊥)
+-- (WriteLocked n, ⊤, ⊥) -AW-> (WriteLocked (n+1), ⊤, ⊥), WR
+-- (WriteLocked n, ⊤, ⊥) -RW-> (Free n, ⊤, ⊤), WW
+--
+-- No other state should be reachable.
 data RWLock = RWLock {
-  state :: !(MVar RWState),
-  -- |Used to signal waiting readers to wake up. This is empty when there is at
-  -- least one active reader and full otherwise.
-  readLock :: !(MVar ()),
-  -- |Used to signal waiting writers to wake up. This is empty when there is an
-  -- active writer, and full otherwise.
-  writeLock :: !(MVar ())
+  -- |The state the lock is currently in.
+  rwlState :: !(MVar RWState),
+  -- |An MVar used to signal waiting readers to wake up. This is empty when
+  -- there is at least one active reader and full otherwise.
+  rwlReadLock :: !(MVar ()),
+  -- |An MVar used to signal waiting writers to wake up. This is empty when
+  -- there is an active writer, and full otherwise.
+  rwlWriteLock :: !(MVar ())
   }
 
 -- |Initialize a lock in the unlocked state.
 initializeLock :: IO RWLock
 initializeLock = do
-  state <- newMVar (Free 0)
-  readLock <- newMVar ()
-  writeLock <- newMVar ()
+  rwlState <- newMVar (Free 0)
+  rwlReadLock <- newMVar ()
+  rwlWriteLock <- newMVar ()
   return RWLock{..}
 
 -- |Acquire a read lock. This will block until there are no pending writers
@@ -74,33 +119,33 @@ initializeLock = do
 acquireRead :: RWLock -> IO ()
 acquireRead RWLock{..} = mask_ go
   where
-    go = takeMVar state >>= \case
+    go = takeMVar rwlState >>= \case
       st@(Free waitingWriters)
         | waitingWriters == 0 -> do
             -- the lock is free and there are no waiting writers. Acquire a read lock.
-            takeMVar readLock
-            putMVar state (ReadLocked 1 0)
+            takeMVar rwlReadLock
+            putMVar rwlState (ReadLocked 1 0)
         | otherwise -> do
             -- the lock is free, but there are waiting writers. We do nothing and try again.
             -- Due to fairness of MVars next time another thread will make progress
             -- so we are going to end up after a finite number of iterations, in a WriteLocked state.
-            putMVar state st
+            putMVar rwlState st
             go
       st@(ReadLocked n waitingWriters)
           | waitingWriters == 0 ->
             -- No waiting writers, add another reader.
-            putMVar state $! ReadLocked (n + 1) 0
+            putMVar rwlState $! ReadLocked (n + 1) 0
           | otherwise -> do
               -- Some readers hold the lock, but there are waiting writers.
               -- We do nothing and wait until there are no more readers and attempt again.
               -- At that point we are likely to end up in WriteLocked state.
-              putMVar state st
-              readMVar readLock
+              putMVar rwlState st
+              readMVar rwlReadLock
               go
       lockState@(WriteLocked _) -> do
         -- There is an active writer. Do nothing and wait until the writer is done.
-        putMVar state lockState
-        readMVar writeLock
+        putMVar rwlState lockState
+        readMVar rwlWriteLock
         go
 
 
@@ -108,45 +153,45 @@ acquireWrite :: RWLock -> IO ()
 acquireWrite RWLock{..} = mask_ $ go False
   where
     -- the boolean flag indicates whether this is a first iteration of the loop (False) or not (True)
-    go b = takeMVar state >>= \case
+    go b = takeMVar rwlState >>= \case
       (Free waitingWriters) -> do
         -- The lock is free, take it.
-        takeMVar writeLock
-        putMVar state $! WriteLocked (waitingWriters - if b then 1 else 0)
+        takeMVar rwlWriteLock
+        putMVar rwlState $! WriteLocked (waitingWriters - if b then 1 else 0)
       (ReadLocked n waitingWriters) -> do
         -- There are active readers. Queue ourselves up and wait until all existing readers
         -- are done. This will block all subsequent readers from acquiring the lock.
-        putMVar state $! ReadLocked n (waitingWriters + if not b then 1 else 0)
-        readMVar readLock
+        putMVar rwlState $! ReadLocked n (waitingWriters + if not b then 1 else 0)
+        readMVar rwlReadLock
         go True
       (WriteLocked waitingWriters) -> do
         -- There is an active writer. Queue ourselves up so that readers are
         -- blocked from acquiring the lock and wait until the current writer is done.
-        putMVar state $! WriteLocked (waitingWriters + if not b then 1 else 0)
-        readMVar writeLock
+        putMVar rwlState $! WriteLocked (waitingWriters + if not b then 1 else 0)
+        readMVar rwlWriteLock
         go True
 
 
 releaseWrite :: RWLock -> IO ()
 releaseWrite RWLock{..} = mask_ $
-   takeMVar state >>= \case
+   takeMVar rwlState >>= \case
     (WriteLocked waitingWriters) -> do
-      putMVar writeLock ()
-      putMVar state (Free waitingWriters)
+      putMVar rwlWriteLock ()
+      putMVar rwlState (Free waitingWriters)
     lockState -> do
-      putMVar state lockState
+      putMVar rwlState lockState
       error $ "releaseWrite: attempting to release while in state: " ++ show lockState
 
 
 releaseRead :: RWLock -> IO ()
 releaseRead RWLock{..} = mask_ $
-  takeMVar state >>= \case
+  takeMVar rwlState >>= \case
     (ReadLocked 1 waitingWriters) -> do
-      putMVar readLock ()
-      putMVar state (Free waitingWriters)
-    (ReadLocked n waitingWriters) -> putMVar state $! ReadLocked (n - 1) waitingWriters
+      putMVar rwlReadLock ()
+      putMVar rwlState (Free waitingWriters)
+    (ReadLocked n waitingWriters) -> putMVar rwlState $! ReadLocked (n - 1) waitingWriters
     lockState -> do
-      putMVar state lockState
+      putMVar rwlState lockState
       error $ "releaseRead: attempting to release read when in state: " ++ show lockState
 
 -- |Acquire the write lock and execute the action. The lock will be released
