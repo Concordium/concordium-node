a1bec53299d8c8ff24523b8222449f758eb456dc
Aleš Bizjak <ab@concordium.com>
2021-11-17 10:56:39 +0100

    Remove obsolete todo.

--- concordium-consensus/src/Concordium/ProtocolUpdate/P2/ProtocolP3.hs
+++ concordium-consensus/src/Concordium/ProtocolUpdate/P2/ProtocolP3.hs
@@ -69,7 +69,6 @@ import Concordium.Kontrol
 
 -- |The hash that identifies a update from P2 to P3 protocol.
 -- This is the hash of the published specification document.
--- TODO: Write the document, insert correct hash.
 updateHash :: SHA256.Hash
 updateHash = read "ec9f7733e872ed0b8f1f386d12c5c725379fc609ce246ffdce28cfb9163ea350"
 
