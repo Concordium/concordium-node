9af573a3cdc699163893556b70e3617d99d04fb2
Cem Eliguzel <ce@concordium.com>
2022-06-20 11:49:18 +0300

    Fixed the issues with the benchmark

--- concordium-consensus/haskell-bench/LRUCache.hs
+++ concordium-consensus/haskell-bench/LRUCache.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Concordium.GlobalState.Persistent.BlobStore
@@ -13,26 +16,25 @@ import Data.Proxy
 import System.Random
 import System.Random.MWC
 import System.Random.MWC.Distributions
+import Control.DeepSeq (NFData (..))
+import GHC.Generics
 
 data CacheOp k v
   = PutItem k v
   | GetItem k
 
-testLRUCache :: LRUCache String -> [CacheKey (LRUCache String)] -> IO ()
-testLRUCache cache keys = do
-  -- cache <- newLRUCache cacheSize :: IO (LRUCache String)
-  let cacheProxy = Proxy :: Proxy (LRUCache String)
+testCache :: forall v. (Cache v, HasCache v v) => v -> [CacheKey v] -> CacheValue v -> Int -> IO ()
+testCache cache keys value txCount = do
+  let cacheProxy = Proxy :: Proxy v
   flip runReaderT cache $ do
-    -- mapM_ (\key -> putCachedValue cacheProxy key "test") keys
-    -- res <- mapM (lookupCachedValue cacheProxy) keys
     res <-
       forM
-        (take 12000 keys)
+        (take txCount (reverse keys))
         ( \key -> do
             r <- lookupCachedValue cacheProxy key
             when (isNothing r) $ do
               liftIO $ threadDelay 500
-              putCachedValue cacheProxy key "test"
+              putCachedValue cacheProxy key value
               return ()
             return r
         )
@@ -40,62 +42,42 @@ testLRUCache cache keys = do
     return ()
 
   return ()
-
-testFIFOCache :: FIFOCache String -> [CacheKey (FIFOCache String)] -> IO ()
-testFIFOCache cache keys = do
-  -- cache <- newFIFOCache cacheSize :: IO (FIFOCache String)
-  let cacheProxy = Proxy :: Proxy (FIFOCache String)
-  flip runReaderT cache $ do
-    -- mapM_ (\key -> putCachedValue cacheProxy key "test") keys
-    res <-
-      forM
-        (take 12000 keys)
-        ( \key -> do
-            r <- lookupCachedValue cacheProxy key
-            when (isNothing r) $ do
-              liftIO $ threadDelay 500
-              putCachedValue cacheProxy key "test"
-              return ()
-            return r
-        )
-    -- liftIO $ print $ length $ filter isNothing res
-    return ()
-
-  return ()
-
-setupEnv :: IO (IO (LRUCache String), IO (FIFOCache String), [BlobRef String])
-setupEnv = do
-  g <- getStdGen
-  mwc <- create
-  let transactions = 500000 -- corresponds to 110K unique accounts
-  let mean = fromIntegral transactions
-  let deviation = fromIntegral transactions / 10 / 2
-  putStrLn $ show mean ++ "-" ++ show deviation
-  xs <- replicateM transactions (normal mean deviation mwc :: IO Double)
-  let keys = map (BlobRef . round) xs
-  return (lruCache keys, fifoCache keys, keys)
-  where
-    cacheSize = 30000
-    lruCache keys = do
-      cache <- newLRUCache cacheSize :: IO (LRUCache String)
-      let lruCacheProxy = Proxy :: Proxy (LRUCache String)
-      flip runReaderT cache $ do
-        mapM_ (\key -> putCachedValue lruCacheProxy key "test") keys
-      return cache
-    fifoCache keys = do
-      cache <- newFIFOCache cacheSize :: IO (FIFOCache String)
-      let fifoCacheProxy = Proxy :: Proxy (FIFOCache String)
-      flip runReaderT cache $ do
-        mapM_ (\key -> putCachedValue fifoCacheProxy key "test") keys
-      return cache
 
 main :: IO ()
 main = do
-  -- print $ length $ nub keys
+  g <- getStdGen
+  mwc <- create
+  let txCount = 500000 -- corresponds to 110K unique accounts
+  let mean = fromIntegral txCount
+  let deviation = fromIntegral txCount / 10 / 2
+  xs <- replicateM txCount (normal mean deviation mwc :: IO Double)
+  let keys = map (BlobRef . round) xs
   defaultMain [
-    env setupEnv $ \ ~(lruCache, fifoCache, keys) -> bgroup "cache" [
-      bench "test LRUCache" $ nfIO $ lruCache >>= flip testLRUCache keys,
-      bench "test FIFOCache" $ nfIO $ fifoCache >>= flip testFIFOCache keys
+    bgroup "cache" [
+      bench "test LRUCache" $ perRunEnv (lruCache keys) $ \ lruCache -> testCache lruCache keys "test" 6000,
+      bench "test FIFOCache" $ perRunEnv (fifoCache keys) $ \ fifoCache -> testCache fifoCache keys "test" 6000
       ]
       ]
+  where
+    cacheSize = 30000
+    lruCache keys = do
+      cache <- newLRUCache cacheSize
+      let lruCacheProxy = Proxy :: Proxy (LRUCache String)
+      flip runReaderT cache $ do
+        mapM_ (\key -> putCachedValue lruCacheProxy key "test") (take (cacheSize*2) keys)
+      return cache
+    fifoCache keys = do
+      cache <- newFIFOCache cacheSize
+      let fifoCacheProxy = Proxy :: Proxy (FIFOCache String)
+      flip runReaderT cache $ do
+        mapM_ (\key -> putCachedValue fifoCacheProxy key "test") (take (cacheSize*2) keys)
+      return cache
 
+instance NFData (LRUCache v) where
+  rnf bs = bs `seq` ()
+
+instance NFData (BlobRef v) where
+  rnf bs = bs `seq` ()
+
+instance NFData (FIFOCache v) where
+  rnf bs = bs `seq` ()
