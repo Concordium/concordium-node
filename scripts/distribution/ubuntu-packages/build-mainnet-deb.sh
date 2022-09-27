9416548495b2a12ae0181ff42679d70e1b2da9ef
Stefan Madsen <snm@concordium.com>
2022-09-27 11:05:30 +0200

    Manuel scripts should inspect specific tag

--- scripts/distribution/ubuntu-packages/build-mainnet-deb.sh
+++ scripts/distribution/ubuntu-packages/build-mainnet-deb.sh
@@ -11,7 +11,7 @@ set -euxo pipefail
 # Set STATIC_BINARIES_IMAGE_TAG to latest if not already set
 export STATIC_BINARIES_IMAGE_TAG="${STATIC_BINARIES_IMAGE_TAG:-latest}"
 
-if ! docker inspect --type=image static-node-binaries > /dev/null 2> /dev/null ; then
+if ! docker inspect --type=image static-node-binaries:$STATIC_BINARIES_IMAGE_TAG > /dev/null 2> /dev/null ; then
     # build static binaries
     export STATIC_LIBRARIES_IMAGE_TAG="${STATIC_LIBRARIES_IMAGE_TAG:-latest}"
     export GHC_VERSION="${GHC_VERSION:-9.0.2}"
