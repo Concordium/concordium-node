3dec5c672bf08ee5e658f7395bcbb34a6d396586
Emil Lai <emil@milkywaypirates.com>
2022-09-30 19:05:36 +0200

    NodeInfo

--- concordium-node/build.rs
+++ concordium-node/build.rs
@@ -455,6 +455,15 @@ fn build_grpc2(proto_root_input: &str) -> std::io::Result<()> {
                 .server_streaming()
                 .build(),
         )
+        .method(
+            tonic_build::manual::Method::builder()
+                .name("get_node_info")
+                .route_name("GetNodeInfo")
+                .input_type("crate::grpc2::types::Empty")
+                .output_type("crate::grpc2::types::NodeInfo")
+                .codec_path("tonic::codec::ProstCodec")
+                .build(),
+        )
         .build();
     // Due to the slightly hacky nature of the RawCodec (i.e., it does not support
     // deserialization) we cannot build the client. But we also don't need it in the
