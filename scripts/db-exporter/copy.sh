221ca1a217fc0b256af17ff3e8a3a80f302a9eab
Stefan Madsen <snm@concordium.com>
2022-10-31 15:22:51 +0100

    spelling

--- scripts/db-exporter/copy.sh
+++ scripts/db-exporter/copy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 rm -rf /out/*
 cp /build/*.deb /out
 
-# Change ownership of build artifacts, this is used to unsure Jenkins 
+# Change ownership of build artifacts, this is used to ensure Jenkins 
 # can clean up build artifacts, as these a generated in docker image running as root.
 if [[ ! -z ${EXTERNAL_UID+x} ]] && [[ ! -z ${EXTERNAL_GID+x} ]]
 then
