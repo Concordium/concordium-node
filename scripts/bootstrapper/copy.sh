d01c329095428e08a766e5e6ff5f8dd52330b04f
Stefan Madsen <Thahara@users.noreply.github.com>
2022-10-31 14:41:13 +0100

    Add build pipeline for bootstrapper (#575)
    Add build pipeline for bootstrapper

---
+++ scripts/bootstrapper/copy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+rm -rf /out/*
+cp /build/*.deb /out
+
+# Change ownership of build artifacts, this is used to unsure Jenkins 
+# can clean up build artifacts, as these a generated in docker image running as root.
+if [[ ! -z ${EXTERNAL_UID+x} ]] && [[ ! -z ${EXTERNAL_GID+x} ]]
+then
+    chown -R $EXTERNAL_UID:$EXTERNAL_GID /out
+fi
