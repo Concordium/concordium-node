9ff069de45d42083ad3f6ae19c058315fc0f2c05
Viktor Fredslund-Hansen <vfh@concordium.com>
2022-12-22 18:29:39 +0100

    Better comments.

--- concordium-consensus/tests/scheduler/SchedulerTests/SmartContracts/V0/SmartContractTests.hs
+++ concordium-consensus/tests/scheduler/SchedulerTests/SmartContracts/V0/SmartContractTests.hs
@@ -40,6 +40,7 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import SchedulerTests.TestUtils
 import Test.Hspec
+import Data.Either (fromRight)
 
 -- ** Test runners **
 
@@ -316,9 +317,10 @@ simpleTransferTestCases =
   where
     contractAddr = Types.AddressContract $ ContractAddress 0 0
     -- When using pointing to 32 0s in memory, this is the address that is used.
-    accRefM = addressFromText "2wkBET2rRgE8pahuaczxKbmv7ciehqsne57F9gtzf1PVdr2VP3"
     -- This is safe, since addressFromText was passed a valid address string.
-    accRef = fromRight (error "addressFromText did not return an account reference") accRefM
+    accRef = fromRight
+                 (error "addressFromText did not return an account reference")
+                 $ addressFromText "2wkBET2rRgE8pahuaczxKbmv7ciehqsne57F9gtzf1PVdr2VP3"
 
 sendTestCases :: [TestCase PV1]
 sendTestCases =
