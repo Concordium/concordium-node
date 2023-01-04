1e9f1fc9b6418ff5d37a02c856038f08046edc8e
Emil Holm Gjørup <eg@concordium.com>
2023-01-04 13:54:47 +0100

    Use persistent block state in SchedulerTests/TransactionExpirySpec

--- concordium-consensus/tests/scheduler/SchedulerTests/TransactionExpirySpec.hs
+++ concordium-consensus/tests/scheduler/SchedulerTests/TransactionExpirySpec.hs
@@ -1,47 +1,57 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module SchedulerTests.TransactionExpirySpec where
+module SchedulerTests.TransactionExpirySpec (tests) where
 
+import Lens.Micro.Platform
 import Test.HUnit
 import Test.Hspec
 
-import Control.Monad.IO.Class
-import Lens.Micro.Platform
-
+import qualified Concordium.Crypto.BlockSignature as BlockSig
+import qualified Concordium.Crypto.BlsSignature as Bls
+import qualified Concordium.Crypto.SignatureScheme as SigScheme
+import qualified Concordium.Crypto.VRF as VRF
+import Concordium.GlobalState.BakerInfo
+import Concordium.GlobalState.DummyData
+import qualified Concordium.GlobalState.Persistent.BlockState as BS
+import qualified Concordium.Scheduler as Sch
+import Concordium.Scheduler.DummyData
+import qualified Concordium.Scheduler.EnvironmentImplementation as Types
+import Concordium.Scheduler.Runner
+import qualified Concordium.Scheduler.Types as Types
 import Concordium.Types.Accounts (
     bakerAggregationVerifyKey,
     bakerElectionVerifyKey,
     bakerInfo,
     bakerSignatureVerifyKey,
  )
+import qualified SchedulerTests.Helpers as Helpers
 
-import qualified Concordium.Crypto.BlockSignature as BlockSig
-import qualified Concordium.Crypto.BlsSignature as Bls
-import qualified Concordium.Crypto.VRF as VRF
-import Concordium.GlobalState.BakerInfo
-import Concordium.GlobalState.Basic.BlockState
-import Concordium.GlobalState.Basic.BlockState.Accounts as Acc
-import Concordium.GlobalState.Basic.BlockState.Invariants
-import qualified Concordium.Scheduler as Sch
-import qualified Concordium.Scheduler.EnvironmentImplementation as Types
-import Concordium.Scheduler.Runner
-import qualified Concordium.Scheduler.Types as Types
-import Concordium.TransactionVerification
+tests :: Spec
+tests =
+    describe "Transaction expiry test:" $ do
+        sequence_ $
+            Helpers.forEveryProtocolVersion $ \spv pvString -> do
+                specify (pvString ++ ": Valid transactions of all payloads with expiry after slot time pass") $ do
+                    testExpiryTime (expiryTime + 1) (allTransactions spv) spv
+                specify (pvString ++ ": Same transactions with expiry set to slot time pass") $
+                    testExpiryTime expiryTime (allTransactions spv) spv
+                specify (pvString ++ ": Same transactions with expiry set before slot time fail") $
+                    testExpiryTime (expiryTime - 1) (allTransactions spv) spv
 
-import Concordium.Crypto.DummyData
-import Concordium.GlobalState.DummyData
-import Concordium.Scheduler.DummyData
-import Concordium.Types.DummyData
+initialBlockState ::
+    (Types.IsProtocolVersion pv) =>
+    Helpers.PersistentBSM pv (BS.HashedPersistentBlockState pv)
+initialBlockState =
+    Helpers.createTestBlockStateWithAccountsM
+        [Helpers.makeTestAccountFromSeed 310_000_000_000 0]
 
-import SchedulerTests.Helpers
-import SchedulerTests.TestUtils
+accountAddress0 :: Types.AccountAddress
+accountAddress0 = Helpers.accountAddressFromSeed 0
 
-shouldReturnP :: Show a => IO a -> (a -> Bool) -> IO ()
-shouldReturnP action f = action >>= (`shouldSatisfy` f)
-
-initialBlockState :: BlockState PV1
-initialBlockState = blockStateWithAlesAccount 310_000_000_000 Acc.emptyAccounts
+keyPair0 :: SigScheme.KeyPair
+keyPair0 = Helpers.keyPairFromSeed 0
 
 baker :: (FullBakerInfo, VRF.SecretKey, BlockSig.SignKey, Bls.SecretKey)
 baker = mkFullBaker 1 0
@@ -50,14 +60,27 @@ baker = mkFullBaker 1 0
 -- Ideally, this should include all payload types to ensure that expiry is handled for
 -- all types of transactions.
 -- TODO: Add other transaction types.
-transactions :: Types.TransactionExpiryTime -> [TransactionJSON]
-transactions t =
+allTransactions ::
+    (Types.IsProtocolVersion pv) =>
+    Types.SProtocolVersion pv ->
+    Types.TransactionExpiryTime ->
+    [TransactionJSON]
+allTransactions spv expiry =
+    transferTransactions expiry
+        ++ if Types.supportsDelegation spv then [] else bakerV0Transactions expiry
+
+transferTransactions :: Types.TransactionExpiryTime -> [TransactionJSON]
+transferTransactions expiry =
     [ TJSON
-        { payload = Transfer{toaddress = alesAccount, amount = 10000},
-          metadata = makeHeaderWithExpiry alesAccount 1 100000 t,
-          keys = [(0, [(0, alesKP)])]
-        },
-      TJSON
+        { payload = Transfer{toaddress = accountAddress0, amount = 10_000},
+          metadata = makeHeaderWithExpiry accountAddress0 1 100_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
+        }
+    ]
+
+bakerV0Transactions :: Types.TransactionExpiryTime -> [TransactionJSON]
+bakerV0Transactions expiry =
+    [ TJSON
         { payload =
             AddBaker
                 { bElectionVerifyKey = baker ^. _1 . bakerInfo . bakerElectionVerifyKey,
@@ -66,16 +89,16 @@ transactions t =
                   bSignSecretKey = baker ^. _3,
                   bAggregateVerifyKey = baker ^. _1 . bakerInfo . bakerAggregationVerifyKey,
                   bAggregateSecretKey = baker ^. _4,
-                  bInitialStake = 300000000000,
+                  bInitialStake = 300_000_000_000,
                   bRestakeEarnings = True
                 },
-          metadata = makeHeaderWithExpiry alesAccount 2 100000 t,
-          keys = [(0, [(0, alesKP)])]
+          metadata = makeHeaderWithExpiry accountAddress0 2 100_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
         },
       TJSON
-        { payload = UpdateBakerStake 300000000001,
-          metadata = makeHeaderWithExpiry alesAccount 3 100000 t,
-          keys = [(0, [(0, alesKP)])]
+        { payload = UpdateBakerStake 300_000_000_001,
+          metadata = makeHeaderWithExpiry accountAddress0 3 100_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
         },
       TJSON
         { payload =
@@ -87,18 +110,18 @@ transactions t =
                   bAggregateVerifyKey = baker ^. _1 . bakerInfo . bakerAggregationVerifyKey,
                   bAggregateSecretKey = baker ^. _4
                 },
-          metadata = makeHeaderWithExpiry alesAccount 4 100000 t,
-          keys = [(0, [(0, alesKP)])]
+          metadata = makeHeaderWithExpiry accountAddress0 4 100_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
         },
       TJSON
         { payload = UpdateBakerRestakeEarnings False,
-          metadata = makeHeaderWithExpiry alesAccount 5 1000000 t,
-          keys = [(0, [(0, alesKP)])]
+          metadata = makeHeaderWithExpiry accountAddress0 5 1_000_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
         },
       TJSON
         { payload = RemoveBaker,
-          metadata = makeHeaderWithExpiry alesAccount 6 100000 t,
-          keys = [(0, [(0, alesKP)])]
+          metadata = makeHeaderWithExpiry accountAddress0 6 100_000 expiry,
+          keys = [(0, [(0, keyPair0)])]
         }
     ]
 
@@ -108,57 +131,57 @@ expiryTime = 1
 slotTime :: Types.Timestamp
 slotTime = Types.transactionTimeToTimestamp expiryTime
 
-type TestResult =
-    ( [(BlockItemWithStatus, Types.ValidResult)],
-      [(TransactionWithStatus, Types.FailureKind)],
-      [TransactionWithStatus]
-    )
+testExpiryTime ::
+    forall pv.
+    (Types.IsProtocolVersion pv) =>
+    Types.TransactionExpiryTime ->
+    ( Types.TransactionExpiryTime ->
+      [TransactionJSON]
+    ) ->
+    Types.SProtocolVersion pv ->
+    Assertion
+testExpiryTime expiry transactions _ =
+    do
+        let contextState =
+                Helpers.defaultContextState
+                    { Types._chainMetadata = dummyChainMeta{Types.slotTime = slotTime}
+                    }
+        let testConfig =
+                Helpers.defaultTestConfig
+                    { Helpers.tcContextState = contextState
+                    }
 
-testExpiryTime :: Types.TransactionExpiryTime -> IO TestResult
-testExpiryTime expiry = do
-    ts <- processUngroupedTransactions $ transactions expiry
-    let (Sch.FilteredTransactions{..}, finState) =
-            Types.runSI
-                (Sch.filterTransactions dummyBlockSize dummyBlockTimeout ts)
-                dummyChainMeta{Types.slotTime = slotTime}
-                maxBound
-                maxBound
+        -- Run the test
+        (Helpers.SchedulerResult{..}, doBlockStateAssertions) <-
+            Helpers.runSchedulerTestTransactionJson
+                testConfig
                 initialBlockState
-    let gstate = finState ^. Types.ssBlockState
-    case invariantBlockState gstate (finState ^. Types.schedulerExecutionCosts) of
-        Left f -> liftIO $ assertFailure f
-        Right _ -> return (getResults ftAdded, ftFailed, ftUnprocessed)
+                (Helpers.checkReloadCheck checkState)
+                (transactions expiry)
 
-checkExpiryTimeResult :: Types.TransactionExpiryTime -> TestResult -> Assertion
-checkExpiryTimeResult expiry (added, fails, unprocs) = do
-    assertEqual "No unprocessed transactions." [] unprocs
-    if expiryTime <= expiry
-        then -- transactions haven't expired, so they should all succeed
-        do
-            assertEqual "No failed transactions." [] fails
-            assertBool "All added transactions succeed." $ all fSucs added
-        else -- transactions expired and they should all fail
-        do
-            assertEqual "No transactions added." [] added
-            assertBool "All failed transactions expired." $ all fFails fails
+        let Sch.FilteredTransactions{..} = srTransactions
+        assertEqual "No unprocessed transactions." [] ftUnprocessed
+        let results = Helpers.getResults ftAdded
+        if expiryTime <= expiry
+            then -- transactions haven't expired, so they should all succeed
+            do
+                assertEqual "No failed transactions." [] ftFailed
+                assertBool "All added transactions succeed." $ all isSuccess results
+            else -- transactions expired and they should all fail
+            do
+                assertEqual "No transactions added." [] results
+                assertBool "All failed transactions expired." $ all isExpired ftFailed
+        doBlockStateAssertions
   where
-    fFails (_, Types.ExpiredTransaction) = True
-    fFails _ = False
+    isSuccess (_, Types.TxSuccess{}) = True
+    isSuccess _ = False
 
-    fSucs (_, Types.TxSuccess{}) = True
-    fSucs _ = False
+    isExpired (_, Types.ExpiredTransaction) = True
+    isExpired _ = False
 
-tests :: Spec
-tests =
-    describe "Transaction expiry test:" $ do
-        specify "Valid transactions of all payloads with expiry after slot time pass" $
-            testExpiry $
-                expiryTime + 1
-        specify "Same transactions with expiry set to slot time pass" $
-            testExpiry $
-                expiryTime
-        specify "Same transactions with expiry set before slot time fail" $
-            testExpiry $
-                expiryTime - 1
-  where
-    testExpiry expiry = testExpiryTime expiry >>= checkExpiryTimeResult expiry
+    checkState ::
+        Helpers.SchedulerResult ->
+        BS.PersistentBlockState pv ->
+        Helpers.PersistentBSM pv Assertion
+    checkState result blockstate =
+        Helpers.assertBlockStateInvariantsH blockstate (Helpers.srExecutionCosts result)
