0a0316c83a3f7f862586ea8b1923eaba85336a1a
Emil Lai <el@concordium.com>
2023-01-31 15:34:24 +0100

    Formatting

--- concordium-consensus/src/Concordium/KonsensusV1/TreeState.hs
+++ concordium-consensus/src/Concordium/KonsensusV1/TreeState.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+
 module Concordium.KonsensusV1.TreeState where
 
 import Concordium.Types.Parameters
@@ -8,9 +9,8 @@ import Concordium.Types.ProtocolVersion
 
 import Concordium.KonsensusV1.Types
 
+import Concordium.GlobalState.TreeState
 import Concordium.GlobalState.Types
-import Concordium.GlobalState.TreeState 
-
 
 -- |Constraint for for ''ConsensusParametersVersion1' based on
 -- the protocol version @pv@.
@@ -18,56 +18,68 @@ type IsConsensusV1 (pv :: ProtocolVersion) =
     ConsensusParametersVersionFor (ChainParametersVersionFor pv) ~ 'ConsensusParametersVersion1
 
 -- |Tree state for 'ConsensusParametersVersion1'
-class (Monad m,
-       IsConsensusV1 (MPV m)
-      ) => TreeStateMonad m where
+class
+    ( Monad m,
+      IsConsensusV1 (MPV m)
+    ) =>
+    TreeStateMonad m
+    where
+    -- * Block operations
 
-  -- * Block operations
-  --
-  -- |Add pending block
-  -- Adds the block to the block table and
-  -- give it status 'Pending'.
-  addPendingBlock :: m ()
-  -- |Promote a pending block to be live.
-  -- Set the status of the block to be 'Alive'.
-  makeLiveBlock :: m ()
-  -- |Mark a pending block as dead
-  markPendingBlockDead :: m ()
-    -- |Mark a live block as dead.
+    --
+
+    -- |Add pending block
+    -- Adds the block to the block table and
+    -- give it status 'Pending'.
+    addPendingBlock :: m ()
+
+    -- |Promote a pending block to be live.
+    -- Set the status of the block to be 'Alive'.
+    makeLiveBlock :: m ()
+
+    -- |Mark a pending block as dead
+    markPendingBlockDead :: m ()
+
+    -- \|Mark a live block as dead.
     -- Mark it as dead.
     -- Drop the transaction results.
     -- Purge the block state assoicated.
-  markLiveBlockDead :: m ()
-  -- |Attach a 'QuorumCertificate' to the block.
-  -- Precondition: The 'QuorumCertificate' must be pointing
-  -- to the block provided.
+    markLiveBlockDead :: m ()
 
+    -- |Attach a 'QuorumCertificate' to the block.
+    -- Precondition: The 'QuorumCertificate' must be pointing
+    -- to the block provided.
 
-  -- * Block queries
-  --
-  -- |Get the last finalized block.
-  getLastFinalized :: m ()
-  -- |Get the current 'BlockStatus' of a block.
-  -- Note. If the block is older than the last finalized block,
-  -- then this will incur a disk lookup.
-  getBlockStatus :: m (Maybe (BlockStatus () ()))  
-  -- |Get the current 'RecentBlockStatus' of a block.
-  -- If the block is older than the last finalized block then
-  -- the block then only this fact is returned and not
-  -- the block hash ,...
-  getRecentBlockStatus :: m (RecentBlockStatus () ())
+    -- * Block queries
 
-  -- *Quorum- and Timeout Certificates
+    --
 
-  -- |Add a quorum certificate and
-  -- if possible mark a predecessor as finalized.
-  addQuorumCertificate :: m ()
-  -- |Add a timeout certificate
-  addTimeoutCertificate :: m ()
+    -- |Get the last finalized block.
+    getLastFinalized :: m ()
 
-  -- *Transactions
-  --
-  -- |Add a transaction to the transaction table.
-  addTransaction :: m ()
-  
-  
+    -- |Get the current 'BlockStatus' of a block.
+    -- Note. If the block is older than the last finalized block,
+    -- then this will incur a disk lookup.
+    getBlockStatus :: m (Maybe (BlockStatus () ()))
+
+    -- |Get the current 'RecentBlockStatus' of a block.
+    -- If the block is older than the last finalized block then
+    -- the block then only this fact is returned and not
+    -- the block hash ,...
+    getRecentBlockStatus :: m (RecentBlockStatus () ())
+
+    -- *Quorum- and Timeout Certificates
+
+    -- |Add a quorum certificate and
+    -- if possible mark a predecessor as finalized.
+    addQuorumCertificate :: m ()
+
+    -- |Add a timeout certificate
+    addTimeoutCertificate :: m ()
+
+    -- *Transactions
+
+    --
+
+    -- |Add a transaction to the transaction table.
+    addTransaction :: m ()
