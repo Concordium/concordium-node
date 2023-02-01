483216a2d12502857cc7d81de8a813fd4f48a9bf
Emil Lai <el@concordium.com>
2023-02-01 15:34:11 +0100

    TreeState API draft

--- concordium-consensus/src/Concordium/KonsensusV1/TreeState.hs
+++ concordium-consensus/src/Concordium/KonsensusV1/TreeState.hs
@@ -18,6 +18,7 @@ type IsConsensusV1 (pv :: ProtocolVersion) =
     ConsensusParametersVersionFor (ChainParametersVersionFor pv) ~ 'ConsensusParametersVersion1
 
 -- |Tree state for 'ConsensusParametersVersion1'
+-- Tree state operations are guarded by a RW lock.
 class
     ( Monad m,
       IsConsensusV1 (MPV m)
@@ -31,19 +32,24 @@ class
     -- |Add pending block
     -- Adds the block to the block table and
     -- give it status 'Pending'.
+    -- Acquires a write lock.
     addPendingBlock :: m ()
 
-    -- |Promote a pending block to be live.
+    -- |Mark a pending block to be live.
     -- Set the status of the block to be 'Alive'.
+    -- This will also update the consensus statistics.
+    -- Acquires a write lock.
     makeLiveBlock :: m ()
 
     -- |Mark a pending block as dead
+    -- Acquires a write lock.
     markPendingBlockDead :: m ()
 
-    -- \|Mark a live block as dead.
+    -- |Mark a live block as dead.
     -- Mark it as dead.
     -- Drop the transaction results.
     -- Purge the block state assoicated.
+    -- Acquires a write lock.
     markLiveBlockDead :: m ()
 
     -- |Attach a 'QuorumCertificate' to the block.
@@ -54,32 +60,224 @@ class
 
     --
 
+    -- |Get a list of pending blocks of a block
+    -- removing the children pending blocks from the pending blocks *something*
+    -- Acquires a read lock.
+    takePendingChildren :: m [()]
+
     -- |Get the last finalized block.
+    -- Acquires a read lock.
     getLastFinalized :: m ()
+
+    -- |Get the block height of the last finalized block.
+    -- Acquires a read lock.
+    getLastFinalizedHeight :: m ()
 
     -- |Get the current 'BlockStatus' of a block.
     -- Note. If the block is older than the last finalized block,
     -- then this will incur a disk lookup.
+    -- Acquires a read lock.
     getBlockStatus :: m (Maybe (BlockStatus () ()))
 
     -- |Get the current 'RecentBlockStatus' of a block.
     -- If the block is older than the last finalized block then
     -- the block then only this fact is returned and not
     -- the block hash ,...
+    -- Acquires a read lock.
     getRecentBlockStatus :: m (RecentBlockStatus () ())
 
-    -- *Quorum- and Timeout Certificates
-
-    -- |Add a quorum certificate and
-    -- if possible mark a predecessor as finalized.
-    addQuorumCertificate :: m ()
-
-    -- |Add a timeout certificate
-    addTimeoutCertificate :: m ()
-
-    -- *Transactions
+    -- * Pending transactions and focus block.
 
     --
 
-    -- |Add a transaction to the transaction table.
-    addTransaction :: m ()
+    -- |Get the focus block.
+    -- This might be the best block but if
+    -- we're pruning a branch this will become the parent block
+    -- Acquires a read lock.
+    getFocusBlock :: m ()
+
+    -- |Update the focus block
+    -- If we're pruning a block then we must also update the transaction statuses
+    -- of the ones comitted to a pending state.
+    -- Acquires a write lock.
+    setFocusBlock :: m ()
+
+    -- NOTE. Are these actually required to be exposed?
+    -- I.e. Should we expose the pending transactions at all?
+    -- How about if we only exposed the get/set focus block above
+    -- and then had another function dedicated for baking i.e.
+    -- acquiring transactions for the block.
+    -- See 'makeBlock' below.
+
+    -- |Get the pending transactions
+    -- I.e. transactions that have not yet been included in a block.
+    -- Note. pending transactions are after the focus block has been executed.
+    -- Acquires a read lock.
+    getPendingTransactions :: m ()
+
+    -- Alternative approach to expose the raw ptt, non finalized credential deployments,
+    -- non finalized chain updates
+    -- The alternative approach aims to solve 2 goals.
+    -- 1. Less stuff exposed in the API.
+    -- 2. Easier reuse of pointers to data structures behind
+    -- the tree state abstraction such as pending transactions, non finalized
+    -- credential deployments and non finalized chain updates.
+    -- Less opportunity to retain multiple copies of the same thing in memory.
+
+    -- |Make a block with given the inputs.
+    -- This should be used by the baker making a block.
+    -- The pending transactions, credential deployments and chain updates are given by this function.
+    -- Acquires a write lock.
+    makeBlock ::
+        -- |Interface to the non finalized chain updates
+        ( () -> -- Interface to the pending transaction table.
+          () -> -- Interface to the non finalized credentials
+          () -- Inteface to the non finalized chain updates
+        ) ->
+        -- |The other parameters the baker provides.
+        () ->
+        -- |The pending block to relay to peers and mark as alive.
+        m ()
+
+    -- |Given a block hash and .. provide
+    -- the context required for executing the block.
+    -- This should be used by a participant who received the block.
+    -- This provides the context of the pending transactions,
+    -- non finalized credential deployments and non finalized chain updates.
+    getExecutableContext ::
+        () -> -- the block hash and some more..
+        ( () -> -- Interface to the pending transaction table.
+          () -> -- Interface to the non finalized credentials.
+          () -- Interface to the non finalized chain updates.
+        ) ->
+        m () -- some block execution result.
+
+    -- End alternative approach
+
+    -- |Set the pending transactions
+    -- Acquires a write lock.
+    setPendingTransactions :: m ()
+
+    -- * Quorum- and Timeout Certificates
+
+    --
+
+    -- |Add a quorum certificate and
+    -- if possible mark the predecessor block as finalized.
+    -- Acquires a write lock.
+    addQuorumCertificate :: m ()
+
+    -- |Add a timeout certificate
+    -- Acquires a write lock.
+    addTimeoutCertificate :: m ()
+
+    -- |Create a signature (i.e. a signature to be aggregated in
+    -- a quorum certificate)
+    -- Note. if one have already signed the current round then
+    -- that signature and message is returned so
+    -- one can relay it.
+    -- Acquires a write lock.
+    makeQuorumSignature :: m ()
+
+    -- |Add quorum message for current round.
+    -- If enough signatures have been received then this
+    -- will create and store the quorum certificate.
+    -- Also this will progress the round if the latter is the case.
+    -- Acquires a write lock.
+    addQuorumSignature :: m ()
+
+    -- |Create a timeout message.
+    -- Creates and signs a timeout message.
+    -- The signed timeout message is returned.
+    -- Note. if one have already signed the current round then
+    -- that signature and message is returned.
+    -- Acquires a write lock.
+    makeTimeoutMessage :: m ()
+
+    -- |Add a timeout message for the current round.
+    -- If enough signatures have been received then this
+    -- will create and store the timeout certificate.
+    -- Also this will progress the round if the latter is the case.
+    -- Acquires a write lock.
+    addTimeoutMessage :: m ()
+
+    -- |Get the current round and epoch.
+    -- Acquires a read lock.
+    getRound :: m ()
+
+    -- * Transactions
+
+    --
+
+    -- |Add a pending transaction to the transaction table.
+    -- Acquires a write lock.
+    addPendingTransaction :: m ()
+
+    -- |Lookup a transaction by its hash.
+    -- Acquires a read lock.
+    lookupTransaction :: m ()
+
+    -- |Purge the transaction table.
+    -- Note. Should this actually be exposed or can
+    -- we choose an opportune time from within the tree state...
+    -- Acquires a write lock.
+    purgeTransactionTable :: m ()
+
+    -- * Account transactions
+
+    --
+
+    -- |Get the next account nonce for an account.
+    -- Returns a tuple consisting of the successor of the
+    -- current account nonce and a boolean value indicating
+    -- that there are no pending or comitted (but only finalized) transactions
+    -- tied to this account.
+    -- Acquires a read lock.
+    getNextAccountNonce :: m (Int, Bool)
+
+    -- * Chain updates
+
+    --
+
+    -- |Get the non finalized chain updates.
+    -- Is this really necessary to expose so raw?
+    -- Same thoughts as for the getPendingTransactions.
+    -- Acquires a read lock.
+    getNonFinalizedChainUpdates :: m ()
+
+    -- * Credential deployments
+
+    --
+
+    -- |Get the non finalized credentials
+    -- Is this really necessary to expose so raw?
+    -- Same thoughts as for the getPendingTransactions.
+    -- Acquires a read lock.
+    getNonFinalizedCredentials :: m ()
+
+    -- * Protocol update
+
+    --
+
+    -- |Clear resources tied to the
+    -- old protocol.
+    --
+    -- * Clear all non finalized blocks from the block table.
+    -- * Clear pending blocks
+    -- * Rollback all committed transactions,
+    -- i.e. they are now in a 'Received' state.
+    -- Acquires a write lock.
+    clearAfterProtocolUpdate :: m ()
+
+    -- * Statistics and parameters
+
+    --
+
+    -- |Get consensus statistics.
+    -- Note that the actual statistics are updated by 'makeLiveBlock'.
+    -- Acquires a read lock.
+    getConsensusStatistics :: m ()
+
+    -- |Get the runtime parameters.
+    -- Acquires a read lock.
+    getRuntimeParameters :: m ()
