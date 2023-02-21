c76cb565a981baddf69a04e0a0d988aa147cb972
Emil Lai <el@concordium.com>
2023-02-21 16:04:11 +0100

    TransactionVerifier for new consensus

--- concordium-consensus/src/Concordium/KonsensusV1/Consensus.hs
+++ concordium-consensus/src/Concordium/KonsensusV1/Consensus.hs
@@ -1,14 +1,18 @@
 module Concordium.KonsensusV1.Consensus where
 
+import Control.Monad.State
 import qualified Data.ByteString as BS
 
 import Concordium.Types
 
+import Concordium.KonsensusV1.TreeState.Implementation
+import qualified Concordium.TransactionVerification as TVer
+
 class MonadMulticast m where
     sendMessage :: BS.ByteString -> m ()
-
-
 
 -- uponTimeoutEvent :: ()
 -- uponTimeoutEvent = ()
 
+receiveBlockItem :: (MonadState (SkovData pv) m) => BlockItem -> m ()
+receiveBlockItem = undefined
