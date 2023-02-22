0d51c4295b2d40b8b27dc304535ce2fdd1f772c7
Emil Lai <mail@emillai.com>
2023-02-22 21:10:09 +0100

    Formatting

--- concordium-consensus/tests/consensus/ConcordiumTests/KonsensusV1/TransactionProcessingTest.hs
+++ concordium-consensus/tests/consensus/ConcordiumTests/KonsensusV1/TransactionProcessingTest.hs
@@ -2,11 +2,10 @@ module ConcordiumTests.KonsensusV1.TransactionProcessingTest (tests) where
 
 import Concordium.KonsensusV1.Consensus
 
-
 testProcessBlockItem :: Spec
 testProcessBlockItem = describe "processBlockItem" $ do
-  it "normal transaction processed" $ do
-      ()
+    it "normal transaction processed" $ do
+        ()
 
 tests :: Spec
 tests = describe "KonsensusV1.TransactionProcessing" $ do
