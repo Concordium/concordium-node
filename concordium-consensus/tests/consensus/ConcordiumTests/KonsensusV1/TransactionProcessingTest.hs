07bb3aea368c7203fbbfcdfaace8eaf236ed1bc5
Emil Lai <mail@emillai.com>
2023-02-22 21:00:33 +0100

    Add test file for transaction processing.

---
+++ concordium-consensus/tests/consensus/ConcordiumTests/KonsensusV1/TransactionProcessingTest.hs
@@ -0,0 +1,14 @@
+module ConcordiumTests.KonsensusV1.TransactionProcessingTest (tests) where
+
+import Concordium.KonsensusV1.Consensus
+
+
+testProcessBlockItem :: Spec
+testProcessBlockItem = describe "processBlockItem" $ do
+  it "normal transaction processed" $ do
+      ()
+
+tests :: Spec
+tests = describe "KonsensusV1.TransactionProcessing" $ do
+    describe "Individual transaction processing" $ do
+        testProcessBlockItem
