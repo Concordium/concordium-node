7f93baa47e388a5dc311947101305f636a18534b
Emil Lai <mail@emillai.com>
2023-03-07 07:23:54 +0100

    Remove unused file

--- concordium-consensus/src/Concordium/KonsensusV1/TransactionVerifier.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-
--- |This module implements the 'TransactionVerifier' for consensus protocol V1.
-module Concordium.KonsensusV1.TransactionVerifier where
