9631acd0c826e4ccfd3b18386529b2adec48e324
Emil Lai <mail@emillai.com>
2023-03-17 15:12:15 +0100

    Quorum tests scaffolding.

--- concordium-consensus/tests/consensus/ConcordiumTests/KonsensusV1/Quorum.hs
+++ concordium-consensus/tests/consensus/ConcordiumTests/KonsensusV1/Quorum.hs
@@ -1,12 +1,23 @@
 -- |Module testing functions from the 'Concordium.KonsensusV1.Quorum' module.
 module ConcordiumTests.KonsensusV1.Quorum where
 
+import Data.Kind (Type)
 import Lens.Micro.Platform
 import Test.HUnit
 import Test.Hspec
 import Test.QuickCheck
+
+import Concordium.KonsensusV1.Consensus
 import Concordium.KonsensusV1.Consensus.Quorum
 
+-- |A type that is an instance of 'MonadMulticast' required for receiving a
+-- 'QuorumMessage'.
+-- Note. We do not test relaying of quorum messages in this test.
+newtype NoMultiCastT (m :: Type -> Type) (a :: Type) = NoMultiCastT {runMyMultiCast :: m a}
+    deriving (Functor, Applicative, Monad)
+
+instance Monad m => MonadMulticast (NoMultiCastT m) where
+    sendMessage _ = return ()
 
 testReceiveQuorumMessage :: Spec
 testReceiveQuorumMessage = describe "receiveQuorumMessage" $ do
@@ -16,6 +27,4 @@ testReceiveQuorumMessage = describe "receiveQuorumMessage" $ do
 
 tests :: Spec
 tests = describe "KonsensusV1.Quorum" $ do
-    describe "receiveQuorumMessage" $ do
-        testReceiveQuorumMessage
-  
+    testReceiveQuorumMessage
