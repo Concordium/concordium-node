3199de762668583ea2bb93257196dd213d028c78
Thomas Dinsdale-Young <ty@concordium.com>
2023-05-24 14:37:57 +0200

    Bypass deduplication for peer that we are currently catching up with.

--- concordium-node/src/connection/mod.rs
+++ concordium-node/src/connection/mod.rs
@@ -536,7 +536,12 @@ impl Connection {
             }
             // deduplicate the incoming packet payload
             if self.is_packet_duplicate(packet)? {
-                return Ok(());
+                // we ignore the fact that the packet is a duplicate if we are catching up with
+                // the peer
+                if read_or_die!(self.handler.peers).catch_up_peer != Some(self.remote_peer.local_id)
+                {
+                    return Ok(());
+                }
             }
         }
 
