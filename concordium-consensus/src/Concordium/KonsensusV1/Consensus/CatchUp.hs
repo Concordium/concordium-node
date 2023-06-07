5d8064f30ec011a6bf63d4d35800c1e929451af7
Thomas Dinsdale-Young <ty@concordium.com>
2023-06-07 09:35:19 +0200

    Catch up terminal data

--- concordium-consensus/src/Concordium/KonsensusV1/Consensus/CatchUp.hs
+++ concordium-consensus/src/Concordium/KonsensusV1/Consensus/CatchUp.hs
@@ -1,5 +1,7 @@
 module Concordium.KonsensusV1.Consensus.CatchUp where
 
+import Concordium.KonsensusV1.Types
+
 -- notes
 
 -- We need to catch up blocks,
@@ -13,3 +15,37 @@ module Concordium.KonsensusV1.Consensus.CatchUp where
 -- Catchup responses must bypass the deduplication layer,
 -- hence if we are catching up from a peer then their responses should
 -- not be deduplicated.
+
+{-
+A node N is up to date with a peer P if:
+    * N considers P's last finalized block to be finalized.
+    * The round of N's highest certified block is at least the round of P's highest certified block.
+    * N's current round (round(N)) is at least P's current round.
+    * N's current epoch is at least P's current epoch.
+    * N has all timeout signatures on round(N) that P has, and considers them valid if they are.
+    * N has all quorum signatures on round(N) that P has, and considers them valid if they are.
+    * If P has only one block in round(N), then N also has that block.
+
+If P is honest and considers a timeout signature on round(N) valid, then
+-}
+
+-- |The 'CatchUpTerminalData' is sent as part of a catch-up response that concludes catch-up with
+-- the peer (i.e. the peer has sent all relevant information).
+--
+-- If the peer is not otherwise aware of them, 'cutdQuorumCertificates' should include QCs on:
+--    * The block in the round after the last finalized block (if the peer does not consider it
+--      finalized already).
+--    * The highest certified block (if for a later round than the peer's highest certified block).
+data CatchUpTerminalData = CatchUpTerminalData
+    { -- |Quorum certificates to ensure agreement on the last finalized block and highest certified
+      -- block.
+      cutdQuorumCertificates :: ![QuorumCertificate],
+      -- |A timeout certificate for the last round, if available.
+      cutdTimeoutCertificate :: !(Option TimeoutCertificate),
+      -- |Valid quorum messages for the current round.
+      -- TODO: Repackage all quorum messages for the same block together.
+      cutdCurrentRoundQuorumMessages :: ![QuorumMessage],
+      -- |Valid timeout messages for the current round.
+      -- TODO: Repackage timeout messages together.
+      cutdCurrentRoundTimeoutMessages :: ![TimeoutMessage]
+    }
