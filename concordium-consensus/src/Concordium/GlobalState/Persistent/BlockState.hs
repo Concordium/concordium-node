09c681ffe10f80a215be8f8273d2fa3f3a43b290
drsk <rsk@concordium.com>
2024-08-19 10:45:47 +0200

    scheduler/queries/blockstate actions for suspend/resume

--- concordium-consensus/src/Concordium/GlobalState/Persistent/BlockState.hs
+++ concordium-consensus/src/Concordium/GlobalState/Persistent/BlockState.hs
@@ -4260,6 +4260,8 @@ instance (PersistentState av pv r m, IsProtocolVersion pv) => AccountOperations 
 
     getAccountCooldowns = accountCooldowns
 
+    getAccountIsSuspended = error "TODO(drsk) implement accountIsSuspended"
+
 instance (IsProtocolVersion pv, PersistentState av pv r m) => BlockStateOperations (PersistentBlockStateMonad pv r m) where
     bsoGetModule pbs mref = doGetModule pbs mref
     bsoGetAccount bs = doGetAccount bs
