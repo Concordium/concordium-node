55511c7c00616ef50571d826cf782cbc8b60836e
Lasse Alm <lassealm2@gmail.com>
2024-10-22 08:33:00 +0200

    rustup does not seem to alter version

--- scripts/distribution/windows/build-all.ps1
+++ scripts/distribution/windows/build-all.ps1
@@ -8,6 +8,8 @@ Write-Output "protoc version: $(protoc --version)"
 # Set the default rust toolchain so that consensus rust dependencies use it.
 rustup default $rustVersion-x86_64-pc-windows-gnu
 
+rustup show
+
 Write-Output "Building consensus..."
 stack build
 if ($LASTEXITCODE -ne 0) { throw "Failed building consensus" }
