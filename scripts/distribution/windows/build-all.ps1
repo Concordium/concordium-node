56e7535f1bed14985f3b914a81268427f34b124c
Lasse Alm <lassealm2@gmail.com>
2024-10-29 15:16:20 +0100

    fix build

--- scripts/distribution/windows/build-all.ps1
+++ scripts/distribution/windows/build-all.ps1
@@ -5,6 +5,9 @@ Write-Output "cargo version: $(cargo --version)"
 Write-Output "flatc version: $(flatc --version)"
 Write-Output "protoc version: $(protoc --version)"
 
+# Set the default rust toolchain so that consensus rust dependencies use it.
+rustup default $rustVersion-x86_64-pc-windows-gnu
+
 Write-Output "Building consensus..."
 stack build
 if ($LASTEXITCODE -ne 0) { throw "Failed building consensus" }
@@ -14,7 +17,7 @@ stack exec -- cargo build --manifest-path concordium-node\Cargo.toml --release -
 if ($LASTEXITCODE -ne 0) { throw "Failed building node" }
 
 Write-Output "Building the collector..."
-cargo build --manifest-path collector\Cargo.toml --release --locked
+cargo +$rustVersion-x86_64-pc-windows-msvc build --manifest-path collector\Cargo.toml --release --locked
 if ($LASTEXITCODE -ne 0) { throw "Failed building the collector" }
 
 Write-Output "Building node runner service..."
