39f452f06de3cd15241d9afd719cf8185a3e2091
drsk <827698+drsk0@users.noreply.github.com>
2025-08-26 15:42:18 +0200

    COR-1627: streamline windows node signing process (#1453)
    - Moves the signing of the libraries and executables into the build script
    - Avoids repacking of cabinet files
    
    Co-authored-by: drsk <rsk@concordium.com>

--- scripts/distribution/windows/build-all.ps1
+++ scripts/distribution/windows/build-all.ps1
@@ -30,4 +30,58 @@ cargo +$rustVersion-x86_64-pc-windows-msvc build --release --locked
 Pop-Location
 if ($LASTEXITCODE -ne 0) { throw "Failed building node runner service" }
 
+if (-not $env:WINDOWS_SM_KEYPAIR_ALIAS) {
+    Write-Output "WINDOWS_SM_KEYPAIR_ALIAS is not set. Not signing."
+}
+if (-not $env:WINDOWS_PKCS11_CONFIG) {
+    Write-Output "WINDOWS_PKCS11_CONFIG is not set. Not signing."
+}
+if (-not $env:SM_ARGS) {
+    Write-Output "SM_ARGS is not set. Not signing."
+}
+
+if (-not (Get-Command "smctl" -ErrorAction SilentlyContinue)){
+    Write-Output "smctl not available. Not signing"
+}
+# Sign files if smctl is available and necessary environment variables are set.
+if ($env:WINDOWS_SM_KEYPAIR_ALIAS -and $env:WINDOWS_PKCS11_CONFIG -and $env:SM_ARGS -and (Get-Command "smctl" -ErrorAction SilentlyContinue)) {
+
+
+try {
+    # Move to the location of the script so that relative paths make sense
+    Push-Location $PSScriptRoot
+    $StackInstallRoot = stack path --local-install-root
+    
+    # Split the SM_ARGS into an array and remove quotes
+    $smArgs = $env:SM_ARGS.Trim('"') -split '\s+'
+    
+    # Define array of files to sign
+    $filesToSign = @(
+        "$StackInstallRoot\lib\concordium-consensus.dll",
+        "..\..\..\concordium-base\lib\concordium_base.dll",
+        "..\..\..\concordium-base\smart-contracts\lib\concordium_smart_contract_engine.dll",
+        "..\..\..\concordium-base\lib\sha_2.dll",
+        "..\..\..\service\windows\target\x86_64-pc-windows-msvc\release\node-runner-service.exe",
+        "..\..\..\collector\target\release\node-collector.exe",
+        "..\..\..\concordium-node\target\release\concordium-node.exe"
+    )
+    
+    # Sign all files
+    foreach ($filePath in $filesToSign) {
+        if (Test-Path $filePath) {
+            Write-Host "Signing $filePath"
+            & smctl sign --keypair-alias $env:WINDOWS_SM_KEYPAIR_ALIAS --input $filePath --config-file $env:WINDOWS_PKCS11_CONFIG @smArgs
+        } else {
+            Write-Warning "File not found for signing: $filePath"
+        }
+    }
+}
+finally {
+    Pop-Location
+}
+} else {
+    Write-Output "Not signing: Missing required environment variables or smctl utility missing."
+}
+
+# Build the installer
 service\windows\installer\build.ps1 -toolchain $rustVersion-x86_64-pc-windows-msvc -nodeVersion $nodeVersion
