e58773bee36fa0f6b380c0d7a7b31c440c5fcf80
Emil Holm Gjørup <eg@concordium.com>
2025-12-16 14:00:42 +0100

    Implement simple binding to ensure the linking works

--- concordium-consensus/Setup.hs
+++ concordium-consensus/Setup.hs
@@ -14,12 +14,13 @@ import Data.Maybe
 -- | Notify and execute a command, if fails exit with the same exit code.
 runCmd :: Verbosity -> String -> IO ()
 runCmd verbosity cmd = do
-  notice verbosity $ "Running '" ++ cmd ++ "'"
-  let command:args = words cmd
-  rawSystemExit verbosity command $ args
+    notice verbosity $ "Running '" ++ cmd ++ "'"
+    let command : args = words cmd
+    rawSystemExit verbosity command $ args
 
 -- | Path to Concordium Smart Contract Engine rust crate relative to this file.
 smartContractEngineCrateRelative = "../concordium-base/smart-contracts/wasm-chain-integration"
+
 -- | Path to plt-scheduler rust crate relative to this file.
 pltSchedulerCrateRelative = "../plt-scheduler"
 
@@ -34,29 +35,29 @@ preConfHook args flags = do
 
     -- Build and copy/symlink Concordium Smart contract Engine library.
     smartContractEngineCrate <- canonicalizePath smartContractEngineCrateRelative
-    runCmd verbosity $ "cargo build --release --locked --features=enable-ffi --manifest-path=" ++ smartContractEngineCrate ++"/Cargo.toml"
+    runCmd verbosity $ "cargo build --release --locked --features=enable-ffi --manifest-path=" ++ smartContractEngineCrate ++ "/Cargo.toml"
     case buildOS of
-      Windows -> do
-        runCmd verbosity $ "cp -u " ++ smartContractEngineCrate ++ "/target/release/concordium_smart_contract_engine.dll " ++ libraryDestination
-      OSX -> do
-        runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.a " ++ libraryDestination
-        runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.dylib " ++ libraryDestination
-      _ -> do
-        runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.a " ++ libraryDestination
-        runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.so " ++ libraryDestination
+        Windows -> do
+            runCmd verbosity $ "cp -u " ++ smartContractEngineCrate ++ "/target/release/concordium_smart_contract_engine.dll " ++ libraryDestination
+        OSX -> do
+            runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.a " ++ libraryDestination
+            runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.dylib " ++ libraryDestination
+        _ -> do
+            runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.a " ++ libraryDestination
+            runCmd verbosity $ "ln -s -f " ++ smartContractEngineCrate ++ "/target/release/libconcordium_smart_contract_engine.so " ++ libraryDestination
 
     -- Build and copy/symlink PLT scheduler project
     pltSchedulerCrate <- canonicalizePath pltSchedulerCrateRelative
-    runCmd verbosity $ "cargo build --release --locked --manifest-path=" ++ pltSchedulerCrate ++"/Cargo.toml"
+    runCmd verbosity $ "cargo build --release --locked --features ffi --manifest-path=" ++ pltSchedulerCrate ++ "/Cargo.toml"
     case buildOS of
-      Windows -> do
-        runCmd verbosity $ "cp -u " ++ pltSchedulerCrate ++ "/target/release/plt_scheduler.dll " ++ libraryDestination
-      OSX -> do
-        runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.a " ++ libraryDestination
-        runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.dylib " ++ libraryDestination
-      _ -> do
-        runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.a " ++ libraryDestination
-        runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.so " ++ libraryDestination
+        Windows -> do
+            runCmd verbosity $ "cp -u " ++ pltSchedulerCrate ++ "/target/release/plt_scheduler.dll " ++ libraryDestination
+        OSX -> do
+            runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.a " ++ libraryDestination
+            runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.dylib " ++ libraryDestination
+        _ -> do
+            runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.a " ++ libraryDestination
+            runCmd verbosity $ "ln -s -f " ++ pltSchedulerCrate ++ "/target/release/libplt_scheduler.so " ++ libraryDestination
     return emptyHookedBuildInfo
 
 -- | On Windows, copy the DLL files to the binary install directory. This is to ensure that they
--- plt-scheduler/src/lib.rs
+++ plt-scheduler/src/lib.rs
@@ -3,6 +3,11 @@ use concordium_base::id::types::AccountAddress;
 use concordium_base::protocol_level_tokens::TokenId;
 use plt_deployment_unit::host_interface::TokenRawAmount;
 
+mod block_state;
+#[cfg(feature = "ffi")]
+mod ffi;
+
+
 // Placeholder types to be defined or replaced with types from other crates.
 
 pub type MutableTokenState = ();
@@ -201,18 +206,18 @@ pub struct OutOfEnergyError;
 #[derive(Debug)]
 pub struct OverflowError;
 
-#[derive(Debug)]
-pub enum TransactionRejectReason {}
+pub struct TransactionRejectReason;
 
-pub type TransactionPayload = Vec<u8>;
 pub type Events = ();
 
 /// Execute a transaction payload modifying `scheduler` and `block_state` accordingly.
 /// Returns the events produce if successful otherwise a reject reason.
+///
+/// The caller must ensure to rollback state changes in case of the transaction being rejected.
 pub fn execute_transaction(
     _scheduler: &mut impl SchedulerOperations,
     _block_state: &mut impl BlockStateOperations,
-    _payload: TransactionPayload,
+    _payload: &[u8],
 ) -> Result<Events, TransactionRejectReason> {
     todo!()
 }
