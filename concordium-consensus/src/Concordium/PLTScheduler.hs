71b3b4311cc435eba0da1b445607bb7ce6a09cc2
Allan Rasmussen <allanbrondum@gmail.com>
2026-01-13 15:45:37 +0100

    fixes

--- concordium-consensus/src/Concordium/PLTScheduler.hs
+++ concordium-consensus/src/Concordium/PLTScheduler.hs
@@ -22,7 +22,6 @@ import qualified Concordium.GlobalState.Persistent.BlockState.ProtocolLevelToken
 import qualified Concordium.PLTScheduler.PLTBlockState as PLTBlockState
 import qualified Concordium.Types as Types
 import qualified Concordium.Types.Tokens as Tokens
-import qualified Data.FixedByteString as FixedByteString
 
 -- | Execute a transaction payload modifying the `block_state` accordingly.
 --
@@ -31,46 +30,46 @@ executeTransaction ::
     (BlobStore.MonadBlobStore m) =>
     -- | Block state to mutate.
     PLTBlockState.PLTBlockState ->
+    -- | Callback for reading the token balance of an account.
+    ReadTokenAccountBalanceCallback ->
     -- | Callback for updating the token balance of an account.
     UpdateTokenAccountBalanceCallback ->
     -- | Transaction payload byte string.
     BS.ByteString ->
     -- | The account index of the account which signed as the sender of the transaction.
     Types.AccountIndex ->
-    -- | The account address of the account which signed as the sender of the transaction.
-    Types.AccountAddress ->
     -- | Remaining energy.
     Types.Energy ->
     -- | Outcome of the execution
     m ExecutionOutcome
 executeTransaction
     blockState
+    readTokenAccountBalanceCallback
     updateTokenAccountBalanceCallback
     transactionPayload
     senderAccountIndex
-    (Types.AccountAddress senderAccountAddress)
     remainingEnergy = do
         loadCallback <- fst <$> BlobStore.getCallbacks
-        liftIO $ FFI.alloca $ \remainingEnergyOut -> FFI.alloca $ \updatedBlockStatePtrOut -> do
+        liftIO $ FFI.alloca $ \usedEnergyOut -> FFI.alloca $ \updatedBlockStatePtrOut -> do
+            readTokenAccountBalanceCallbackPtr <- wrapReadTokenAccountBalanceCallback readTokenAccountBalanceCallback
             updateTokenAccountBalanceCallbackPtr <- wrapUpdateTokenAccountBalanceCallback updateTokenAccountBalanceCallback
             -- Invoke the ffi call
             statusCode <- PLTBlockState.withPLTBlockState blockState $ \blockStatePtr ->
-                FixedByteString.withPtrReadOnly senderAccountAddress $ \senderAccountAddressPtr ->
-                    BS.unsafeUseAsCStringLen transactionPayload $
-                        \(transactionPayloadPtr, transactionPayloadLen) ->
-                            ffiExecuteTransaction
-                                loadCallback
-                                updateTokenAccountBalanceCallbackPtr
-                                blockStatePtr
-                                (FFI.castPtr transactionPayloadPtr)
-                                (fromIntegral transactionPayloadLen)
-                                (fromIntegral senderAccountIndex)
-                                senderAccountAddressPtr
-                                (fromIntegral remainingEnergy)
-                                updatedBlockStatePtrOut
-                                remainingEnergyOut
+                BS.unsafeUseAsCStringLen transactionPayload $
+                    \(transactionPayloadPtr, transactionPayloadLen) ->
+                        ffiExecuteTransaction
+                            loadCallback
+                            readTokenAccountBalanceCallbackPtr
+                            updateTokenAccountBalanceCallbackPtr
+                            blockStatePtr
+                            (FFI.castPtr transactionPayloadPtr)
+                            (fromIntegral transactionPayloadLen)
+                            (fromIntegral senderAccountIndex)
+                            (fromIntegral remainingEnergy)
+                            updatedBlockStatePtrOut
+                            usedEnergyOut
             -- Process the and construct the outcome
-            newRemainingEnergy <- fromIntegral <$> FFI.peek remainingEnergyOut
+            usedEnergy <- fromIntegral <$> FFI.peek usedEnergyOut
             status <- case statusCode of
                 0 -> do
                     updatedBlockState <- FFI.peek updatedBlockStatePtrOut >>= PLTBlockState.wrapFFIPtr
@@ -84,7 +83,7 @@ executeTransaction
                 _ -> error "Unexpected status code from calling 'ffiExecuteTransaction'"
             return
                 ExecutionOutcome
-                    { erRemainingEnergy = newRemainingEnergy,
+                    { erUsedEnergy = usedEnergy,
                       erStatus = status
                     }
 
@@ -92,6 +91,8 @@ foreign import ccall "ffi_execute_transaction"
     ffiExecuteTransaction ::
         -- | Called to read data from blob store.
         FFI.LoadCallback ->
+        -- | Called to get the token account balance in the haskell-managed block state.
+        ReadTokenAccountBalanceCallbackPtr ->
         -- | Called to set the token account balance in the haskell-managed block state.
         UpdateTokenAccountBalanceCallbackPtr ->
         -- | Pointer to the starting block state.
@@ -102,22 +103,19 @@ foreign import ccall "ffi_execute_transaction"
         FFI.CSize ->
         -- | The account index of the account which signed as the sender of the transaction.
         Word.Word64 ->
-        -- | Pointer to 32 bytes representing the account address of the account which signed as the
-        -- sender of the transaction.
-        FFI.Ptr Word.Word8 ->
         -- | Remaining energy
         Word.Word64 ->
         -- | Output location for the updated block state.
         FFI.Ptr (FFI.Ptr PLTBlockState.PLTBlockState) ->
-        -- | Output location for the remaining energy after execution.
+        -- | Output location for the energy used by the execution.
         FFI.Ptr Word.Word64 ->
         -- | Status code
         IO Word.Word8
 
 -- | The outcome of executing a transaction using the PLT scheduler.
 data ExecutionOutcome = ExecutionOutcome
-    { -- | The amount of energy remaining after the execution.
-      erRemainingEnergy :: Types.Energy,
+    { -- | The amount of energy used by the execution.
+      erUsedEnergy :: Types.Energy,
       -- | The resulting execution status.
       erStatus :: Either () ExecutionAccepts
     }
@@ -130,16 +128,57 @@ data ExecutionAccepts = ExecutionAccepts
       eaEvents :: ()
     }
 
+-- | Callback function for reading a token account balance.
+type ReadTokenAccountBalanceCallback =
+    -- | Index of the account to read a token balance for.
+    Types.AccountIndex ->
+    -- | Index of the token to read the balance of.
+    Tokens.TokenIndex ->
+    -- | The balance.
+    IO Tokens.TokenRawAmount
+
+-- | Internal helper function for mapping the `ReadTokenAccountBalanceCallback` into the more
+-- low-level function pointer which can be passed in FFI.
+wrapReadTokenAccountBalanceCallback :: ReadTokenAccountBalanceCallback -> IO ReadTokenAccountBalanceCallbackPtr
+wrapReadTokenAccountBalanceCallback callback =
+    ffiWrapReadTokenAccountBalanceCallback $ wrapped
+  where
+    wrapped :: ReadTokenAccountBalanceCallbackFFI
+    wrapped accountIndex tokenIndex = do
+        amount <- callback (fromIntegral accountIndex) (fromIntegral tokenIndex)
+        return $ Tokens.theTokenRawAmount amount
+
+-- | Callback function for reading a token account balance.
+--
+-- This is passed as a function pointer in FFI to call, see also `ReadTokenAccountBalanceCallback`
+-- for the more type-safe variant.
+type ReadTokenAccountBalanceCallbackFFI =
+    -- | Index of the account to read a token balance for.
+    Word.Word64 ->
+    -- | Index of the token to read the balance of.
+    Word.Word64 ->
+    -- | The balanacep
+    IO Word.Word64
+
+-- | The callback function pointer type for reading a token account balance.
+type ReadTokenAccountBalanceCallbackPtr = FFI.FunPtr ReadTokenAccountBalanceCallbackFFI
+
+-- | Function to wrap Haskell functions or closures into a function pointer which can be passed over
+-- FFI.
+foreign import ccall "wrapper"
+    ffiWrapReadTokenAccountBalanceCallback ::
+        ReadTokenAccountBalanceCallbackFFI -> IO ReadTokenAccountBalanceCallbackPtr
+
 -- | Callback function for updating a token account balance.
 type UpdateTokenAccountBalanceCallback =
     -- | Index of the account to update a token balance for.
     Types.AccountIndex ->
     -- | Index of the token to update the balance of.
     Tokens.TokenIndex ->
-    -- | The token amount to add to the balance.
+    -- | The token amount to add to or subtract from the balance.
     Tokens.TokenRawAmount ->
-    -- | The token amount to subtract from the balance.
-    Tokens.TokenRawAmount ->
+    -- | If True, add the amount to the balance. If False, subtract the amount from the balance.
+    Bool ->
     -- | Status code, where non-null represents a balance overflow.
     IO Bool
 
@@ -150,8 +189,8 @@ wrapUpdateTokenAccountBalanceCallback callback =
     ffiWrapUpdateTokenAccountBalanceCallback $ wrapped
   where
     wrapped :: UpdateTokenAccountBalanceCallbackFFI
-    wrapped accountIndex tokenIndex add remove = do
-        overflow <- callback (fromIntegral accountIndex) (fromIntegral tokenIndex) (fromIntegral add) (fromIntegral remove)
+    wrapped accountIndex tokenIndex amount addAmount = do
+        overflow <- callback (fromIntegral accountIndex) (fromIntegral tokenIndex) (fromIntegral amount) (if addAmount == 0 then False else True)
         return $ if overflow then 1 else 0
 
 -- | Callback function for updating a token account balance.
@@ -163,10 +202,10 @@ type UpdateTokenAccountBalanceCallbackFFI =
     Word.Word64 ->
     -- | Index of the token to update the balance of.
     Word.Word64 ->
-    -- | The token amount to add to the balance.
+    -- | The token amount to add to or subtract from the balance.
     Word.Word64 ->
-    -- | The token amount to subtract from the balance.
-    Word.Word64 ->
+    -- | If 1, add the amount to the balance. If 0, subtract the amount from the balance.
+    Word.Word8 ->
     -- | Status code, where non-null represents a balance overflow.
     IO Word.Word8
 
