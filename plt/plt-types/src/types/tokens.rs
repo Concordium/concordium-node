96e9e371b858fb2c8890c3d7e289b58eb88cba41
Allan Rasmussen <allanbrondum@gmail.com>
2026-01-27 22:26:47 +0100

    tokenamount

--- plt/plt-types/src/types/tokens.rs
+++ plt/plt-types/src/types/tokens.rs
@@ -1,4 +1,7 @@
-use concordium_base::common::{Buffer, Deserial, ParseResult, Put, ReadBytesExt, Serial};
+use concordium_base::common::__serialize_private::anyhow::{Context, bail};
+use concordium_base::common::{
+    Buffer, Deserial, Get, ParseResult, Put, ReadBytesExt, Serial, Serialize,
+};
 
 /// Token amount without decimals specified. The token amount represented by
 /// this type must always be represented with the number of decimals
@@ -41,13 +44,33 @@ impl Serial for RawTokenAmount {
         for byte in bytes.iter().rev() {
             out.put(byte);
         }
+
+        // todo ar non alloc
     }
 }
 
 impl Deserial for RawTokenAmount {
     fn deserial<R: ReadBytesExt>(source: &mut R) -> ParseResult<Self> {
-        // todo ar
-        todo!()
+        // todo ar don't allow padding
+
+        // Decode first byte and use 7 bits from it.
+        let mut byte: u8 = source.get()?;
+        let mut value = 0x7f & byte as u64;
+
+        // Decode additional byte if 8'th bit is set.
+        while 0x80 & byte != 0 {
+            // Check if shifting would overflow the value
+            if value & (0x7f << (64 - 7)) != 0 {
+                bail!("Token amount not representable as u64");
+            }
+
+            // Decode additional byte and use 7 bits from it as least significant bytes in value
+            byte = source.get()?;
+            value <<= 7;
+            value |= 0x7f & byte as u64;
+        }
+
+        Ok(Self(value))
     }
 }
 
@@ -57,7 +80,7 @@ impl Deserial for RawTokenAmount {
 /// decimals for the token it represents an amount for.
 ///
 /// Corresponding Haskell type: `Concordium.Types.Tokens.TokenAmount`
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Serial)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Serialize)]
 pub struct TokenAmount {
     /// The amount of tokens as an unscaled integer value.
     pub amount: RawTokenAmount,
@@ -75,42 +98,82 @@ mod test {
         let token_amount = RawTokenAmount(0);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "00");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(1);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "01");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(2);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "02");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(127);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "7f");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(128);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "8100");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(129);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "8101");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(128 * 128 - 1);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "ff7f");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(128 * 128);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "818000");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(128 * 128 + 1);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "818001");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
 
         let token_amount = RawTokenAmount(u64::MAX);
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "81ffffffffffffffff7f");
+        let token_amount_deserialized: RawTokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
+
+        let bytes = hex::decode("82808080808080808000").unwrap();
+        let err = common::from_bytes::<RawTokenAmount, _>(&mut bytes.as_slice())
+            .expect_err("deserialize");
+        assert!(
+            err.to_string()
+                .contains("Token amount not representable as u64"),
+            "err: {}",
+            err
+        );
     }
 
     #[test]
@@ -122,5 +185,9 @@ mod test {
 
         let bytes = common::to_bytes(&token_amount);
         assert_eq!(hex::encode(&bytes), "876804");
+
+        let token_amount_deserialized: TokenAmount =
+            common::from_bytes(&mut bytes.as_slice()).unwrap();
+        assert_eq!(token_amount_deserialized, token_amount);
     }
 }
