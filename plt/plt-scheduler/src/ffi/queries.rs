474cdded02af9d4be738b7524ec7dc49da9370d4
Allan Rasmussen <allanbrondum@gmail.com>
2026-02-12 09:24:44 +0100

    FFI for PLT list query

--- plt/plt-scheduler/src/ffi/queries.rs
+++ plt/plt-scheduler/src/ffi/queries.rs
@@ -110,7 +110,7 @@ extern "C" fn ffi_query_plt_list(
     get_account_index_by_address_callback: GetAccountIndexByAddressCallback,
     get_token_account_states_callback: GetTokenAccountStatesCallback,
     block_state: *const PltBlockStateSavepoint,
-    return_data_out: *mut *const u8,
+    return_data_out: *mut *mut u8,
     return_data_len_out: *mut size_t,
 ) -> u8 {
     assert!(!block_state.is_null(), "block_state is a null pointer.");
