48ad67e7045687b876281a501b286f3ba6b36559
allanbrondum <ar@concordium.com>
2026-03-26 07:46:07 +0100

    Rely on toolchain.toml for Rust version … (#1595)
    * Rely on toolchain.toml for Rust version and use windows-mscv instead of windows-gnu platform target
    
    * Move stack.static.yaml next to stack.yaml
    
    * updatebase
    
    * Rust toolchain readme
    
    * Windows Rust toolchain
    
    * Windows Rust toolchain
    
    * Windows Rust toolchain
    
    * Remove musl build variant
    
    * Read Rust version on powershell
    
    * print toolchain
    
    * prebuild Rust library
    
    * Prebuild Rust library
    
    * comment

--- scripts/distribution/windows/build-all.ps1
+++ scripts/distribution/windows/build-all.ps1
@@ -1,18 +1,30 @@
-param ([string] $rustVersion = "1.73", [string] $nodeVersion)
+param ([string] $rustVersion, [string] $nodeVersion)
 
 Write-Output "stack version: $(stack --version)"
 Write-Output "cargo version: $(cargo --version)"
 Write-Output "flatc version: $(flatc --version)"
 Write-Output "protoc version: $(protoc --version)"
 
-# Set the default rust toolchain so that consensus rust dependencies use it.
-rustup default $rustVersion-x86_64-pc-windows-gnu
+# Override the rust toolchain so that consensus rust dependencies use it.
+rustup override set $rustVersion-x86_64-pc-windows-gnu
+
+# The reason we "prebuild" the node Rust library dependency:
+# When Setup.hs is run (which invokes the cargo build), stack puts 
+# the LLVM version of dlltool on the path, which is not compatible with the gnu dlltool. 
+# It thus does not generate the expected import lib. By the rust upfront, when it is 
+# subsequently built in Setup.hs it will already be built and so does not get rebuilt.
+Write-Output "Prebuilding node Rust library..."
+Push-Location plt
+rustup show active-toolchain
+cargo build --release --locked -p node-rust-library
+Pop-Location
 
 Write-Output "Building consensus..."
 stack build
 if ($LASTEXITCODE -ne 0) { throw "Failed building consensus" }
 
 Write-Output "Building node..."
+stack exec -- rustup show active-toolchain
 stack exec -- cargo build --manifest-path concordium-node\Cargo.toml --release --locked
 if ($LASTEXITCODE -ne 0) { throw "Failed building node" }
 
