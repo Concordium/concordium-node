09a034de0fd08e0f2c0e9c10a8f4650f137a71ef
Allan Rasmussen <allanbrondum@gmail.com>
2026-04-11 09:40:39 +0200

    benchmark

--- plt/plt-block-state/benches/lfmb_tree.rs
+++ plt/plt-block-state/benches/lfmb_tree.rs
@@ -1,6 +1,7 @@
 //! Benchmarks for the [`LFMBTree`] covering asymptotic behavior with varying tree size.
 
-use divan::Bencher;
+use divan::counter::ItemsCount;
+use divan::{AllocProfiler, Bencher};
 use plt_block_state::block_state::blob_store::StoreSerialized;
 use plt_block_state::block_state::blob_store::test_stub::{BlobStoreStub, UnreachableBlobStore};
 use plt_block_state::block_state::lfmb_tree::{LFMBTree, LFMBTreeKey};
@@ -37,22 +38,22 @@ fn build_tree(size: u64) -> BenchTree {
     tree
 }
 
-/// Warmup CPU. Divan does not seem to be good at doing this automatically.
-#[divan::bench]
-fn a_warmup() {
-    let mut x: u64 = 0;
-    for i in divan::black_box(0..10_000_000) {
-        x = x.wrapping_add(i);
-    }
-    divan::black_box(x);
-}
+// /// Warmup CPU. Divan does not seem to be good at doing this automatically.
+// #[divan::bench]
+// fn a_warmup() {
+//     let mut x: u64 = 0;
+//     for i in divan::black_box(0..10_000_000) {
+//         x = x.wrapping_add(i);
+//     }
+//     divan::black_box(x);
+// }
 
 #[divan::bench(args = SIZES)]
 fn bench_with_value(bencher: Bencher, size: u64) {
     let tree = divan::black_box(build_tree(size));
     bencher
         .with_inputs(|| BenchKey(rand::random_range(0..size)))
-        .bench_values(|key| {
+        .bench_local_values(|key| {
             tree.with_value(&UnreachableBlobStore, key, |v| Ok(*v))
                 .unwrap()
                 .unwrap()
@@ -62,7 +63,7 @@ fn bench_with_value(bencher: Bencher, size: u64) {
 #[divan::bench(args = SIZES)]
 fn bench_insert_value(bencher: Bencher, size: u64) {
     let tree = divan::black_box(build_tree(size));
-    bencher.bench(|| {
+    bencher.bench_local(|| {
         tree.insert_value(&UnreachableBlobStore, StoreSerialized(0))
             .unwrap()
     });
@@ -73,7 +74,7 @@ fn bench_update_value(bencher: Bencher, size: u64) {
     let tree = divan::black_box(build_tree(size));
     bencher
         .with_inputs(|| BenchKey(rand::random_range(0..size)))
-        .bench_values(|key| {
+        .bench_local_values(|key| {
             tree.update_value(&UnreachableBlobStore, key, |v| Ok(StoreSerialized(v.0 + 1)))
                 .unwrap()
                 .unwrap()
@@ -83,7 +84,7 @@ fn bench_update_value(bencher: Bencher, size: u64) {
 #[divan::bench(args = SIZES)]
 fn bench_values(bencher: Bencher, size: u64) {
     let tree = divan::black_box(build_tree(size));
-    bencher.bench(|| {
+    bencher.bench_local(|| {
         tree.values(&UnreachableBlobStore, |v| Ok(v.0))
             .map(|r| r.unwrap())
             .sum::<u64>()
