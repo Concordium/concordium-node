64250a07e18eec23ee054558bc99bf03d9f4cdba
Allan Rasmussen <allanbrondum@gmail.com>
2026-04-14 10:42:18 +0200

    Benchmark LFMB tree

--- plt/plt-block-state/benches/lfmb_tree.rs
+++ plt/plt-block-state/benches/lfmb_tree.rs
@@ -1,8 +1,11 @@
 //! Benchmarks for the [`LfmbTree`] covering asymptotic behavior with varying tree size.
 
 use divan::Bencher;
+use plt_block_state::block_state::blob_store;
 use plt_block_state::block_state::blob_store::StoreSerialized;
-use plt_block_state::block_state::blob_store::test_stub::UnreachableBlobStore;
+use plt_block_state::block_state::blob_store::test_stub::{BlobStoreStub, UnreachableBlobStore};
+use plt_block_state::block_state::cacheable::Cacheable;
+use plt_block_state::block_state::hash::Hashable;
 use plt_block_state::block_state::lfmb_tree::{LfmbTree, LfmbTreeKey};
 
 fn main() {
@@ -24,7 +27,12 @@ impl LfmbTreeKey for BenchKey {
 
 type BenchTree = LfmbTree<BenchKey, StoreSerialized<u64>>;
 
+/// Sizes of trees to benchmark operations on.
 const SIZES: &[u64] = &[(1 << 10) - 1, (1 << 15) - 1, (1 << 20) - 1];
+
+/// Small sizes of trees to benchmark operations on. Used for benchmarks that needs
+/// to create the full tree for each iteration.
+const SMALL_SIZES: &[u64] = &[1 << 5, 1 << 10, 1 << 15];
 
 fn build_tree(size: u64) -> BenchTree {
     let mut tree = BenchTree::empty();
@@ -59,14 +67,10 @@ fn bench_with_value(bencher: Bencher, size: u64) {
         });
 }
 
-/// Benchmark [`LfmbTree::insert_value`] for different tree sizes.
-#[divan::bench(args = SIZES)]
-fn bench_insert_value(bencher: Bencher, size: u64) {
-    let tree = divan::black_box(build_tree(size));
-    bencher.bench_local(|| {
-        tree.insert_value(&UnreachableBlobStore, StoreSerialized(0))
-            .unwrap()
-    });
+/// Benchmark [`LfmbTree::insert_value`] by creating trees of different sizes.
+#[divan::bench(args = SMALL_SIZES)]
+fn bench_insert_values(bencher: Bencher, size: u64) {
+    bencher.bench_local(|| build_tree(size));
 }
 
 /// Benchmark [`LfmbTree::update_value`] for different tree sizes.
@@ -91,4 +95,35 @@ fn bench_values(bencher: Bencher, size: u64) {
             .map(|r| r.unwrap())
             .sum::<u64>()
     });
+}
+
+/// Benchmark [`LfmbTree::hash`] iterator for different tree sizes.
+#[divan::bench(args = SMALL_SIZES)]
+fn bench_hash(bencher: Bencher, size: u64) {
+    bencher
+        .with_inputs(|| build_tree(size))
+        .bench_local_values(|tree| tree.hash(&UnreachableBlobStore).unwrap());
+}
+
+/// Benchmark [`LfmbTree::hash`] iterator for different tree sizes.
+#[divan::bench(args = SMALL_SIZES)]
+fn bench_store(bencher: Bencher, size: u64) {
+    bencher
+        .with_inputs(|| build_tree(size))
+        .bench_local_values(|tree| blob_store::store_to_store(&mut BlobStoreStub::default(), tree));
+}
+
+/// Benchmark [`LfmbTree::hash`] iterator for different tree sizes.
+#[divan::bench(args = SMALL_SIZES)]
+fn bench_cache(bencher: Bencher, size: u64) {
+    let mut store = BlobStoreStub::default();
+    let tree = build_tree(size);
+    let blob_ref = divan::black_box(blob_store::store_to_store(&mut store, tree));
+
+    bencher
+        .with_inputs(|| {
+            let tree: BenchTree = blob_store::load_from_store(&store, blob_ref).unwrap();
+            tree
+        })
+        .bench_local_values(|tree| tree.cache_reference_values(&store));
 }
