305ddecd4cb57fb31288360f3a0f658e3151f689
Søren Bruus Zeppelin <soerenbf@gmail.com>
2026-04-16 13:41:51 +0200

    Remove init state from lock controller interface

--- plt/plt-lock-module/src/lock_controller.rs
+++ plt/plt-lock-module/src/lock_controller.rs
@@ -1,38 +1,30 @@
 //! Runtime interface for protocol-level lock controllers.
 
-use concordium_base::{
-    base::AccountIndex, protocol_level_locks::LockId, protocol_level_tokens::RawCbor,
-};
-use plt_block_state::block_state_interface::{BlockStateOperations, BlockStateQuery};
+use concordium_base::protocol_level_tokens::RawCbor;
+use plt_block_state::block_state_interface::BlockStateQuery;
 use plt_scheduler_types::types::locks::LockControllerConfig;
 
-/// Temporary runtime lock operation model.
-///
-/// TODO: Replace this placeholder with the corresponding lock operation type
-/// defined in `concordium_base` once it exists.
+/// Runtime lock operation model.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum LockOperation {}
 
 /// Lock-controller specific rejection.
-///
-/// TODO: Replace this placeholder with the corresponding lock reject reason type
-/// defined in `concordium_base` once it exists.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct LockControllerRejectReason;
 
 /// Runtime interface implemented by protocol-level locks.
 pub trait LockController {
-    fn initialize_state<BSO: BlockStateOperations>(
-        &self,
-        bso: &mut BSO,
-        lock_id: &LockId,
-    ) -> BSO::Lock;
-
     /// Approve or reject a lock operation.
+    ///
+    /// * `bsq`: the block state to query on
+    /// * `sender`: the transaction sender reference
+    /// * `lock`: the lock reference. This is used instead of the lock ID from the operation.
+    /// * `operation`: the lock operation to approve/reject.
     fn approve_operation<BSQ: BlockStateQuery>(
         &self,
         bsq: &BSQ,
-        sender: AccountIndex,
+        sender: &BSQ::Account,
+        lock: &BSQ::Lock,
         operation: &LockOperation,
     ) -> Result<(), LockControllerRejectReason>;
 
@@ -40,28 +32,17 @@ pub trait LockController {
     fn query_info<BSQ: BlockStateQuery>(&self, bsq: &BSQ, lock: &BSQ::Lock) -> RawCbor;
 }
 
-impl LockController for &LockControllerConfig {
-    fn initialize_state<BSO: BlockStateOperations>(
-        &self,
-        bso: &mut BSO,
-        lock_id: &LockId,
-    ) -> BSO::Lock {
-        match self {
-            LockControllerConfig::SimpleV0(lock_controller_simple_v0) => {
-                lock_controller_simple_v0.initialize_state(bso, lock_id)
-            }
-        }
-    }
-
+impl LockController for LockControllerConfig {
     fn approve_operation<BSQ: BlockStateQuery>(
         &self,
         bsq: &BSQ,
-        sender: AccountIndex,
+        sender: &BSQ::Account,
+        lock: &BSQ::Lock,
         operation: &LockOperation,
     ) -> Result<(), LockControllerRejectReason> {
         match self {
             LockControllerConfig::SimpleV0(lock_controller_simple_v0) => {
-                lock_controller_simple_v0.approve_operation(bsq, sender, operation)
+                lock_controller_simple_v0.approve_operation(bsq, sender, lock, operation)
             }
         }
     }
--- plt/plt-lock-module/src/lock_controller_simple.rs
+++ plt/plt-lock-module/src/lock_controller_simple.rs
@@ -1,26 +1,16 @@
-use concordium_base::{
-    base::AccountIndex, protocol_level_locks::LockId, protocol_level_tokens::RawCbor,
-};
-use plt_block_state::block_state_interface::{BlockStateOperations, BlockStateQuery};
+use concordium_base::protocol_level_tokens::RawCbor;
+use plt_block_state::block_state_interface::BlockStateQuery;
 use plt_scheduler_types::types::locks::LockControllerSimpleV0;
 
 use crate::lock_controller::{LockController, LockControllerRejectReason, LockOperation};
 
 impl LockController for &LockControllerSimpleV0 {
-    // TODO: implemented as part of COR-2302
-    fn initialize_state<BSO: BlockStateOperations>(
-        &self,
-        _bso: &mut BSO,
-        _lock_id: &LockId,
-    ) -> BSO::Lock {
-        todo!()
-    }
-
     // TODO: implemented as part of COR-2305
     fn approve_operation<BSQ: BlockStateQuery>(
         &self,
         _bsq: &BSQ,
-        _sender: AccountIndex,
+        _sender: &BSQ::Account,
+        _lock: &BSQ::Lock,
         _operation: &LockOperation,
     ) -> Result<(), LockControllerRejectReason> {
         todo!()
