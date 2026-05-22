c421eebf8424f9ca2a9b9ed96249dc3fd1c09ee8
Allan Rasmussen <allanbrondum@gmail.com>
2026-05-22 10:57:11 +0200

    Implement protocol specific scheduler operations

--- plt/plt-scheduler/src/protocol_level_tokens/token_module/errors.rs
+++ plt/plt-scheduler/src/protocol_level_tokens/token_module/errors.rs
@@ -34,19 +34,10 @@ pub struct MintWouldOverflowError {
     pub max_representable_amount: RawTokenAmount,
 }
 
-// todo ar delete
-
-/// An invariant in the token state that should be enforced
-/// is broken. This is generally an error that should never happen and is unrecoverable.
-#[derive(Debug, thiserror::Error)]
-#[error("Token module state invariant broken: {0}")]
-pub struct TokenStateInvariantError(pub String);
 
 /// Represents the reasons why a token transfer may fail.
 #[derive(Debug, thiserror::Error)]
 pub enum TokenTransferError {
-    #[error("{0}")]
-    StateInvariantViolation(#[from] TokenStateInvariantError),
     #[error("Insufficient balance for transfer: {0}")]
     InsufficientBalance(#[from] InsufficientBalanceError),
 }
@@ -55,16 +46,12 @@ pub enum TokenTransferError {
 #[derive(Debug, thiserror::Error)]
 pub enum TokenMintError {
     #[error("{0}")]
-    StateInvariantViolation(#[from] TokenStateInvariantError),
-    #[error("{0}")]
     MintWouldOverflow(#[from] MintWouldOverflowError),
 }
 
 /// Represents the reasons why a token burn may fail.
 #[derive(Debug, thiserror::Error)]
 pub enum TokenBurnError {
-    #[error("{0}")]
-    StateInvariantViolation(#[from] TokenStateInvariantError),
     #[error("Insufficient balance for burn: {0}")]
     InsufficientBalance(#[from] InsufficientBalanceError),
 }
