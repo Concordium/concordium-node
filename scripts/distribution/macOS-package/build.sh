7ee3e49e007da090f27d9c91bea74e95556afc67
Søren Bruus Zeppelin <sz@concordium.com>
2026-09-08 13:46:54 +0200

    Add release environment

--- scripts/distribution/macOS-package/build.sh
+++ scripts/distribution/macOS-package/build.sh
@@ -295,9 +295,9 @@ function collectDylibs() {
     readonly stackLibDirs
 
     logInfo " -- Processing concordium-node"
-    collectDylibsFor "$payloadDir/Library/Concordium Node/concordium-node"
+    collectDylibsFor "$payloadDir/Library/Concordium Node/concordium-node" > /dev/null
     logInfo " -- Processing node-collector"
-    collectDylibsFor "$payloadDir/Library/Concordium Node/node-collector"
+    collectDylibsFor "$payloadDir/Library/Concordium Node/node-collector" > /dev/null
 
     logInfo "Done"
 }
