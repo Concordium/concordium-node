7532cf09a2140e95fd884b003861bbeb3fb8eec4
Søren Bruus Zeppelin <sz@concordium.com>
2026-09-23 09:44:22 +0200

    Remove unused function

--- plt/plt-block-state/src/persistent/smart_contract_trie.rs
+++ plt/plt-block-state/src/persistent/smart_contract_trie.rs
@@ -62,26 +62,6 @@ impl PersistentState {
         })
     }
 
-    /// Iterate keys that start with the given prefix without loading their values.
-    pub fn keys_with_prefix<'a, L: BlobStoreLoad>(
-        &self,
-        loader: &'a L,
-        prefix: &[u8],
-    ) -> BlockStateResult<impl Iterator<Item = Vec<u8>> + use<'a, L>> {
-        let mut loader_adapter = LoaderAdapter(loader);
-        let mut mutable_state = self.lock_read().thaw();
-        let mut trie = mutable_state.get_inner(&mut loader_adapter).lock();
-        let trie_iter = trie.iter(&mut loader_adapter, prefix).map_err(|err| {
-            BlockStateFailure::Invariant(format!("Error iterating keys in MutableTrie: {err}"))
-        })?;
-
-        Ok(KeyPrefixIterator {
-            loader,
-            trie: trie.clone(),
-            trie_iter,
-        })
-    }
-
     /// Thaw the trie to make it [mutable](MutableState).
     pub fn thaw(&self) -> MutableState {
         let persistent_state = self.lock_read();
@@ -123,37 +103,6 @@ impl BlobStoreMovable for PersistentState {
             })?;
 
         Ok(Self(RwLock::new(new_persistent_state)))
-    }
-}
-
-struct KeyPrefixIterator<'a, L> {
-    loader: &'a L,
-    trie: trie::MutableTrie,
-    trie_iter: Option<trie::low_level::Iterator>,
-}
-
-impl<L> Drop for KeyPrefixIterator<'_, L> {
-    fn drop(&mut self) {
-        if let Some(trie_iter) = self.trie_iter.as_ref() {
-            self.trie.delete_iter(trie_iter);
-        }
-    }
-}
-
-impl<L: BlobStoreLoad> Iterator for KeyPrefixIterator<'_, L> {
-    type Item = Vec<u8>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let trie_iter = self.trie_iter.as_mut()?;
-        let mut loader = LoaderAdapter(self.loader);
-        match self
-            .trie
-            .next(&mut loader, trie_iter, &mut trie::EmptyCounter)
-        {
-            Ok(Some(_)) => Some(trie_iter.get_key().to_vec()),
-            Ok(None) => None,
-            Err(counter_err) => match counter_err {},
-        }
     }
 }
 
